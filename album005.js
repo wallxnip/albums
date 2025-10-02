b9b69b7f202ff7ee050b3cb2e5f0957abbff9d77
Apps <wallxnip@gmail.com>
2025-10-02 12:26:30 -0300

    Criar album005.js

---
+++ album005.js
@@ -0,0 +1,115 @@
+// Lista de músicas
+const playlist504 = [
+  {
+    title: "Alegra",
+    artist: "",
+    file: "https://files.catbox.moe/tg9gli.mp3",
+    cover: ""
+  },
+  {
+    title: "Amanheceu",
+    artist: "",
+    file: "https://files.catbox.moe/9xfgv9.mp3",
+    cover: ""
+  },
+  {
+    title: "Aprendi Maldade Com Essa Maldita",
+    artist: "",
+    file: "https://files.catbox.moe/1bvl6s.mp3",
+    cover: ""
+  },
+  {
+    title: "Bela Dupla",
+    artist: "",
+    file: "https://files.catbox.moe/a2lv8n.mp3",
+    cover: ""
+  },
+  {
+    title: "É Que Eu Te Sarrei",
+    artist: "",
+    file: "https://files.catbox.moe/kzwwdx.mp3",
+    cover: ""
+  },
+  {
+    title: "Maldito Cupido",
+    artist: "",
+    file: "https://files.catbox.moe/f5kizt.mp3",
+    cover: ""
+  },
+  {
+    title: "Não Dá pra Negar",
+    artist: "",
+    file: "https://files.catbox.moe/s8exg9.mp3",
+    cover: ""
+  },
+  {
+    title: "O Que Que Tá Acontecendo",
+    artist: "",
+    file: "https://files.catbox.moe/usjjy7.mp3",
+    cover: ""
+  },
+  {
+    title: "Perdoa os Meus Pecados",
+    artist: "",
+    file: "https://files.catbox.moe/ft4unw.mp3",
+    cover: ""
+  },
+  {
+    title: "Plano Perfeito",
+    artist: "",
+    file: "https://files.catbox.moe/t1za0g.mp3",
+    cover: ""
+  },
+  {
+    title: "Postura",
+    artist: "",
+    file: "https://files.catbox.moe/ezvvku.mp3",
+    cover: ""
+  },
+  {
+    title: "Quadradinho",
+    artist: "",
+    file: "https://files.catbox.moe/w6z0fw.mp3",
+    cover: ""
+  },
+  {
+    title: "Se Tu Quer Replay",
+    artist: "",
+    file: "https://files.catbox.moe/n87je8.mp3",
+    cover: ""
+  },
+  {
+    title: "Segue o Baile",
+    artist: "",
+    file: "https://files.catbox.moe/dg23ru.mp3",
+    cover: ""
+  },
+  {
+    title: "Tatuagem da Borboleta",
+    artist: "",
+    file: "https://files.catbox.moe/2sre6r.mp3",
+    cover: ""
+  },
+  {
+    title: "Tava Escrito nas Estrelas",
+    artist: "",
+    file: "https://files.catbox.moe/brqkwj.mp3",
+    cover: ""
+  },
+  {
+    title: "Vou te Contar um Segredinho",
+    artist: "",
+    file: "https://files.catbox.moe/26y95p.mp3",
+    cover: ""
+  },
+  {
+    title: "",
+    artist: "",
+    file: "",
+    cover: ""
+  },
+
+
+
+
+];
