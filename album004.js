e55f0065c3e50d5c0b61b44fe7823600f4858e60
Apps <wallxnip@gmail.com>
2025-10-02 08:45:26 -0300

    Criar album004.js

---
+++ album004.js
@@ -0,0 +1,98 @@
+// Lista de músicas
+const playlist4 = [
+  {
+    title: "Hold Me Now",
+    artist: "DJ Eighties",
+    file: "https://files.catbox.moe/a752j2.mp3",
+    cover: "https://i.postimg.cc/P5hXfy7j/Beauty-Plus-20251002082827535-saveg.png"
+  },
+  {
+    title: "",
+    artist: "DJ Eighties",
+    file: "",
+    cover: ""
+  },
+  {
+    title: "",
+    artist: "DJ Eighties",
+    file: "",
+    cover: ""
+  },
+  {
+    title: "",
+    artist: "DJ Eighties",
+    file: "",
+    cover: ""
+  },
+  {
+    title: "",
+    artist: "DJ Eighties",
+    file: "",
+    cover: ""
+  },
+  {
+    title: "",
+    artist: "DJ Eighties",
+    file: "",
+    cover: ""
+  },
+  {
+    title: "",
+    artist: "DJ Eighties",
+    file: "",
+    cover: ""
+  },
+  {
+    title: "",
+    artist: "DJ Eighties",
+    file: "",
+    cover: ""
+  },
+  {
+    title: "",
+    artist: "DJ Eighties",
+    file: "",
+    cover: ""
+  },
+  {
+    title: "",
+    artist: "",
+    file: "",
+    cover: ""
+  },
+  {
+    title: "",
+    artist: "",
+    file: "",
+    cover: ""
+  },
+  {
+    title: "",
+    artist: "",
+    file: "",
+    cover: ""
+  },
+  {
+    title: "",
+    artist: "",
+    file: "",
+    cover: ""
+  },
+  {
+    title: "",
+    artist: "",
+    file: "",
+    cover: ""
+  },
+  {
+    title: "",
+    artist: "",
+    file: "",
+    cover: ""
+  },
+
+
+
+
+
+];
