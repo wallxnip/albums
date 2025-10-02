f05b984848d8fe624fa1e380d3001b96345697c8
Apps <wallxnip@gmail.com>
2025-10-02 12:05:11 -0300

    Atualizar o album004.js

--- album004.js
+++ album004.js
@@ -7,91 +7,65 @@ const playlist503 = [
     cover: "https://i.postimg.cc/P5hXfy7j/Beauty-Plus-20251002082827535-saveg.png"
   },
   {
-    title: "",
+    title: "Im Starting To Love You Now",
     artist: "DJ Eighties",
-    file: "",
-    cover: ""
+    file: "https://files.catbox.moe/fk4002.mp3",
+    cover: "https://i.postimg.cc/P5hXfy7j/Beauty-Plus-20251002082827535-saveg.png"
   },
   {
-    title: "",
+    title: "Not Now",
     artist: "DJ Eighties",
-    file: "",
-    cover: ""
+    file: "https://files.catbox.moe/viqisw.mp3",
+    cover: "https://i.postimg.cc/P5hXfy7j/Beauty-Plus-20251002082827535-saveg.png"
   },
   {
-    title: "",
+    title: "Whispers In The Night",
     artist: "DJ Eighties",
-    file: "",
-    cover: ""
+    file: "https://files.catbox.moe/1eu8iv.mp3",
+    cover: "https://i.postimg.cc/P5hXfy7j/Beauty-Plus-20251002082827535-saveg.png"
   },
   {
-    title: "",
+    title: "Darling my Darling",
     artist: "DJ Eighties",
-    file: "",
-    cover: ""
+    file: "https://files.catbox.moe/cq1in6.mp3",
+    cover: "https://i.postimg.cc/P5hXfy7j/Beauty-Plus-20251002082827535-saveg.png"
   },
   {
-    title: "",
+    title: "Back To My Hear",
     artist: "DJ Eighties",
-    file: "",
-    cover: ""
+    file: "https://files.catbox.moe/mc3ndl.mp3",
+    cover: "https://i.postimg.cc/P5hXfy7j/Beauty-Plus-20251002082827535-saveg.png"
   },
   {
-    title: "",
+    title: "Forever",
     artist: "DJ Eighties",
-    file: "",
-    cover: ""
+    file: "https://files.catbox.moe/9e7j8g.mp3",
+    cover: "https://i.postimg.cc/P5hXfy7j/Beauty-Plus-20251002082827535-saveg.png"
   },
   {
-    title: "",
+    title: "Im not your Prey",
     artist: "DJ Eighties",
-    file: "",
-    cover: ""
+    file: "https://files.catbox.moe/rir4t0.mp3",
+    cover: "https://i.postimg.cc/P5hXfy7j/Beauty-Plus-20251002082827535-saveg.png"
   },
   {
-    title: "",
+    title: "Im Starting To Love You Now",
     artist: "DJ Eighties",
-    file: "",
-    cover: ""
+    file: "https://files.catbox.moe/rir4t0.mp3",
+    cover: "https://i.postimg.cc/P5hXfy7j/Beauty-Plus-20251002082827535-saveg.png"
   },
   {
-    title: "",
-    artist: "",
-    file: "",
-    cover: ""
+    title: "It Was a Kiss",
+    artist: "DJ Eighties",
+    file: "https://files.catbox.moe/kev8u4.mp3",
+    cover: "https://i.postimg.cc/P5hXfy7j/Beauty-Plus-20251002082827535-saveg.png"
   },
   {
-    title: "",
-    artist: "",
-    file: "",
-    cover: ""
+    title: "You Are My Star",
+    artist: "DJ Eighties",
+    file: "https://files.catbox.moe/ccw0rq.mp3",
+    cover: "https://i.postimg.cc/P5hXfy7j/Beauty-Plus-20251002082827535-saveg.png"
   },
-  {
-    title: "",
-    artist: "",
-    file: "",
-    cover: ""
-  },
-  {
-    title: "",
-    artist: "",
-    file: "",
-    cover: ""
-  },
-  {
-    title: "",
-    artist: "",
-    file: "",
-    cover: ""
-  },
-  {
-    title: "",
-    artist: "",
-    file: "",
-    cover: ""
-  },
-
-
 
 
 
