6b11f15ca8465a27eac3af079ca5932c46b4cc80
Apps <wallxnip@gmail.com>
2025-10-02 00:22:44 -0300

    Atualizar o album003.js

--- album003.js
+++ album003.js
@@ -1,96 +1,168 @@
-// Lista de músicas
+// Hip-Hop 
 const playlist502 = [
   {
-    title: "",
-    artist: "",
-    file: "",
-    cover: ""
+    title: "Esquecer de Tudo",
+    artist: "Hip-Hop",
+    file: "https://dn721806.ca.archive.org/0/items/quebra-cabeca/Esquecer%20de%20Tudo.mp3",
+    cover: "https://i.postimg.cc/1tDTh4fd/6.jpg"
   },
   {
-    title: "",
-    artist: "",
-    file: "",
-    cover: ""
+    title: "Agora",
+    artist: "Hip-Hop",
+    file: "https://dn721806.ca.archive.org/0/items/quebra-cabeca/Agora.mp3",
+    cover: "https://i.postimg.cc/1tDTh4fd/6.jpg"
   },
   {
-    title: "",
-    artist: "",
-    file: "",
-    cover: ""
+    title: "Alma de Pipa",
+    artist: "Hip-Hop",
+    file: "https://dn721806.ca.archive.org/0/items/quebra-cabeca/Alma%20de%20Pipa.mp3",
+    cover: "https://i.postimg.cc/1tDTh4fd/6.jpg"
   },
   {
-    title: "",
-    artist: "",
-    file: "",
-    cover: ""
+    title: "Carro Velho",
+    artist: "Hip-Hop",
+    file: "https://dn721806.ca.archive.org/0/items/quebra-cabeca/Carro%20Velho.mp3",
+    cover: "https://i.postimg.cc/1tDTh4fd/6.jpg"
   },
   {
-    title: "",
-    artist: "",
-    file: "",
-    cover: ""
+    title: "Copo Pro Alto",
+    artist: "Hip-Hop",
+    file: "https://dn721806.ca.archive.org/0/items/quebra-cabeca/Copo%20Pro%20Alto.mp3",
+    cover: "https://i.postimg.cc/1tDTh4fd/6.jpg"
   },
   {
-    title: "",
-    artist: "",
-    file: "",
-    cover: ""
+    title: "Coração de Aço",
+    artist: "Hip-Hop",
+    file: "https://dn721806.ca.archive.org/0/items/quebra-cabeca/Cora%C3%A7%C3%A3o%20de%20A%C3%A7o.mp3",
+    cover: "https://i.postimg.cc/1tDTh4fd/6.jpg"
   },
   {
-    title: "",
-    artist: "",
-    file: "",
-    cover: ""
+    title: "Coração de Carro Forte",
+    artist: "Hip-Hop",
+    file: "https://dn721806.ca.archive.org/0/items/quebra-cabeca/Cora%C3%A7%C3%A3o%20de%20Carro%20Forte.mp3",
+    cover: "https://i.postimg.cc/1tDTh4fd/6.jpg"
   },
   {
-    title: "",
-    artist: "",
-    file: "",
-    cover: ""
+    title: "Obsessão",
+    artist: "Hip-Hop",
+    file: "https://dn721806.ca.archive.org/0/items/quebra-cabeca/Obsess%C3%A3o.mp3",
+    cover: "https://i.postimg.cc/1tDTh4fd/6.jpg"
   },
   {
-    title: "",
-    artist: "",
-    file: "",
-    cover: ""
+    title: "A Fenda",
+    artist: "Hip-Hop",
+    file: "https://dn721806.ca.archive.org/0/items/quebra-cabeca/A%20Fenda.mp3",
+    cover: "https://i.postimg.cc/1tDTh4fd/6.jpg"
   },
   {
-    title: "",
-    artist: "",
-    file: "",
-    cover: ""
+    title: "Preta",
+    artist: "Hip-Hop",
+    file: "https://dn721806.ca.archive.org/0/items/quebra-cabeca/Preta.mp3",
+    cover: "https://i.postimg.cc/1tDTh4fd/6.jpg"
   },
   {
-    title: "",
-    artist: "",
-    file: "",
-    cover: ""
+    title: "Vida Bagunçada",
+    artist: "Hip-Hop",
+    file: "https://dn721806.ca.archive.org/0/items/quebra-cabeca/Vida%20Bagun%C3%A7ada.mp3",
+    cover: "https://i.postimg.cc/1tDTh4fd/6.jpg"
   },
   {
-    title: "",
-    artist: "",
-    file: "",
-    cover: ""
+    title: "Quebra Cabeça",
+    artist: "Hip-Hop",
+    file: "https://dn721806.ca.archive.org/0/items/quebra-cabeca/Quebra%20Cabe%C3%A7a.mp3",
+    cover: "https://i.postimg.cc/1tDTh4fd/6.jpg"
   },
   {
-    title: "",
-    artist: "",
-    file: "",
-    cover: ""
+    title: "Shortinho Curto",
+    artist: "Hip-Hop",
+    file: "https://dn721806.ca.archive.org/0/items/quebra-cabeca/Shortinho%20Curto.mp3",
+    cover: "https://i.postimg.cc/1tDTh4fd/6.jpg"
   },
   {
-    title: "",
-    artist: "",
-    file: "",
-    cover: ""
+    title: "Temporal",
+    artist: "Hip-Hop",
+    file: "https://dn721806.ca.archive.org/0/items/quebra-cabeca/Temporal.mp3",
+    cover: "https://i.postimg.cc/1tDTh4fd/6.jpg"
   },
   {
-    title: "",
-    artist: "",
-    file: "",
-    cover: ""
+    title: "Um Pedido",
+    artist: "Hip-Hop",
+    file: "https://dn721806.ca.archive.org/0/items/quebra-cabeca/Um%20Pedido.mp3",
+    cover: "https://i.postimg.cc/1tDTh4fd/6.jpg"
+  },
+  {
+    title: "Valores",
+    artist: "Hip-Hop",
+    file: "https://dn721806.ca.archive.org/0/items/quebra-cabeca/Valores.mp3",
+    cover: "https://i.postimg.cc/1tDTh4fd/6.jpg"
+  },
+  {
+    title: "Terrorista Memo",
+    artist: "Hip-Hop",
+    file: "https://dn721806.ca.archive.org/0/items/quebra-cabeca/Terrorista%20Memo.mp3",
+    cover: "https://i.postimg.cc/1tDTh4fd/6.jpg"
+  },
+  {
+    title: "Sexta-Feira",
+    artist: "Hip-Hop",
+    file: "https://ia801608.us.archive.org/6/items/quebra-cabeca/Sexta-Feira.mp3",
+    cover: "https://i.postimg.cc/1tDTh4fd/6.jpg"
+  },
+  {
+    title: "Seu Coração Rodou",
+    artist: "Hip-Hop",
+    file: "https://dn721806.ca.archive.org/0/items/quebra-cabeca/Seu%20Cora%C3%A7%C3%A3o%20Rodou.mp3",
+    cover: "https://i.postimg.cc/1tDTh4fd/6.jpg"
+  },
+  {
+    title: "Renascer",
+    artist: "Hip-Hop",
+    file: "https://dn721806.ca.archive.org/0/items/quebra-cabeca/Renascer.mp3",
+    cover: "https://i.postimg.cc/1tDTh4fd/6.jpg"
+  },
+  {
+    title: "Pirata de Esquina",
+    artist: "Hip-Hop",
+    file: "https://dn721806.ca.archive.org/0/items/quebra-cabeca/Pirata%20de%20Esquina.mp3",
+    cover: "https://i.postimg.cc/1tDTh4fd/6.jpg"
+  },
+  {
+    title: "O Amanhã Vai Ser Melhor",
+    artist: "Hip-Hop",
+    file: "https://ia801608.us.archive.org/6/items/quebra-cabeca/O%20Amanh%C3%A3%20Vai%20Ser%20Melhor.mp3",
+    cover: "https://i.postimg.cc/1tDTh4fd/6.jpg"
+  },
+  {
+    title: "Furacão de Ninar",
+    artist: "Hip-Hop",
+    file: "https://dn721806.ca.archive.org/0/items/quebra-cabeca/Furac%C3%A3o%20de%20Ninar.mp3",
+    cover: "https://i.postimg.cc/1tDTh4fd/6.jpg"
   },
 
+  {
+    title: "Imprevisível",
+    artist: "Hip-Hop",
+    file: "https://dn721806.ca.archive.org/0/items/quebra-cabeca/Imprevis%C3%ADvel.mp3",
+    cover: "https://i.postimg.cc/1tDTh4fd/6.jpg"
+  },
+  {
+    title: "Meu Melhor",
+    artist: "Hip-Hop",
+    file: "https://dn721806.ca.archive.org/0/items/quebra-cabeca/Meu%20Melhor.mp3",
+    cover: "https://i.postimg.cc/1tDTh4fd/6.jpg"
+  },
+  {
+    title: "Nosso Plano",
+    artist: "Hip-Hop",
+    file: "https://dn721806.ca.archive.org/0/items/quebra-cabeca/Nosso%20Plano.mp3",
+    cover: "https://i.postimg.cc/1tDTh4fd/6.jpg"
+  },
+  {
+    title: "Escondido",
+    artist: "Hip-Hop",
+    file: "https://dn721806.ca.archive.org/0/items/quebra-cabeca/Escondido.mp3",
+    cover: "https://i.postimg.cc/1tDTh4fd/6.jpg"
+  }
 
 
 
