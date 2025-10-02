3df1c4ce3015ffe1150929e61e4b43f5fc5c014d
Apps <wallxnip@gmail.com>
2025-10-02 13:47:29 -0300

    Atualizar o album007.js

--- album007.js
+++ album007.js
@@ -4,37 +4,37 @@ const playlist506 = [
     title: "Another Love",
     artist: "Temporal",
     file: "https://files.catbox.moe/8oelmc.mp3",
-    cover: "https://i.postimg.cc/dt30kD1C/9.jpg"
+    cover: "https://i.postimg.cc/TYFtZ4Fb/Beauty-Plus-20251002134530125-save.jpg"
   },
   {
     title: "Apocalypse",
     artist: "Temporal",
     file: "https://files.catbox.moe/gmsls0.mp3",
-    cover: "https://i.postimg.cc/dt30kD1C/9.jpg"
+    cover: "https://i.postimg.cc/TYFtZ4Fb/Beauty-Plus-20251002134530125-save.jpg"
   },
   {
     title: "Dynasty",
     artist: "Temporal",
     file: "https://files.catbox.moe/n4pgnt.mp3",
-    cover: "https://i.postimg.cc/dt30kD1C/9.jpg"
+    cover: "https://i.postimg.cc/TYFtZ4Fb/Beauty-Plus-20251002134530125-save.jpg"
   },
   {
     title: "One More Light",
     artist: "Temporal",
     file: "https://files.catbox.moe/rnom3s.mp3",
-    cover: "https://i.postimg.cc/dt30kD1C/9.jpg"
+    cover: "https://i.postimg.cc/TYFtZ4Fb/Beauty-Plus-20251002134530125-save.jpg"
   },
   {
     title: "Say Yes To Heaven",
     artist: "Temporal",
     file: "https://files.catbox.moe/q7o25y.mp3",
-    cover: "https://i.postimg.cc/dt30kD1C/9.jpg"
+    cover: "https://i.postimg.cc/TYFtZ4Fb/Beauty-Plus-20251002134530125-save.jpg"
   },
   {
     title: "Royalty",
     artist: "Temporal",
     file: "https://files.catbox.moe/9ok0rh.mp3",
-    cover: "https://i.postimg.cc/dt30kD1C/9.jpg"
+    cover: "https://i.postimg.cc/TYFtZ4Fb/Beauty-Plus-20251002134530125-save.jpg"
   },
 
 
