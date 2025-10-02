f482730d4626aaa5dd23d40438f606737b96746e
Apps <wallxnip@gmail.com>
2025-10-01 22:12:28 -0300

    Atualizar o album01.js

--- album01.js
+++ album01.js
@@ -1,8 +1,8 @@
-// Lista de músicas
+// Drilland
 const playlist2 = [
   {
     title: "MOOD",
-    artist: "Drilland (Melodic Drill Type Beat)",
+    artist: "Drilland",
     file: "https://dn721905.ca.archive.org/0/items/humor_20251001/MOOD.mp3",
     cover: "https://i.postimg.cc/JhDvxpmM/2.jpg"
   },
