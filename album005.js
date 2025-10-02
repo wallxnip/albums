41b7ec6a1773af940406b90dfb987713e69c58b2
Apps <wallxnip@gmail.com>
2025-10-02 12:29:44 -0300

    Atualizar o album005.js

--- album005.js
+++ album005.js
@@ -2,111 +2,105 @@
 const playlist504 = [
   {
     title: "Alegra",
-    artist: "",
+    artist: "Funk (MIX)",
     file: "https://files.catbox.moe/tg9gli.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/Y2fVdGcG/4.png"
   },
   {
     title: "Amanheceu",
-    artist: "",
+    artist: "Funk (MIX)",
     file: "https://files.catbox.moe/9xfgv9.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/Y2fVdGcG/4.png"
   },
   {
     title: "Aprendi Maldade Com Essa Maldita",
-    artist: "",
+    artist: "Funk (MIX)",
     file: "https://files.catbox.moe/1bvl6s.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/Y2fVdGcG/4.png"
   },
   {
     title: "Bela Dupla",
-    artist: "",
+    artist: "Funk (MIX)",
     file: "https://files.catbox.moe/a2lv8n.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/Y2fVdGcG/4.png"
   },
   {
     title: "É Que Eu Te Sarrei",
-    artist: "",
+    artist: "Funk (MIX)",
     file: "https://files.catbox.moe/kzwwdx.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/Y2fVdGcG/4.png"
   },
   {
     title: "Maldito Cupido",
-    artist: "",
+    artist: "Funk (MIX)",
     file: "https://files.catbox.moe/f5kizt.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/Y2fVdGcG/4.png"
   },
   {
     title: "Não Dá pra Negar",
-    artist: "",
+    artist: "Funk (MIX)",
     file: "https://files.catbox.moe/s8exg9.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/Y2fVdGcG/4.png"
   },
   {
     title: "O Que Que Tá Acontecendo",
-    artist: "",
+    artist: "Funk (MIX)",
     file: "https://files.catbox.moe/usjjy7.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/Y2fVdGcG/4.png"
   },
   {
     title: "Perdoa os Meus Pecados",
-    artist: "",
+    artist: "Funk (MIX)",
     file: "https://files.catbox.moe/ft4unw.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/Y2fVdGcG/4.png"
   },
   {
     title: "Plano Perfeito",
-    artist: "",
+    artist: "Funk (MIX)",
     file: "https://files.catbox.moe/t1za0g.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/Y2fVdGcG/4.png"
   },
   {
     title: "Postura",
-    artist: "",
+    artist: "Funk (MIX)",
     file: "https://files.catbox.moe/ezvvku.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/Y2fVdGcG/4.png"
   },
   {
     title: "Quadradinho",
-    artist: "",
+    artist: "Funk (MIX)",
     file: "https://files.catbox.moe/w6z0fw.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/Y2fVdGcG/4.png"
   },
   {
     title: "Se Tu Quer Replay",
-    artist: "",
+    artist: "Funk (MIX)",
     file: "https://files.catbox.moe/n87je8.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/Y2fVdGcG/4.png"
   },
   {
     title: "Segue o Baile",
-    artist: "",
+    artist: "Funk (MIX)",
     file: "https://files.catbox.moe/dg23ru.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/Y2fVdGcG/4.png"
   },
   {
     title: "Tatuagem da Borboleta",
-    artist: "",
+    artist: "Funk (MIX)",
     file: "https://files.catbox.moe/2sre6r.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/Y2fVdGcG/4.png"
   },
   {
     title: "Tava Escrito nas Estrelas",
-    artist: "",
+    artist: "Funk (MIX)",
     file: "https://files.catbox.moe/brqkwj.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/Y2fVdGcG/4.png"
   },
   {
     title: "Vou te Contar um Segredinho",
-    artist: "",
+    artist: "Funk (MIX)",
     file: "https://files.catbox.moe/26y95p.mp3",
-    cover: ""
-  },
-  {
-    title: "",
-    artist: "",
-    file: "",
-    cover: ""
+    cover: "https://i.postimg.cc/Y2fVdGcG/4.png"
   },
 
 
