e3e73483ef7e2191968ed6eb7ec96321a3a51842
Apps <wallxnip@gmail.com>
2025-10-05 05:31:05 -0300

    Criar album011.js

---
+++ album011.js
@@ -0,0 +1,90 @@
+// Robério e Seus Teclados
+const playlist = [
+  {
+    title: "Te Quero Baby",
+    artist: "Robério",
+    file: "https://files.catbox.moe/wmmdgf.mp3",
+    cover: "https://i.postimg.cc/xT8JDCKG/Rob-rio.jpg"
+  },
+  {
+    title: "Casa Comigo",
+    artist: "Robério",
+    file: "https://files.catbox.moe/s3839s.mp3",
+    cover: "https://i.postimg.cc/xT8JDCKG/Rob-rio.jpg"
+  },
+  {
+    title: "A Lua",
+    artist: "Robério",
+    file: "https://files.catbox.moe/cvvgj0.mp3",
+    cover: "https://i.postimg.cc/xT8JDCKG/Rob-rio.jpg"
+  },
+  {
+    title: "São Paulo Espera",
+    artist: "Robério",
+    file: "https://files.catbox.moe/xv79mi.mp3",
+    cover: "https://i.postimg.cc/xT8JDCKG/Rob-rio.jpg"
+  },
+  {
+    title: "Atraso na pensão",
+    artist: "Robério",
+    file: "https://files.catbox.moe/fmi9cq.mp3",
+    cover: "https://i.postimg.cc/xT8JDCKG/Rob-rio.jpg"
+  },
+  {
+    title: "Lá Se Vai",
+    artist: "Robério",
+    file: "https://files.catbox.moe/ln60j2.mp3",
+    cover: "https://i.postimg.cc/xT8JDCKG/Rob-rio.jpg"
+  },
+  {
+    title: "Oi",
+    artist: "Robério",
+    file: "https://files.catbox.moe/aof8kz.mp3",
+    cover: "https://i.postimg.cc/xT8JDCKG/Rob-rio.jpg"
+  },
+  {
+    title: "Capitão do Amor",
+    artist: "Robério",
+    file: "https://files.catbox.moe/eiewst.mp3",
+    cover: "https://i.postimg.cc/xT8JDCKG/Rob-rio.jpg"
+  },
+  {
+    title: "Alô no Ar",
+    artist: "Robério",
+    file: "https://files.catbox.moe/ne9wcs.mp3",
+    cover: "https://i.postimg.cc/xT8JDCKG/Rob-rio.jpg"
+  },
+  {
+    title: "Ela É Toda Boa",
+    artist: "Robério",
+    file: "https://files.catbox.moe/fwfhee.mp3",
+    cover: "https://i.postimg.cc/xT8JDCKG/Rob-rio.jpg"
+  },
+  {
+    title: "Não Vá (Pense Um Pouco Antes de Partir)",
+    artist: "Robério",
+    file: "https://files.catbox.moe/8ci9jh.mp3",
+    cover: "https://i.postimg.cc/xT8JDCKG/Rob-rio.jpg"
+  },
+  {
+    title: "Procurando Casamento",
+    artist: "Robério",
+    file: "https://files.catbox.moe/2rxwcr.mp3",
+    cover: "https://i.postimg.cc/xT8JDCKG/Rob-rio.jpg"
+  },
+  {
+    title: "Recadinho",
+    artist: "Robério",
+    file: "https://files.catbox.moe/2w0z42.mp3",
+    cover: "https://i.postimg.cc/xT8JDCKG/Rob-rio.jpg"
+  },
+  {
+    title: "Todo Atrapalhado",
+    artist: "Robério",
+    file: "https://files.catbox.moe/04bzoz.mp3",
+    cover: "https://i.postimg.cc/xT8JDCKG/Rob-rio.jpg"
+  },
+
+
+
+];
