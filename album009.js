58b56bec0f31de0374d3235677d300ab3bdbfbab
Apps <wallxnip@gmail.com>
2025-10-05 03:52:56 -0300

    Atualizar o album009.js

--- album009.js
+++ album009.js
@@ -1,1 +1,29 @@
-g
+const playlist508 = [
+  {
+    title: "Eu Quero Só Você",
+    artist: "Kiko Chicabana",
+    file: "https://files.catbox.moe/y16opc.mp3",
+    cover: "https://i.postimg.cc/d0f98rQq/Chicabana.jpg"
+  },
+  {
+    title: "Á Sua Maneira",
+    artist: "Kiko Chicabana",
+    file: "https://files.catbox.moe/jo9njo.mp3",
+    cover: "https://i.postimg.cc/d0f98rQq/Chicabana.jpg"
+  },
+  {
+    title: "Malandramente",
+    artist: "Kiko Chicabana",
+    file: "https://files.catbox.moe/u91rbt.mp3",
+    cover: "https://i.postimg.cc/d0f98rQq/Chicabana.jpg"
+  },
+  {
+    title: "Te Amo Sim",
+    artist: "Kiko Chicabana",
+    file: "https://files.catbox.moe/7ccaaj.mp3",
+    cover: "https://i.postimg.cc/d0f98rQq/Chicabana.jpg"
+  },
+
+
+
+];
