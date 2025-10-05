a0203301df45cbc7e5b5293292d69efb9f86e2db
Apps <wallxnip@gmail.com>
2025-10-05 04:02:08 -0300

    Criar album510.js

---
+++ album510.js
@@ -0,0 +1,24 @@
+// Sky Love do Forró
+const playlist509 = [
+  {
+    title: "Lua",
+    artist: "Sky love",
+    file: "https://files.catbox.moe/ghnevg.mp3",
+    cover: "https://i.postimg.cc/wBrkhLx9/Sou-Love.jpg"
+  },
+  {
+    title: "250",
+    artist: "Sky Love",
+    file: "https://files.catbox.moe/y01bl4.mp3",
+    cover: "https://i.postimg.cc/wBrkhLx9/Sou-Love.jpg"
+  },
+  {
+    title: "Luana",
+    artist: "Sky Love",
+    file: "https://files.catbox.moe/oguiu6.mp3",
+    cover: "https://i.postimg.cc/wBrkhLx9/Sou-Love.jpg"
+  },
+
+
+
+];
