4d333457c1aae1d4cd4fcdbc4fcb96e629e1a868
Apps <wallxnip@gmail.com>
2025-10-05 01:35:49 -0300

    Criar album008.js

---
+++ album008.js
@@ -0,0 +1,66 @@
+// Pisadinha do Vaqueiro
+const playlist507 = [
+  {
+    title: "Ela Kika Diferente",
+    artist: "Francildo Silva",
+    file: "https://files.catbox.moe/ro3awu.mp3",
+    cover: "https://i.postimg.cc/pXg3vxVc/Pisadinha-do-Vaqueiro.jpg"
+  },
+  {
+    title: "Tara Louca",
+    artist: "Francildo Silva",
+    file: "https://files.catbox.moe/kbavdx.mp3",
+    cover: "https://i.postimg.cc/pXg3vxVc/Pisadinha-do-Vaqueiro.jpg"
+  },
+  {
+    title: "Farra 100 Limite",
+    artist: "Francildo Silva",
+    file: "https://files.catbox.moe/87odja.mp3",
+    cover: "https://i.postimg.cc/pXg3vxVc/Pisadinha-do-Vaqueiro.jpg"
+  },
+  {
+    title: "Cavalo de um Milhão",
+    artist: "Francildo Silva",
+    file: "https://files.catbox.moe/5crl1g.mp3",
+    cover: "https://i.postimg.cc/pXg3vxVc/Pisadinha-do-Vaqueiro.jpg"
+  },
+  {
+    title: "Burguesinha",
+    artist: "Francildo Silva",
+    file: "https://files.catbox.moe/9rfcf7.mp3",
+    cover: "https://i.postimg.cc/pXg3vxVc/Pisadinha-do-Vaqueiro.jpg"
+  },
+  {
+    title: "Eu Nem Lembro",
+    artist: "Francildo Silva",
+    file: "https://files.catbox.moe/8hly4f.mp3",
+    cover: "https://i.postimg.cc/pXg3vxVc/Pisadinha-do-Vaqueiro.jpg"
+  },
+  {
+    title: "Eu Tenho É Gratidão",
+    artist: "Francildo Silva",
+    file: "https://files.catbox.moe/8pvqrn.mp3",
+    cover: "https://i.postimg.cc/pXg3vxVc/Pisadinha-do-Vaqueiro.jpg"
+  },
+  {
+    title: "Ronco Do Riacho",
+    artist: "Francildo Silva",
+    file: "https://files.catbox.moe/6e2adp.mp3",
+    cover: "https://i.postimg.cc/pXg3vxVc/Pisadinha-do-Vaqueiro.jpg"
+  },
+  {
+    title: "Aliança",
+    artist: "Francildo Silva",
+    file: "https://files.catbox.moe/vn2ams.mp3",
+    cover: "https://i.postimg.cc/pXg3vxVc/Pisadinha-do-Vaqueiro.jpg"
+  },
+  {
+    title: "Colecionador de Porquês",
+    artist: "Francildo Silva",
+    file: "https://files.catbox.moe/y3c6l2.mp3",
+    cover: "https://i.postimg.cc/pXg3vxVc/Pisadinha-do-Vaqueiro.jpg"
+  },
+
+
+
+];
