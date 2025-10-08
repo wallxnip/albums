e5ee4b79298c4de48a6b2e010a0acfdea3dc8fc0
Apps <wallxnip@gmail.com>
2025-10-08 09:49:22 -0300

    Atualizar o album008.js

--- album008.js
+++ album008.js
@@ -4,67 +4,67 @@ const playlist507 = [
     title: "Estilo Capa Loka",
     artist: "Francildo Silva",
     file: "https://hs7.krakenfiles.com/uploads/05-10-2025/QhvkCSe8q3/music.m4a",
-    cover: "https://i.postimg.cc/pXg3vxVc/Pisadinha-do-Vaqueiro.jpg"
+    cover: "https://i.postimg.cc/2SYbMkP4/Pisadinha-do-Vaqueiro.webp"
   },
   {
     title: "Ela Kika Diferente",
     artist: "Francildo Silva",
     file: "https://hs6.krakenfiles.com/uploads/05-10-2025/InW4RNIlip/music.m4a",
-    cover: "https://i.postimg.cc/pXg3vxVc/Pisadinha-do-Vaqueiro.jpg"
+    cover: "https://i.postimg.cc/2SYbMkP4/Pisadinha-do-Vaqueiro.webp"
   },
   {
     title: "Tara Louca",
     artist: "Francildo Silva",
     file: "https://hs4.krakenfiles.com/uploads/05-10-2025/A5IA0zumDc/music.m4a",
-    cover: "https://i.postimg.cc/pXg3vxVc/Pisadinha-do-Vaqueiro.jpg"
+    cover: "https://i.postimg.cc/2SYbMkP4/Pisadinha-do-Vaqueiro.webp"
   },
   {
     title: "Farra sem Limite",
     artist: "Francildo Silva",
     file: "https://hs5.krakenfiles.com/uploads/05-10-2025/XOnlA4T5NW/music.m4a",
-    cover: "https://i.postimg.cc/pXg3vxVc/Pisadinha-do-Vaqueiro.jpg"
+    cover: "https://i.postimg.cc/2SYbMkP4/Pisadinha-do-Vaqueiro.webp"
   },
   {
     title: "Cavalo de um Milhão",
     artist: "Francildo Silva",
     file: "https://hs6.krakenfiles.com/uploads/05-10-2025/q95OfZC33V/music.m4a",
-    cover: "https://i.postimg.cc/pXg3vxVc/Pisadinha-do-Vaqueiro.jpg"
+    cover: "https://i.postimg.cc/2SYbMkP4/Pisadinha-do-Vaqueiro.webp"
   },
   {
     title: "Burguesinha",
     artist: "Francildo Silva",
     file: "https://hs10.krakenfiles.com/uploads/05-10-2025/hNA7pvY91a/music.m4a",
-    cover: "https://i.postimg.cc/pXg3vxVc/Pisadinha-do-Vaqueiro.jpg"
+    cover: "https://i.postimg.cc/2SYbMkP4/Pisadinha-do-Vaqueiro.webp"
   },
   {
     title: "Eu Nem Lembro",
     artist: "Francildo Silva",
     file: "https://hs4.krakenfiles.com/uploads/05-10-2025/x5PqFeBBMN/music.m4a",
-    cover: "https://i.postimg.cc/pXg3vxVc/Pisadinha-do-Vaqueiro.jpg"
+    cover: "https://i.postimg.cc/2SYbMkP4/Pisadinha-do-Vaqueiro.webp"
   },
   {
     title: "Eu Tenho É Gratidão",
     artist: "Francildo Silva",
     file: "https://hs5.krakenfiles.com/uploads/05-10-2025/c4rQF0yHku/music.m4a",
-    cover: "https://i.postimg.cc/pXg3vxVc/Pisadinha-do-Vaqueiro.jpg"
+    cover: "https://i.postimg.cc/2SYbMkP4/Pisadinha-do-Vaqueiro.webp"
   },
   {
     title: "Ronco Do Riacho",
     artist: "Francildo Silva",
     file: "https://hs8.krakenfiles.com/uploads/05-10-2025/quG6bXYTUq/music.m4a",
-    cover: "https://i.postimg.cc/pXg3vxVc/Pisadinha-do-Vaqueiro.jpg"
+    cover: "https://i.postimg.cc/2SYbMkP4/Pisadinha-do-Vaqueiro.webp"
   },
   {
     title: "Aliança",
     artist: "Francildo Silva",
     file: "https://hs10.krakenfiles.com/uploads/05-10-2025/DeWigyhCmL/music.m4a",
-    cover: "https://i.postimg.cc/pXg3vxVc/Pisadinha-do-Vaqueiro.jpg"
+    cover: "https://i.postimg.cc/2SYbMkP4/Pisadinha-do-Vaqueiro.webp"
   },
   {
     title: "Colecionador de Porquês",
     artist: "Francildo Silva",
     file: "https://hs10.krakenfiles.com/uploads/05-10-2025/1uMdOloCqt/music.m4a",
-    cover: "https://i.postimg.cc/pXg3vxVc/Pisadinha-do-Vaqueiro.jpg"
+    cover: "https://i.postimg.cc/2SYbMkP4/Pisadinha-do-Vaqueiro.webp"
   },
 
 
