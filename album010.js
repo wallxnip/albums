be9eed368cc429203959cc00b50955514f80e480
Apps <wallxnip@gmail.com>
2025-10-08 09:45:47 -0300

    Atualizar o album010.js

--- album010.js
+++ album010.js
@@ -4,19 +4,19 @@ const playlist509 = [
     title: "Lua",
     artist: "Sky love",
     file: "https://hs4.krakenfiles.com/uploads/05-10-2025/QxbReUgV4R/music.m4a",
-    cover: "https://i.postimg.cc/wBrkhLx9/Sou-Love.jpg"
+    cover: "https://i.postimg.cc/mg4BBtLK/Sou-Love.webp"
   },
   {
     title: "250",
     artist: "Sky Love",
     file: "https://hs3.krakenfiles.com/uploads/05-10-2025/chdcxzGe3k/music.m4a",
-    cover: "https://i.postimg.cc/wBrkhLx9/Sou-Love.jpg"
+    cover: "https://i.postimg.cc/mg4BBtLK/Sou-Love.webp"
   },
   {
     title: "Luana",
     artist: "Sky Love",
     file: "https://hs4.krakenfiles.com/uploads/05-10-2025/04vqF7OEE3/music.m4a",
-    cover: "https://i.postimg.cc/wBrkhLx9/Sou-Love.jpg"
+    cover: "https://i.postimg.cc/mg4BBtLK/Sou-Love.webp"
   },
 
 
