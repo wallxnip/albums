7562677efd33e9aec47624a46dc497d741a67f1b
Apps <wallxnip@gmail.com>
2025-10-08 08:31:53 -0300

    Atualizar o album001.js

--- album001.js
+++ album001.js
@@ -4,69 +4,69 @@ const playlist500 = [
     title: "MOOD",
     artist: "Drilland",
     file: "https://hs7.krakenfiles.com/uploads/05-10-2025/Dxg5Mc70JY/music.m4a",
-    cover: "https://i.postimg.cc/JhDvxpmM/2.jpg"
+    cover: "https://i.postimg.cc/85J3d5YQ/Drilland.webp"
   },
   {
     title: "CRY",
     artist: "Drilland",
     file: "https://hs4.krakenfiles.com/uploads/05-10-2025/DANXTjlgIP/music.m4a",
-    cover: "https://i.postimg.cc/JhDvxpmM/2.jpg"
+    cover: "https://i.postimg.cc/85J3d5YQ/Drilland.webp"
   },
   {
     title: "LOVE V",
     artist: "Drilland",
     file: "https://hs5.krakenfiles.com/uploads/05-10-2025/o7lqqAh3RT/music.m4a",
-    cover: "https://i.postimg.cc/JhDvxpmM/2.jpg"
+    cover: "https://i.postimg.cc/85J3d5YQ/Drilland.webp"
   },
   {
     title: "WALK",
     artist: "Drilland",
     file: "https://hs7.krakenfiles.com/uploads/05-10-2025/GXJunRFBaj/music.m4a",
-    cover: "https://i.postimg.cc/JhDvxpmM/2.jpg"
+    cover: "https://i.postimg.cc/85J3d5YQ/Drilland.webp"
   },
   {
     title: "LOST",
     artist: "Drilland",
     file: "https://hs8.krakenfiles.com/uploads/05-10-2025/59SVNKPRBH/music.m4a",
-    cover: "https://i.postimg.cc/JhDvxpmM/2.jpg"
+    cover: "https://i.postimg.cc/85J3d5YQ/Drilland.webp"
   },
   
   {
     title: "LOCKED",
     artist: "Drilland",
     file: "https://hs1.krakenfiles.com/uploads/05-10-2025/d1MfOZGDPP/music.m4a",
-    cover: "https://i.postimg.cc/JhDvxpmM/2.jpg"
+    cover: "https://i.postimg.cc/85J3d5YQ/Drilland.webp"
   },
   {
     title: "HOLD",
     artist: "Drilland",
     file: "https://hs10.krakenfiles.com/uploads/05-10-2025/yv4Y3IFfZ8/music.m4a",
-    cover: "https://i.postimg.cc/JhDvxpmM/2.jpg"
+    cover: "https://i.postimg.cc/85J3d5YQ/Drilland.webp"
   },
   {
     title: "LOST II",
     artist: "Drilland",
     file: "https://hs7.krakenfiles.com/uploads/05-10-2025/xHaNPeFXa9/music.m4a",
-    cover: "https://i.postimg.cc/JhDvxpmM/2.jpg"
+    cover: "https://i.postimg.cc/85J3d5YQ/Drilland.webp"
   },
   {
     title: "OLDER",
     artist: "Drilland",
     file: "https://hs8.krakenfiles.com/uploads/05-10-2025/D2G7Ccfz6h/music.m4a",
-    cover: "https://i.postimg.cc/JhDvxpmM/2.jpg"
+    cover: "https://i.postimg.cc/85J3d5YQ/Drilland.webp"
   },
 
   {
     title: "SAY",
     artist: "Drilland",
     file: "https://hs4.krakenfiles.com/uploads/05-10-2025/RRSTk6qRkY/music.m4a",
-    cover: "https://i.postimg.cc/JhDvxpmM/2.jpg"
+    cover: "https://i.postimg.cc/85J3d5YQ/Drilland.webp"
   },
   {
     title: "SMILE",
     artist: "Drilland",
     file: "https://hs9.krakenfiles.com/uploads/05-10-2025/eieoCm7AI7/music.m4a",
-    cover: "https://i.postimg.cc/JhDvxpmM/2.jpg"
+    cover: "https://i.postimg.cc/85J3d5YQ/Drilland.webp"
   },
 
 
