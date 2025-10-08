c2408dcfb1cd383aafb4cb73f3096f7dd5a59c95
Apps <wallxnip@gmail.com>
2025-10-08 08:49:48 -0300

    Atualizar o album005.js

--- album005.js
+++ album005.js
@@ -4,103 +4,103 @@ const playlist504 = [
     title: "Alegra",
     artist: "Funk (MIX)",
     file: "https://hs3.krakenfiles.com/uploads/05-10-2025/aDn5Pq6qtG/music.m4a",
-    cover: "https://i.postimg.cc/Y2fVdGcG/4.png"
+    cover: "https://i.postimg.cc/139sPG8G/Funk.webp"
   },
   {
     title: "Amanheceu",
     artist: "Funk (MIX)",
     file: "https://hs2.krakenfiles.com/uploads/05-10-2025/sspheqELIt/music.m4a",
-    cover: "https://i.postimg.cc/Y2fVdGcG/4.png"
+    cover: "https://i.postimg.cc/139sPG8G/Funk.webp"
   },
   {
     title: "Aprendi Maldade Com Essa Maldita",
     artist: "Funk (MIX)",
     file: "https://hs5.krakenfiles.com/uploads/05-10-2025/RsddNVWrQR/music.m4a",
-    cover: "https://i.postimg.cc/Y2fVdGcG/4.png"
+    cover: "https://i.postimg.cc/139sPG8G/Funk.webp"
   },
   {
     title: "Bela Dupla",
     artist: "Funk (MIX)",
     file: "https://hs7.krakenfiles.com/uploads/05-10-2025/41V1meZYYK/music.m4a",
-    cover: "https://i.postimg.cc/Y2fVdGcG/4.png"
+    cover: "https://i.postimg.cc/139sPG8G/Funk.webp"
   },
   {
     title: "É Que Eu Te Sarrei",
     artist: "Funk (MIX)",
     file: "https://hs7.krakenfiles.com/uploads/05-10-2025/JsAH7R8fKJ/music.m4a",
-    cover: "https://i.postimg.cc/Y2fVdGcG/4.png"
+    cover: "https://i.postimg.cc/139sPG8G/Funk.webp"
   },
   {
     title: "Maldito Cupido",
     artist: "Funk (MIX)",
     file: "https://hs6.krakenfiles.com/uploads/05-10-2025/etNfyFkA9C/music.m4a",
-    cover: "https://i.postimg.cc/Y2fVdGcG/4.png"
+    cover: "https://i.postimg.cc/139sPG8G/Funk.webp"
   },
   {
     title: "Não Dá pra Negar",
     artist: "Funk (MIX)",
     file: "https://hs9.krakenfiles.com/uploads/05-10-2025/D8CrnjFbNJ/music.m4a",
-    cover: "https://i.postimg.cc/Y2fVdGcG/4.png"
+    cover: "https://i.postimg.cc/139sPG8G/Funk.webp"
   },
   {
     title: "O Que Que Tá Acontecendo",
     artist: "Funk (MIX)",
     file: "https://hs1.krakenfiles.com/uploads/05-10-2025/2qbFexcAGL/music.m4a",
-    cover: "https://i.postimg.cc/Y2fVdGcG/4.png"
+    cover: "https://i.postimg.cc/139sPG8G/Funk.webp"
   },
   {
     title: "Perdoa os Meus Pecados",
     artist: "Funk (MIX)",
     file: "https://hs1.krakenfiles.com/uploads/05-10-2025/F5V1l1Bg6w/music.m4a",
-    cover: "https://i.postimg.cc/Y2fVdGcG/4.png"
+    cover: "https://i.postimg.cc/139sPG8G/Funk.webp"
   },
   {
     title: "Plano Perfeito",
     artist: "Funk (MIX)",
     file: "https://hs7.krakenfiles.com/uploads/05-10-2025/YOmZUNgL67/music.m4a",
-    cover: "https://i.postimg.cc/Y2fVdGcG/4.png"
+    cover: "https://i.postimg.cc/139sPG8G/Funk.webp"
   },
   {
     title: "Postura",
     artist: "Funk (MIX)",
     file: "https://hs7.krakenfiles.com/uploads/05-10-2025/72pZ95Sb98/music.m4a",
-    cover: "https://i.postimg.cc/Y2fVdGcG/4.png"
+    cover: "https://i.postimg.cc/139sPG8G/Funk.webp"
   },
   {
     title: "Quadradinho",
     artist: "Funk (MIX)",
     file: "https://hs6.krakenfiles.com/uploads/05-10-2025/zRZhViZs6q/music.m4a",
-    cover: "https://i.postimg.cc/Y2fVdGcG/4.png"
+    cover: "https://i.postimg.cc/139sPG8G/Funk.webp"
   },
   {
     title: "Se Tu Quer Replay",
     artist: "Funk (MIX)",
     file: "https://hs5.krakenfiles.com/uploads/05-10-2025/HQUfWSpIEE/music.m4a",
-    cover: "https://i.postimg.cc/Y2fVdGcG/4.png"
+    cover: "https://i.postimg.cc/139sPG8G/Funk.webp"
   },
   {
     title: "Segue o Baile",
     artist: "Funk (MIX)",
     file: "https://hs10.krakenfiles.com/uploads/05-10-2025/pcUDgVtjcd/music.m4a",
-    cover: "https://i.postimg.cc/Y2fVdGcG/4.png"
+    cover: "https://i.postimg.cc/139sPG8G/Funk.webp"
   },
   {
     title: "Tatuagem da Borboleta",
     artist: "Funk (MIX)",
     file: "https://hs7.krakenfiles.com/uploads/05-10-2025/bwOgi4wiH3/music.m4a",
-    cover: "https://i.postimg.cc/Y2fVdGcG/4.png"
+    cover: "https://i.postimg.cc/139sPG8G/Funk.webp"
   },
   {
     title: "Tava Escrito nas Estrelas",
     artist: "Funk (MIX)",
     file: "https://hs10.krakenfiles.com/uploads/05-10-2025/XpdF3MsxUL/music.m4a",
-    cover: "https://i.postimg.cc/Y2fVdGcG/4.png"
+    cover: "https://i.postimg.cc/139sPG8G/Funk.webp"
   },
   {
     title: "Vou te Contar um Segredinho",
     artist: "Funk (MIX)",
     file: "https://hs10.krakenfiles.com/uploads/05-10-2025/HCbccjjg4D/music.m4a",
-    cover: "https://i.postimg.cc/Y2fVdGcG/4.png"
+    cover: "https://i.postimg.cc/139sPG8G/Funk.webp"
   },
 
 
