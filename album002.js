90196ef4037df24848c96bf0843f9b891b4e1a97
Apps <wallxnip@gmail.com>
2025-10-08 09:09:08 -0300

    Atualizar o album002.js

--- album002.js
+++ album002.js
@@ -4,98 +4,98 @@ const playlist501 = [
     title: "So Good",
     artist: "Summer (MIX)",
     file: "https://hs3.krakenfiles.com/uploads/05-10-2025/CanWdRZqGX/music.m4a",
-    cover: "https://i.postimg.cc/4x4X66bB/Summer.webp"
+    cover: "https://i.postimg.cc/J44t5H8J/Summer.webp"
   },
   
   {
     title: "Hight Again",
     artist: "Summer (MIX)",
     file: "https://hs3.krakenfiles.com/uploads/05-10-2025/hsvKNmEDdW/music.m4a",
-    cover: "https://i.postimg.cc/4x4X66bB/Summer.webp"
+    cover: "https://i.postimg.cc/J44t5H8J/Summer.webp"
   },
 
   {
     title: "Good Feeling",
     artist: "Summer (MIX)",
     file: "https://hs8.krakenfiles.com/uploads/05-10-2025/pHVpdK8LDY/music.m4a",
-    cover: "https://i.postimg.cc/4x4X66bB/Summer.webp"
+    cover: "https://i.postimg.cc/J44t5H8J/Summer.webp"
   },
 
   {
     title: "Girls in Luv",
     artist: "Summer (MIX)",
     file: "https://hs8.krakenfiles.com/uploads/05-10-2025/UbmAFxDLve/music.m4a",
-    cover: "https://i.postimg.cc/4x4X66bB/Summer.webp"
+    cover: "https://i.postimg.cc/J44t5H8J/Summer.webp"
   },
 
   {
     title: "Summer",
     artist: "Summer (MIX)",
     file: "https://hs5.krakenfiles.com/uploads/05-10-2025/6uQ11wwWLN/music.m4a",
-    cover: "https://i.postimg.cc/4x4X66bB/Summer.webp"
+    cover: "https://i.postimg.cc/J44t5H8J/Summer.webp"
   },
 
   {
     title: "Fall In Love",
     artist: "Summer (MIX)",
     file: "https://hs8.krakenfiles.com/uploads/05-10-2025/6wAEHXGCDC/music.m4a",
-    cover: "https://i.postimg.cc/4x4X66bB/Summer.webp"
+    cover: "https://i.postimg.cc/J44t5H8J/Summer.webp"
   },
 
   {
     title: "Deserto Rain",
     artist: "Summer (MIX)",
     file: "https://hs3.krakenfiles.com/uploads/05-10-2025/ozqJrjBHms/music.m4a",
-    cover: "https://i.postimg.cc/4x4X66bB/Summer.webp"
+    cover: "https://i.postimg.cc/J44t5H8J/Summer.webp"
   },
 
   {
     title: "Alive",
     artist: "Summer (MIX)",
     file: "https://hs1.krakenfiles.com/uploads/05-10-2025/yjcwUl0YZD/music.m4a",
-    cover: "https://i.postimg.cc/4x4X66bB/Summer.webp"
+    cover: "https://i.postimg.cc/J44t5H8J/Summer.webp"
   },
 
   {
     title: "Holiday",
     artist: "Summer (MIX)",
     file: "https://hs8.krakenfiles.com/uploads/05-10-2025/eyL16DwZWM/music.m4a",
-    cover: "https://i.postimg.cc/4x4X66bB/Summer.webp"
+    cover: "https://i.postimg.cc/J44t5H8J/Summer.webp"
   },
 
   {
     title: "Infinity",
     artist: "Summer (MIX)",
     file: "https://hs8.krakenfiles.com/uploads/05-10-2025/xImc6K9r0A/music.m4a",
-    cover: "https://i.postimg.cc/4x4X66bB/Summer.webp"
+    cover: "https://i.postimg.cc/J44t5H8J/Summer.webp"
   },
 
   {
     title: "Love Again",
     artist: "Summer (MIX)",
     file: "https://hs1.krakenfiles.com/uploads/05-10-2025/aCP2sFU8nb/music.m4a",
-    cover: "https://i.postimg.cc/4x4X66bB/Summer.webp"
+    cover: "https://i.postimg.cc/J44t5H8J/Summer.webp"
   },
 
   {
     title: "Play Hard",
     artist: "Summer (MIX)",
     file: "https://hs10.krakenfiles.com/uploads/05-10-2025/Nf2LaD8cXh/music.m4a",
-    cover: "https://i.postimg.cc/4x4X66bB/Summer.webp"
+    cover: "https://i.postimg.cc/J44t5H8J/Summer.webp"
   },
 
   {
     title: "Player In C",
     artist: "Summer (MIX)",
     file: "https://hs7.krakenfiles.com/uploads/05-10-2025/60m9A4e4hv/music.m4a",
-    cover: "https://i.postimg.cc/4x4X66bB/Summer.webp"
+    cover: "https://i.postimg.cc/J44t5H8J/Summer.webp"
   },
 
   {
     title: "Stereo Love",
     artist: "Summer (MIX)",
     file: "https://hs10.krakenfiles.com/uploads/05-10-2025/IJvnq8J3QE/music.m4a",
-    cover: "https://i.postimg.cc/4x4X66bB/Summer.webp"
+    cover: "https://i.postimg.cc/J44t5H8J/Summer.webp"
   },
 
 
