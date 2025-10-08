77b37bcb8820492a517735d4cb808f54ae6e90da
Apps <wallxnip@gmail.com>
2025-10-08 08:52:09 -0300

    Atualizar o album006.js

--- album006.js
+++ album006.js
@@ -4,265 +4,265 @@ const playlist505 = [
     title: "Ma Chérie",
     artist: "Alone (EDM)",
     file: "https://hs3.krakenfiles.com/uploads/05-10-2025/FEGMPJNEIj/music.m4a",
-    cover: "https://i.postimg.cc/NFYngYXj/1.png"
+    cover: "https://i.postimg.cc/BbKQLJ3X/Alone.webp"
   },
   {
     title: "Das Geht Boom",
     artist: "Alone (EDM)",
     file: "https://hs4.krakenfiles.com/uploads/05-10-2025/GQ7R8jJmlD/music.m4a",
-    cover: "https://i.postimg.cc/NFYngYXj/1.png"
+    cover: "https://i.postimg.cc/BbKQLJ3X/Alone.webp"
   },
   {
     title: "Kiss Me",
     artist: "Alone (EDM)",
     file: "https://hs10.krakenfiles.com/uploads/05-10-2025/Fuy5nx88t6/music.m4a",
-    cover: "https://i.postimg.cc/NFYngYXj/1.png"
+    cover: "https://i.postimg.cc/BbKQLJ3X/Alone.webp"
   },
   {
     title: "Tired",
     artist: "Alone (EDM)",
     file: "https://hs6.krakenfiles.com/uploads/05-10-2025/TzmO4FjL95/music.m4a",
-    cover: "https://i.postimg.cc/NFYngYXj/1.png"
+    cover: "https://i.postimg.cc/BbKQLJ3X/Alone.webp"
   },
   {
     title: "One More Night",
     artist: "Alone (EDM)",
     file: "https://hs7.krakenfiles.com/uploads/05-10-2025/NNlTPsDvrz/music.m4a",
-    cover: "https://i.postimg.cc/NFYngYXj/1.png"
+    cover: "https://i.postimg.cc/BbKQLJ3X/Alone.webp"
   },
   {
     title: "Whistle",
     artist: "Alone (EDM)",
     file: "https://hs4.krakenfiles.com/uploads/05-10-2025/lSDVQMGcuP/music.m4a",
-    cover: "https://i.postimg.cc/NFYngYXj/1.png"
+    cover: "https://i.postimg.cc/BbKQLJ3X/Alone.webp"
   },
   {
     title: "Good Feeling",
     artist: "Alone (EDM)",
     file: "https://hs1.krakenfiles.com/uploads/05-10-2025/KfLH13MTQu/music.m4a",
-    cover: "https://i.postimg.cc/NFYngYXj/1.png"
+    cover: "https://i.postimg.cc/BbKQLJ3X/Alone.webp"
   },
   {
     title: "From Up Above",
     artist: "Alone (EDM)",
     file: "https://hs10.krakenfiles.com/uploads/05-10-2025/1BXy7G7tov/music.m4a",
-    cover: "https://i.postimg.cc/NFYngYXj/1.png"
+    cover: "https://i.postimg.cc/BbKQLJ3X/Alone.webp"
   },
   {
     title: "Sorry",
     artist: "Alone (EDM)",
     file: "https://hs6.krakenfiles.com/uploads/05-10-2025/jB3ApIBCEo/music.m4a",
-    cover: "https://i.postimg.cc/NFYngYXj/1.png"
+    cover: "https://i.postimg.cc/BbKQLJ3X/Alone.webp"
   },
   {
     title: "Ghost",
     artist: "Alone (EDM)",
     file: "https://hs5.krakenfiles.com/uploads/05-10-2025/74LLkJm7gh/music.m4a",
-    cover: "https://i.postimg.cc/NFYngYXj/1.png"
+    cover: "https://i.postimg.cc/BbKQLJ3X/Alone.webp"
   },
   {
     title: "What Do You Mean",
     artist: "Alone (EDM)",
     file: "https://hs5.krakenfiles.com/uploads/05-10-2025/OiVkm84i5R/music.m4a",
-    cover: "https://i.postimg.cc/NFYngYXj/1.png"
+    cover: "https://i.postimg.cc/BbKQLJ3X/Alone.webp"
   },
   {
     title: "Baby",
     artist: "Alone (EDM)",
     file: "https://hs3.krakenfiles.com/uploads/05-10-2025/FqgvTUKRPx/music.m4a",
-    cover: "https://i.postimg.cc/NFYngYXj/1.png"
+    cover: "https://i.postimg.cc/BbKQLJ3X/Alone.webp"
   },
   {
     title: "Eenie Meenie",
     artist: "Alone (EDM)",
     file: "https://hs6.krakenfiles.com/uploads/05-10-2025/i5j0ztz8K5/music.m4a",
-    cover: "https://i.postimg.cc/NFYngYXj/1.png"
+    cover: "https://i.postimg.cc/BbKQLJ3X/Alone.webp"
   },
   {
     title: "What the Hell",
     artist: "Alone (EDM)",
     file: "https://hs6.krakenfiles.com/uploads/05-10-2025/ZrSrcb1MGJ/music.m4a",
-    cover: "https://i.postimg.cc/NFYngYXj/1.png"
+    cover: "https://i.postimg.cc/BbKQLJ3X/Alone.webp"
   },
   {
     title: "Call Me Maybe",
     artist: "Alone (EDM)",
     file: "https://hs1.krakenfiles.com/uploads/05-10-2025/WIuekRN9sW/music.m4a",
-    cover: "https://i.postimg.cc/NFYngYXj/1.png"
+    cover: "https://i.postimg.cc/BbKQLJ3X/Alone.webp"
   },
   {
     title: "Good Time",
     artist: "Alone (EDM)",
     file: "https://hs9.krakenfiles.com/uploads/05-10-2025/JvqwFwj8Wy/music.m4a",
-    cover: "https://i.postimg.cc/NFYngYXj/1.png"
+    cover: "https://i.postimg.cc/BbKQLJ3X/Alone.webp"
   },
   {
     title: "Get Back",
     artist: "Alone (EDM)",
     file: "https://hs3.krakenfiles.com/uploads/05-10-2025/0cgdnEFYDH/music.m4a",
-    cover: "https://i.postimg.cc/NFYngYXj/1.png"
+    cover: "https://i.postimg.cc/BbKQLJ3X/Alone.webp"
   },
   {
     title: "Million",
     artist: "Alone (EDM)",
     file: "https://hs6.krakenfiles.com/uploads/05-10-2025/c4e3xY8bz0/music.m4a",
-    cover: "https://i.postimg.cc/NFYngYXj/1.png"
+    cover: "https://i.postimg.cc/BbKQLJ3X/Alone.webp"
   },
   {
     title: "Taste",
     artist: "Alone (EDM)",
     file: "https://hs3.krakenfiles.com/uploads/05-10-2025/J1mYghkIHT/music.m4a",
-    cover: "https://i.postimg.cc/NFYngYXj/1.png"
+    cover: "https://i.postimg.cc/BbKQLJ3X/Alone.webp"
   },
   {
     title: "Someone To You",
     artist: "Alone (EDM)",
     file: "https://hs5.krakenfiles.com/uploads/05-10-2025/BNq6qykigG/music.m4a",
-    cover: "https://i.postimg.cc/NFYngYXj/1.png"
+    cover: "https://i.postimg.cc/BbKQLJ3X/Alone.webp"
   },
   {
     title: "Waka Waka",
     artist: "Alone (EDM)",
     file: "https://hs9.krakenfiles.com/uploads/05-10-2025/DgTEfpVaWA/music.m4a",
-    cover: "https://i.postimg.cc/NFYngYXj/1.png"
+    cover: "https://i.postimg.cc/BbKQLJ3X/Alone.webp"
   },
   {
     title: "All My People",
     artist: "Alone (EDM)",
     file: "https://hs8.krakenfiles.com/uploads/05-10-2025/7gPRp162F0/music.m4a",
-    cover: "https://i.postimg.cc/NFYngYXj/1.png"
+    cover: "https://i.postimg.cc/BbKQLJ3X/Alone.webp"
   },
   {
     title: "Blinding Lights",
     artist: "Alone (EDM)",
     file: "https://hs5.krakenfiles.com/uploads/05-10-2025/f1G8tc1eb6/music.m4a",
-    cover: "https://i.postimg.cc/NFYngYXj/1.png"
+    cover: "https://i.postimg.cc/BbKQLJ3X/Alone.webp"
   },
   {
     title: "Fiesta",
     artist: "Alone (EDM)",
     file: "https://hs7.krakenfiles.com/uploads/05-10-2025/kBrx2RXOoF/music.m4a",
-    cover: "https://i.postimg.cc/NFYngYXj/1.png"
+    cover: "https://i.postimg.cc/BbKQLJ3X/Alone.webp"
   },
   {
     title: "Forever",
     artist: "Alone (EDM)",
     file: "https://hs3.krakenfiles.com/uploads/05-10-2025/U7MUgfQJiq/music.m4a",
-    cover: "https://i.postimg.cc/NFYngYXj/1.png"
+    cover: "https://i.postimg.cc/BbKQLJ3X/Alone.webp"
   },
   {
     title: "Girl Like Me",
     artist: "Alone (EDM)",
     file: "https://hs3.krakenfiles.com/uploads/05-10-2025/eSWvLFcjZV/music.m4a",
-    cover: "https://i.postimg.cc/NFYngYXj/1.png"
+    cover: "https://i.postimg.cc/BbKQLJ3X/Alone.webp"
   },
   {
     title: "Greedy",
     artist: "Alone (EDM)",
     file: "https://hs5.krakenfiles.com/uploads/05-10-2025/41FWKurXOv/music.m4a",
-    cover: "https://i.postimg.cc/NFYngYXj/1.png"
+    cover: "https://i.postimg.cc/BbKQLJ3X/Alone.webp"
   },
   {
     title: "Gypsy",
     artist: "Alone (EDM)",
     file: "https://hs7.krakenfiles.com/uploads/05-10-2025/7M6Sx1KvM5/music.m4a",
-    cover: "https://i.postimg.cc/NFYngYXj/1.png"
+    cover: "https://i.postimg.cc/BbKQLJ3X/Alone.webp"
   },
   {
     title: "Hangover",
     artist: "Alone (EDM)",
     file: "https://hs9.krakenfiles.com/uploads/05-10-2025/k89kU2qm3L/music.m4a",
-    cover: "https://i.postimg.cc/NFYngYXj/1.png"
+    cover: "https://i.postimg.cc/BbKQLJ3X/Alone.webp"
   },
   {
     title: "Hotel Room Service",
     artist: "Alone (EDM)",
     file: "https://hs5.krakenfiles.com/uploads/05-10-2025/JwPqAwhLi3/music.m4a",
-    cover: "https://i.postimg.cc/NFYngYXj/1.png"
+    cover: "https://i.postimg.cc/BbKQLJ3X/Alone.webp"
   },
   {
     title: "I Know You Want Me",
     artist: "Alone (EDM)",
     file: "https://hs2.krakenfiles.com/uploads/05-10-2025/YrYL7MAkc2/music.m4a",
-    cover: "https://i.postimg.cc/NFYngYXj/1.png"
+    cover: "https://i.postimg.cc/BbKQLJ3X/Alone.webp"
   },
   {
     title: "I Really Like You",
     artist: "Alone (EDM)",
     file: "https://hs6.krakenfiles.com/uploads/05-10-2025/39QdASWYMS/music.m4a",
-    cover: "https://i.postimg.cc/NFYngYXj/1.png"
+    cover: "https://i.postimg.cc/BbKQLJ3X/Alone.webp"
   },
   {
     title: "If You Believe",
     artist: "Alone (EDM)",
     file: "https://hs4.krakenfiles.com/uploads/05-10-2025/iOzDNkERKO/music.m4a",
-    cover: "https://i.postimg.cc/NFYngYXj/1.png"
+    cover: "https://i.postimg.cc/BbKQLJ3X/Alone.webp"
   },
   {
     title: "International Love",
     artist: "Alone (EDM)",
     file: "https://hs2.krakenfiles.com/uploads/05-10-2025/mnUin476BC/music.m4a",
-    cover: "https://i.postimg.cc/NFYngYXj/1.png"
+    cover: "https://i.postimg.cc/BbKQLJ3X/Alone.webp"
   },
   {
     title: "Kiss You",
     artist: "Alone (EDM)",
     file: "8bkr3c.mp3",
-    cover: "https://i.postimg.cc/NFYngYXj/1.png"
+    cover: "https://i.postimg.cc/BbKQLJ3X/Alone.webp"
   },
   {
     title: "Live Your Life",
     artist: "Alone (EDM)",
     file: "https://hs5.krakenfiles.com/uploads/05-10-2025/6L1iUXwNWQ/music.m4a",
-    cover: "https://i.postimg.cc/NFYngYXj/1.png"
+    cover: "https://i.postimg.cc/BbKQLJ3X/Alone.webp"
   },
   {
     title: "Love Me Like You Do",
     artist: "Alone (EDM)",
     file: "https://hs9.krakenfiles.com/uploads/05-10-2025/xfNtFBZD0z/music.m4a",
-    cover: "https://i.postimg.cc/NFYngYXj/1.png"
+    cover: "https://i.postimg.cc/BbKQLJ3X/Alone.webp"
   },
   {
     title: "On The Floor",
     artist: "Alone (EDM)",
     file: "https://hs8.krakenfiles.com/uploads/05-10-2025/3QibjQocPs/music.m4a",
-    cover: "https://i.postimg.cc/NFYngYXj/1.png"
+    cover: "https://i.postimg.cc/BbKQLJ3X/Alone.webp"
   },
   {
     title: "Remember to Forget",
     artist: "Alone (EDM)",
     file: "https://hs3.krakenfiles.com/uploads/05-10-2025/lKJWT2Dlbn/music.m4a",
-    cover: "https://i.postimg.cc/NFYngYXj/1.png"
+    cover: "https://i.postimg.cc/BbKQLJ3X/Alone.webp"
   },
   {
     title: "Run Away With Me.",
     artist: "Alone (EDM)",
     file: "https://hs7.krakenfiles.com/uploads/05-10-2025/IyHWk9CX7C/music.m4a",
-    cover: "https://i.postimg.cc/NFYngYXj/1.png"
+    cover: "https://i.postimg.cc/BbKQLJ3X/Alone.webp"
   },
   {
     title: "Serve the Lord",
     artist: "Alone (EDM)",
     file: "https://hs8.krakenfiles.com/uploads/05-10-2025/LXKfMT28Yv/music.m4a",
-    cover: "https://i.postimg.cc/NFYngYXj/1.png"
+    cover: "https://i.postimg.cc/BbKQLJ3X/Alone.webp"
   },
   {
     title: "Suave",
     artist: "Alone (EDM)",
     file: "https://hs3.krakenfiles.com/uploads/05-10-2025/QCqIz9yDoh/music.m4a",
-    cover: "https://i.postimg.cc/NFYngYXj/1.png"
+    cover: "https://i.postimg.cc/BbKQLJ3X/Alone.webp"
   },
   {
     title: "Take Your Time",
     artist: "Alone (EDM)",
     file: "https://hs4.krakenfiles.com/uploads/05-10-2025/Zl6DTnzETJ/music.m4a",
-    cover: "https://i.postimg.cc/NFYngYXj/1.png"
+    cover: "https://i.postimg.cc/BbKQLJ3X/Alone.webp"
   },
   {
     title: "Tusa",
     artist: "Alone (EDM)",
     file: "https://hs10.krakenfiles.com/uploads/05-10-2025/UTqmvC4Wzs/music.m4a",
-    cover: "https://i.postimg.cc/NFYngYXj/1.png"
+    cover: "https://i.postimg.cc/BbKQLJ3X/Alone.webp"
   },
 
 
