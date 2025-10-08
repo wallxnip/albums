13abf3ae49459c480d5f1896cb3051194f98dc57
Apps <wallxnip@gmail.com>
2025-10-08 09:34:59 -0300

    Atualizar o album007.js

--- album007.js
+++ album007.js
@@ -1,40 +1,40 @@
-// Lista de músicas
+// Temporal
 const playlist506 = [
   {
     title: "Another Love",
     artist: "Temporal",
     file: "https://hs4.krakenfiles.com/uploads/05-10-2025/EhTzWnotHS/music.m4a",
-    cover: "https://i.postimg.cc/xd2pNQrC/Temporal.webp"
+    cover: "https://i.postimg.cc/25Yrw3LF/Temporal.webp"
   },
   {
     title: "Apocalypse",
     artist: "Temporal",
     file: "https://hs5.krakenfiles.com/uploads/05-10-2025/RVYnPDnGbS/music.m4a",
-    cover: "https://i.postimg.cc/xd2pNQrC/Temporal.webp"
+    cover: "https://i.postimg.cc/25Yrw3LF/Temporal.webp"
   },
   {
     title: "Dynasty",
     artist: "Temporal",
     file: "https://hs7.krakenfiles.com/uploads/05-10-2025/tozBloXZHa/music.m4a",
-    cover: "https://i.postimg.cc/xd2pNQrC/Temporal.webp"
+    cover: "https://i.postimg.cc/25Yrw3LF/Temporal.webp"
   },
   {
     title: "One More Light",
     artist: "Temporal",
     file: "https://hs1.krakenfiles.com/uploads/05-10-2025/hhRlecC1no/music.m4a",
-    cover: "https://i.postimg.cc/xd2pNQrC/Temporal.webp"
+    cover: "https://i.postimg.cc/25Yrw3LF/Temporal.webp"
   },
   {
     title: "Say Yes To Heaven",
     artist: "Temporal",
     file: "https://hs4.krakenfiles.com/uploads/05-10-2025/6pqXYSzljN/music.m4a",
-    cover: "https://i.postimg.cc/xd2pNQrC/Temporal.webp"
+    cover: "https://i.postimg.cc/25Yrw3LF/Temporal.webp"
   },
   {
     title: "Royalty",
     artist: "Temporal",
     file: "https://hs3.krakenfiles.com/uploads/05-10-2025/4FTZitFF0q/music.m4a",
-    cover: "https://i.postimg.cc/xd2pNQrC/Temporal.webp"
+    cover: "https://i.postimg.cc/25Yrw3LF/Temporal.webp"
   },
 
 
