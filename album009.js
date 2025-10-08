07f05df2e30d28f111c4ffcba5da92187a891fdd
Apps <wallxnip@gmail.com>
2025-10-08 09:43:41 -0300

    Atualizar o album009.js

--- album009.js
+++ album009.js
@@ -4,25 +4,25 @@ const playlist508 = [
     title: "Eu Quero Só Você",
     artist: "Kiko Chicabana",
     file: "https://hs9.krakenfiles.com/uploads/05-10-2025/7AuEay7ZEp/music.m4a",
-    cover: "https://i.postimg.cc/d0f98rQq/Chicabana.jpg"
+    cover: "https://i.postimg.cc/ZKc6xvMD/Chicabana.webp"
   },
   {
     title: "Á Sua Maneira",
     artist: "Kiko Chicabana",
     file: "https://hs6.krakenfiles.com/uploads/05-10-2025/LDELW9RDWO/music.m4a",
-    cover: "https://i.postimg.cc/d0f98rQq/Chicabana.jpg"
+    cover: "https://i.postimg.cc/ZKc6xvMD/Chicabana.webp"
   },
   {
     title: "Malandramente",
     artist: "Kiko Chicabana",
     file: "https://hs8.krakenfiles.com/uploads/05-10-2025/nudlnnMaPY/music.m4a",
-    cover: "https://i.postimg.cc/d0f98rQq/Chicabana.jpg"
+    cover: "https://i.postimg.cc/ZKc6xvMD/Chicabana.webp"
   },
   {
     title: "Te Amo Sim",
     artist: "Kiko Chicabana",
     file: "https://hs3.krakenfiles.com/uploads/05-10-2025/ITbYay3FUF/music.m4a",
-    cover: "https://i.postimg.cc/d0f98rQq/Chicabana.jpg"
+    cover: "https://i.postimg.cc/ZKc6xvMD/Chicabana.webp"
   },
 
 
