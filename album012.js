afda739fe041a9249f84c9c5a5215b952af22775
Apps <wallxnip@gmail.com>
2025-10-09 05:31:08 -0300

    Criar album012.js

---
+++ album012.js
@@ -0,0 +1,16 @@
+// Minha Favorita 
+const playlist511 = [
+  {
+    title: "Good Night",
+    artist: "Dayystar",
+    file: "https://hs2.krakenfiles.com/uploads/09-10-2025/6dpkpdcXkf/music.m4a",
+    cover: "https://i.postimg.cc/5yqKVrHK/Wallker.webp"
+  },
+  {
+    title: "Love Dies Slow",
+    artist: "Dayystar",
+    file: "https://hs5.krakenfiles.com/uploads/09-10-2025/ioidPIpLvw/music.m4a",
+    cover: "https://i.postimg.cc/5yqKVrHK/Wallker.webp"
+  },
+  
+];
