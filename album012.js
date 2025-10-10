425a7fcace37f782a288d4c474365ca837d4ba08
Apps <wallxnip@gmail.com>
2025-10-10 09:18:19 -0300

    Atualizar o album012.js

--- album012.js
+++ album012.js
@@ -4,25 +4,25 @@ const playlist511 = [
     title: "Good Night",
     artist: "Dayystar",
     file: "https://hs2.krakenfiles.com/uploads/09-10-2025/6dpkpdcXkf/music.m4a",
-    cover: "https://i.postimg.cc/5yqKVrHK/Wallker.webp"
+    cover: "https://i.postimg.cc/cCtYCdnK/wK.webp"
   },
   {
     title: "Love Dies Slow",
     artist: "Dayystar",
     file: "https://hs5.krakenfiles.com/uploads/09-10-2025/ioidPIpLvw/music.m4a",
-    cover: "https://i.postimg.cc/5yqKVrHK/Wallker.webp"
+    cover: "https://i.postimg.cc/cCtYCdnK/wK.webp"
   },
   {
     title: "Sunflower",
     artist: "Post Malone, Swae Lee",
-    file: "https://hs2.krakenfiles.com/uploads/10-10-2025/a0B0BPs1sF/music.m4a",
-    cover: "https://i.postimg.cc/5yqKVrHK/Wallker.webp"
+    file: "https://hs7.krakenfiles.com/uploads/10-10-2025/aRMSd54nfX/music.m4a",
+    cover: "https://i.postimg.cc/cCtYCdnK/wK.webp"
   },
   {
     title: "Let's Go",
     artist: "Post Malone",
     file: "https://hs4.krakenfiles.com/uploads/10-10-2025/b2FqNVr28G/music.m4a",
-    cover: "https://i.postimg.cc/5yqKVrHK/Wallker.webp"
+    cover: "https://i.postimg.cc/cCtYCdnK/wK.webp"
   },
   
 ];
