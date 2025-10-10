51c1d5d8d155d56501b68b17e1ef8b30a8798a5d
Apps <wallxnip@gmail.com>
2025-10-10 09:04:24 -0300

    Atualizar o album012.js

--- album012.js
+++ album012.js
@@ -12,5 +12,17 @@ const playlist511 = [
     file: "https://hs5.krakenfiles.com/uploads/09-10-2025/ioidPIpLvw/music.m4a",
     cover: "https://i.postimg.cc/5yqKVrHK/Wallker.webp"
   },
+  {
+    title: "Sunflower",
+    artist: "Post Malone, Swae Lee",
+    file: "https://hs2.krakenfiles.com/uploads/10-10-2025/a0B0BPs1sF/music.m4a",
+    cover: "https://i.postimg.cc/5yqKVrHK/Wallker.webp"
+  },
+  {
+    title: "Let's Go",
+    artist: "Post Malone",
+    file: "https://hs4.krakenfiles.com/uploads/10-10-2025/b2FqNVr28G/music.m4a",
+    cover: "https://i.postimg.cc/5yqKVrHK/Wallker.webp"
+  },
   
 ];
