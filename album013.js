85978a65af3be63bddf7e24ecf5890283923755d
Apps <wallxnip@gmail.com>
2025-10-11 23:28:31 -0300

    Criar album013.js

---
+++ album013.js
@@ -0,0 +1,107 @@
+// Lista de músicas
+const playlist512 = [
+    {
+    title: "Baba O'Riley",
+    artist: "Rock (Clássic)",
+    file: "https://hs9.krakenfiles.com/uploads/12-10-2025/N0Quo8aTd9/music.m4a",
+    cover: "https://i.postimg.cc/nchyMbnj/TheRock.webp"
+  },
+  {
+    title: "Be Somebody - Reignited",
+    artist: "Rock (Clássic)",
+    file: "https://hs9.krakenfiles.com/uploads/12-10-2025/UA3DLxzTmo/music.m4a",
+    cover: "https://i.postimg.cc/nchyMbnj/TheRock.webp"
+  },
+  {
+    title: "Bring Me To Life",
+    artist: "Rock (Clássic)",
+    file: "https://hs9.krakenfiles.com/uploads/12-10-2025/gNdrAzMGyD/music.m4a",
+    cover: "https://i.postimg.cc/nchyMbnj/TheRock.webp"
+  },
+  {
+    title: "21 Guns",
+    artist: "Rock (Clássic)",
+    file: "https://hs4.krakenfiles.com/uploads/12-10-2025/908gtr8aHA/music.m4a",
+    cover: "https://i.postimg.cc/nchyMbnj/TheRock.webp"
+  },
+  {
+    title: "Burn It Down",
+    artist: "Rock (Clássic)",
+    file: "https://hs8.krakenfiles.com/uploads/12-10-2025/Y71wDngccQ/music.m4a",
+    cover: "https://i.postimg.cc/nchyMbnj/TheRock.webp"
+  },
+  {
+    title: "Castle Of Glass",
+    artist: "Rock (Clássic)",
+    file: "https://hs7.krakenfiles.com/uploads/12-10-2025/xJVnlVhQne/music.m4a",
+    cover: "https://i.postimg.cc/nchyMbnj/TheRock.webp"
+  },
+  {
+    title: "Crawling",
+    artist: "Rock (Clássic)",
+    file: "https://hs2.krakenfiles.com/uploads/12-10-2025/rrA0vp0fvd/music.m4a",
+    cover: "https://i.postimg.cc/nchyMbnj/TheRock.webp"
+  },
+  {
+    title: "How You Remind Me",
+    artist: "Rock (Clássic)",
+    file: "https://hs3.krakenfiles.com/uploads/12-10-2025/BnsqfrQ48x/music.m4a",
+    cover: "https://i.postimg.cc/nchyMbnj/TheRock.webp"
+  },
+  {
+    title: "In the End",
+    artist: "Rock (Clássic)",
+    file: "https://hs10.krakenfiles.com/uploads/12-10-2025/MU2chgdveR/music.m4a",
+    cover: "https://i.postimg.cc/nchyMbnj/TheRock.webp"
+  },
+    {
+    title: "Numb",
+    artist: "Rock (Clássic)",
+    file: "https://hs7.krakenfiles.com/uploads/12-10-2025/XaHQtUYu9w/music.m4a",
+    cover: "https://i.postimg.cc/nchyMbnj/TheRock.webp"
+  },
+  {
+    title: "On My Own",
+    artist: "Rock (Clássic)",
+    file: "https://hs10.krakenfiles.com/uploads/12-10-2025/N3hSfXJcmT/music.m4a",
+    cover: "https://i.postimg.cc/nchyMbnj/TheRock.webp"
+  },
+  {
+    title: "Rise Up",
+    artist: "Rock (Clássic)",
+    file: "https://hs1.krakenfiles.com/uploads/12-10-2025/QQRW3uRWnn/music.m4a",
+    cover: "https://i.postimg.cc/nchyMbnj/TheRock.webp"
+  },
+    {
+    title: "Wrecked",
+    artist: "Rock (Clássic)",
+    file: "https://hs6.krakenfiles.com/uploads/12-10-2025/cVckkdHWTS/music.m4a",
+    cover: "https://i.postimg.cc/nchyMbnj/TheRock.webp"
+  },
+  {
+    title: "What I've Done",
+    artist: "Rock (Clássic)",
+    file: "https://hs10.krakenfiles.com/uploads/12-10-2025/thXHFXQO57/music.m4a",
+    cover: "https://i.postimg.cc/nchyMbnj/TheRock.webp"
+  },
+  {
+    title: "Wherever You Will Go",
+    artist: "Rock (Clássic)",
+    file: "https://hs3.krakenfiles.com/uploads/12-10-2025/74tMFPMK57/music.m4a",
+    cover: "https://i.postimg.cc/nchyMbnj/TheRock.webp"
+  },
+  {
+    title: "Without You",
+    artist: "Rock (Clássic)",
+    file: "https://hs2.krakenfiles.com/uploads/12-10-2025/WM6hWjnckG/music.m4a",
+    cover: "https://i.postimg.cc/nchyMbnj/TheRock.webp"
+  },
+  {
+    title: "It's My Life",
+    artist: "Rock (Clássic)",
+    file: "https://hs3.krakenfiles.com/uploads/12-10-2025/RybDOx6zhg/music.m4a",
+    cover: "https://i.postimg.cc/nchyMbnj/TheRock.webp"
+  },
+  
+  
+];
