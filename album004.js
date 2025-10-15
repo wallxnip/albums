9f1e66f28ea5d13a935b94f37633fce1280eff3a
Apps <wallxnip@gmail.com>
2025-10-15 18:32:43 -0300

    Criar album004.js

---
+++ album004.js
@@ -0,0 +1,178 @@
+// NEFFEX
+const playlist503 = [
+  {
+    title: "Best of Me",
+    artist: "NEFFEX",
+    file: "https://hs10.krakenfiles.com/uploads/15-10-2025/2Yk4gZUnen/music.m4a",
+    cover: "https://hs7.krakenfiles.com/uploads/15-10-2025/6xQ7OC9etf/cover.png"
+  },
+  {
+    title: "Life",
+    artist: "NEFFEX",
+    file: "https://hs10.krakenfiles.com/uploads/15-10-2025/mH74lOz8lB/music.m4a",
+    cover: "https://hs7.krakenfiles.com/uploads/15-10-2025/6xQ7OC9etf/cover.png"
+  },
+  {
+    title: "Careless",
+    artist: "NEFFEX",
+    file: "https://hs10.krakenfiles.com/uploads/15-10-2025/zKLVBJ28jz/music.m4a",
+    cover: "https://hs7.krakenfiles.com/uploads/15-10-2025/6xQ7OC9etf/cover.png"
+  },
+  {
+    title: "Cold",
+    artist: "NEFFEX",
+    file: "https://hs1.krakenfiles.com/uploads/15-10-2025/R3VSX3jfXz/music.m4a",
+    cover: "https://hs7.krakenfiles.com/uploads/15-10-2025/6xQ7OC9etf/cover.png"
+  },
+  {
+    title: "Grateful",
+    artist: "NEFFEX",
+    file: "https://hs6.krakenfiles.com/uploads/15-10-2025/WNxRDX27d3/music.m4a",
+    cover: "https://hs7.krakenfiles.com/uploads/15-10-2025/6xQ7OC9etf/cover.png"
+  },
+  {
+    title: "Fight Back",
+    artist: "NEFFEX",
+    file: "https://hs4.krakenfiles.com/uploads/15-10-2025/Dwaix1oEGd/music.m4a",
+    cover: "https://hs7.krakenfiles.com/uploads/15-10-2025/6xQ7OC9etf/cover.png"
+  },
+  {
+    title: "Rumors",
+    artist: "NEFFEX",
+    file: "https://hs2.krakenfiles.com/uploads/15-10-2025/hNnYLpxoDf/music.m4a",
+    cover: "https://hs7.krakenfiles.com/uploads/15-10-2025/6xQ7OC9etf/cover.png"
+  },
+  {
+    title: "Summertime",
+    artist: "NEFFEX",
+    file: "https://hs10.krakenfiles.com/uploads/15-10-2025/4Z9GlE7CNa/music.m4a",
+    cover: "https://hs7.krakenfiles.com/uploads/15-10-2025/6xQ7OC9etf/cover.png"
+  },
+  {
+    title: "With You",
+    artist: "NEFFEX",
+    file: "https://hs7.krakenfiles.com/uploads/15-10-2025/6xQ7OC9etf/music.m4a",
+    cover: "https://hs7.krakenfiles.com/uploads/15-10-2025/6xQ7OC9etf/cover.png"
+  },
+  {
+    title: "Home",
+    artist: "NEFFEX",
+    file: "https://hs10.krakenfiles.com/uploads/15-10-2025/lzPjQV5VwK/music.m4a",
+    cover: "https://hs7.krakenfiles.com/uploads/15-10-2025/6xQ7OC9etf/cover.png"
+  },
+  {
+    title: "Hope",
+    artist: "NEFFEX",
+    file: "https://hs6.krakenfiles.com/uploads/15-10-2025/zhlHx0GuFp/music.m4a",
+    cover: "https://hs7.krakenfiles.com/uploads/15-10-2025/6xQ7OC9etf/cover.png"
+  },
+  {
+    title: "A Feeling",
+    artist: "NEFFEX",
+    file: "https://hs3.krakenfiles.com/uploads/15-10-2025/4hpViKQyOz/music.m4a",
+    cover: "https://hs7.krakenfiles.com/uploads/15-10-2025/6xQ7OC9etf/cover.png"
+  },
+  {
+    title: "Alive",
+    artist: "NEFFEX",
+    file: "https://hs4.krakenfiles.com/uploads/15-10-2025/BEqSOVWAet/music.m4a",
+    cover: "https://hs7.krakenfiles.com/uploads/15-10-2025/6xQ7OC9etf/cover.png"
+  },
+  {
+    title: "All These Thoughts",
+    artist: "NEFFEX",
+    file: "https://hs4.krakenfiles.com/uploads/15-10-2025/kzkRSijXZY/music.m4a",
+    cover: "https://hs7.krakenfiles.com/uploads/15-10-2025/6xQ7OC9etf/cover.png"
+  },
+  {
+    title: "Baller",
+    artist: "NEFFEX",
+    file: "https://hs9.krakenfiles.com/uploads/15-10-2025/J83xmyvcZd/music.m4a",
+    cover: "https://hs7.krakenfiles.com/uploads/15-10-2025/6xQ7OC9etf/cover.png"
+  },
+  {
+    title: "Broken Dreams",
+    artist: "NEFFEX",
+    file: "https://hs9.krakenfiles.com/uploads/15-10-2025/QB9oN2sXwO/music.m4a",
+    cover: "https://hs7.krakenfiles.com/uploads/15-10-2025/6xQ7OC9etf/cover.png"
+  },
+  {
+    title: "Climb",
+    artist: "NEFFEX",
+    file: "https://hs2.krakenfiles.com/uploads/15-10-2025/GK18W9HZDp/music.m4a",
+    cover: "https://hs7.krakenfiles.com/uploads/15-10-2025/6xQ7OC9etf/cover.png"
+  },
+  {
+    title: "Deep Thoughts",
+    artist: "NEFFEX",
+    file: "https://hs7.krakenfiles.com/uploads/15-10-2025/Glg4oVeylo/music.m4a",
+    cover: "https://hs7.krakenfiles.com/uploads/15-10-2025/6xQ7OC9etf/cover.png"
+  },
+  {
+    title: "Fade Away",
+    artist: "NEFFEX",
+    file: "https://hs8.krakenfiles.com/uploads/15-10-2025/wKx3nsgGlg/music.m4a",
+    cover: "https://hs7.krakenfiles.com/uploads/15-10-2025/6xQ7OC9etf/cover.png"
+  },
+  {
+    title: "Hate It or Love It",
+    artist: "NEFFEX",
+    file: "https://hs1.krakenfiles.com/uploads/15-10-2025/y2D5wGSKI0/music.m4a",
+    cover: "https://hs7.krakenfiles.com/uploads/15-10-2025/6xQ7OC9etf/cover.png"
+  },
+  {
+    title: "It's Just Not Fair",
+    artist: "NEFFEX",
+    file: "https://hs2.krakenfiles.com/uploads/15-10-2025/FvwGtok43B/music.m4a",
+    cover: "https://hs7.krakenfiles.com/uploads/15-10-2025/6xQ7OC9etf/cover.png"
+  },
+  {
+    title: "Lost Not Found",
+    artist: "NEFFEX",
+    file: "https://hs3.krakenfiles.com/uploads/15-10-2025/FiYym5azeM/music.m4a",
+    cover: "https://hs7.krakenfiles.com/uploads/15-10-2025/6xQ7OC9etf/cover.png"
+  },
+  {
+    title: "Lost Within",
+    artist: "NEFFEX",
+    file: "https://hs8.krakenfiles.com/uploads/15-10-2025/xLvaEzE9Ul/music.m4a",
+    cover: "https://hs7.krakenfiles.com/uploads/15-10-2025/6xQ7OC9etf/cover.png"
+  },
+  {
+    title: "Never Give Up.",
+    artist: "NEFFEX",
+    file: "https://hs3.krakenfiles.com/uploads/15-10-2025/fPHv3bA1H1/music.m4a",
+    cover: "https://hs7.krakenfiles.com/uploads/15-10-2025/6xQ7OC9etf/cover.png"
+  },
+  {
+    title: "Numb",
+    artist: "NEFFEX",
+    file: "https://hs10.krakenfiles.com/uploads/15-10-2025/nnhelnW88K/music.m4a",
+    cover: "https://hs7.krakenfiles.com/uploads/15-10-2025/6xQ7OC9etf/cover.png"
+  },
+  {
+    title: "Runaway",
+    artist: "NEFFEX",
+    file: "https://hs8.krakenfiles.com/uploads/15-10-2025/FhWF4lGoIZ/music.m4a",
+    cover: "https://hs7.krakenfiles.com/uploads/15-10-2025/6xQ7OC9etf/cover.png"
+  },
+  {
+    title: "Savage",
+    artist: "NEFFEX",
+    file: "https://hs5.krakenfiles.com/uploads/15-10-2025/TrHTceuZl6/music.m4a",
+    cover: "https://hs7.krakenfiles.com/uploads/15-10-2025/6xQ7OC9etf/cover.png"
+  },
+  {
+    title: "Take Me Back Again",
+    artist: "NEFFEX",
+    file: "https://hs10.krakenfiles.com/uploads/15-10-2025/zC6RyFL2qH/music.m4a",
+    cover: "https://hs7.krakenfiles.com/uploads/15-10-2025/6xQ7OC9etf/cover.png"
+  },
+    {
+    title: "Trust Me",
+    artist: "NEFFEX",
+    file: "https://hs6.krakenfiles.com/uploads/15-10-2025/eauTJ5H9Ry/music.m4a",
+    cover: "https://hs7.krakenfiles.com/uploads/15-10-2025/6xQ7OC9etf/cover.png"
+  },
+  
+];
