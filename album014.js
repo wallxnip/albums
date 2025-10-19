24157ccaa54c3732eb6264f02063177ee50e5dc4
Apps <wallxnip@gmail.com>
2025-10-18 22:03:35 -0300

    Criar album014.js

---
+++ album014.js
@@ -0,0 +1,13 @@
+// LATINA
+const playlist513 = [
+  {
+    title: "Sorry",
+    artist: "Justin Bieber",
+    file: "https://hs4.krakenfiles.com/uploads/19-10-2025/xclxk4gOrl/music.m4a",
+    cover: "https://i.postimg.cc/g0DQcZxR/Latina.webp"
+  },
+  
+  
+  
+  
+];
