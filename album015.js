02f64573281ead210c50ed384aa80d8e0b41eecf
Apps <wallxnip@gmail.com>
2025-10-20 03:38:23 -0300

    Criar album015.js

---
+++ album015.js
@@ -0,0 +1,24 @@
+// Trilha Sonora de Games
+const playlist514 = [
+  {
+    title: "The Flames Of Fate",
+    artist: "Naraka: Bladepoint",
+    file: "https://hs10.krakenfiles.com/uploads/20-10-2025/zwM1jA7sRg/music.m4a",
+    cover: "https://i.postimg.cc/NFdmV5Hh/nk.webp"
+  },
+  {
+    title: "From the Ashes",
+    artist: "Naraka: Bladepoint",
+    file: "https://hs9.krakenfiles.com/uploads/20-10-2025/e0LwpWLUgi/music.m4a",
+    cover: "https://i.postimg.cc/NFdmV5Hh/nk.webp"
+  },
+  {
+    title: "The Bullet Or The Blade",
+    artist: "Capcom",
+    file: "https://hs1.krakenfiles.com/uploads/20-10-2025/VrfrKhIZsz/music.m4a",
+    cover: "https://i.postimg.cc/kGTKZBt0/re4.webp"
+  },
+  
+  
+  
+];
