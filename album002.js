b64fc99c01496cf2ea234c4ef19a5dee0fe6ab1a
Apps <wallxnip@gmail.com>
2025-11-11 04:49:16 -0300

    Add files via upload

--- album002.js
+++ album002.js
@@ -4,123 +4,123 @@ const playlist501 = [
   {
     title: "Here Comes the Sun",
     artist: "Summer (MIX)",
-    file: "https://hs3.krakenfiles.com/uploads/13-10-2025/dnRopCvKi0/music.m4a",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Summer (MIX)/Here Comes the Sun.mp3",
     cover: "https://i.postimg.cc/J44t5H8J/Summer.webp"
   },
 
   {
     title: "So Good",
     artist: "Summer (MIX)",
-    file: "https://hs3.krakenfiles.com/uploads/05-10-2025/CanWdRZqGX/music.m4a",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Summer (MIX)/So Good.mp3",
     cover: "https://i.postimg.cc/J44t5H8J/Summer.webp"
   },
   
   {
     title: "Hight Again",
     artist: "Summer (MIX)",
-    file: "https://hs3.krakenfiles.com/uploads/05-10-2025/hsvKNmEDdW/music.m4a",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Summer (MIX)/Hight Again.mp3",
     cover: "https://i.postimg.cc/J44t5H8J/Summer.webp"
   },
 
   {
     title: "Good Feeling",
     artist: "Summer (MIX)",
-    file: "https://hs8.krakenfiles.com/uploads/05-10-2025/pHVpdK8LDY/music.m4a",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Summer (MIX)/Good Feeling.mp3",
     cover: "https://i.postimg.cc/J44t5H8J/Summer.webp"
   },
 
   {
     title: "Girls in Luv",
     artist: "Summer (MIX)",
-    file: "https://hs8.krakenfiles.com/uploads/05-10-2025/UbmAFxDLve/music.m4a",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Summer (MIX)/Girls in Luv.mp3",
     cover: "https://i.postimg.cc/J44t5H8J/Summer.webp"
   },
 
   {
     title: "Summer",
     artist: "Summer (MIX)",
-    file: "https://hs5.krakenfiles.com/uploads/05-10-2025/6uQ11wwWLN/music.m4a",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Summer (MIX)/Summer.mp3",
     cover: "https://i.postimg.cc/J44t5H8J/Summer.webp"
   },
 
   {
     title: "Fall In Love",
     artist: "Summer (MIX)",
-    file: "https://hs8.krakenfiles.com/uploads/05-10-2025/6wAEHXGCDC/music.m4a",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Summer (MIX)/Fall In Love.mp3",
     cover: "https://i.postimg.cc/J44t5H8J/Summer.webp"
   },
 
   {
-    title: "Deserto Rain",
+    title: "Desert Rain",
     artist: "Summer (MIX)",
-    file: "https://hs3.krakenfiles.com/uploads/05-10-2025/ozqJrjBHms/music.m4a",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Summer (MIX)/Desert Rain.mp3",
     cover: "https://i.postimg.cc/J44t5H8J/Summer.webp"
   },
 
   {
     title: "Alive",
     artist: "Summer (MIX)",
-    file: "https://hs1.krakenfiles.com/uploads/05-10-2025/yjcwUl0YZD/music.m4a",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Summer (MIX)/Alive.mp3",
     cover: "https://i.postimg.cc/J44t5H8J/Summer.webp"
   },
 
   {
     title: "Holiday",
     artist: "Summer (MIX)",
-    file: "https://hs8.krakenfiles.com/uploads/05-10-2025/eyL16DwZWM/music.m4a",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Summer (MIX)/Holiday.mp3",
     cover: "https://i.postimg.cc/J44t5H8J/Summer.webp"
   },
 
   {
     title: "Infinity",
     artist: "Summer (MIX)",
-    file: "https://hs8.krakenfiles.com/uploads/05-10-2025/xImc6K9r0A/music.m4a",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Summer (MIX)/Infinity.mp3",
     cover: "https://i.postimg.cc/J44t5H8J/Summer.webp"
   },
 
   {
     title: "Love Again",
     artist: "Summer (MIX)",
-    file: "https://hs1.krakenfiles.com/uploads/05-10-2025/aCP2sFU8nb/music.m4a",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Summer (MIX)/Love Again.mp3",
     cover: "https://i.postimg.cc/J44t5H8J/Summer.webp"
   },
 
   {
     title: "Play Hard",
     artist: "Summer (MIX)",
-    file: "https://hs10.krakenfiles.com/uploads/05-10-2025/Nf2LaD8cXh/music.m4a",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Summer (MIX)/Play Hard.mp3",
     cover: "https://i.postimg.cc/J44t5H8J/Summer.webp"
   },
 
   {
     title: "Player In C",
     artist: "Summer (MIX)",
-    file: "https://hs7.krakenfiles.com/uploads/05-10-2025/60m9A4e4hv/music.m4a",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Summer (MIX)/Player In C.mp3",
     cover: "https://i.postimg.cc/J44t5H8J/Summer.webp"
   },
 
   {
     title: "Stereo Love",
     artist: "Summer (MIX)",
-    file: "https://hs10.krakenfiles.com/uploads/05-10-2025/IJvnq8J3QE/music.m4a",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Summer (MIX)/Stereo Love.mp3",
     cover: "https://i.postimg.cc/J44t5H8J/Summer.webp"
   },
   {
     title: "Hold On",
     artist: "Summer (MIX)",
-    file: "https://hs1.krakenfiles.com/uploads/13-10-2025/C5X4nh6hnx/music.m4a",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Summer (MIX)/Hold On.mp3",
     cover: "https://i.postimg.cc/J44t5H8J/Summer.webp"
   },
   {
     title: "Live a Life You Will Remember",
     artist: "Summer (MIX)",
-    file: "https://hs6.krakenfiles.com/uploads/14-10-2025/e55wULjxF0/music.m4a",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Summer (MIX)/Live a Life You Will Remember.mp3",
     cover: "https://i.postimg.cc/J44t5H8J/Summer.webp"
   },
   {
     title: "You're Somebody Else",
     artist: "Summer (MIX)",
-    file: "https://hs4.krakenfiles.com/uploads/14-10-2025/VrjTKXGg4x/music.m4a",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Summer (MIX)/You're Somebody Else.mp3",
     cover: "https://i.postimg.cc/J44t5H8J/Summer.webp"
   },
   
