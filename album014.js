02f684dbfc5389518a01feb23c20948c00e05cc5
Apps <wallxnip@gmail.com>
2025-11-11 07:14:01 -0300

    Add files via upload

--- album014.js
+++ album014.js
@@ -3,7 +3,7 @@ const playlist513 = [
   {
     title: "Sorry",
     artist: "Justin Bieber",
-    file: "https://hs4.krakenfiles.com/uploads/19-10-2025/xclxk4gOrl/music.m4a",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Latina/Sorry.mp3",
     cover: "https://i.postimg.cc/g0DQcZxR/Latina.webp"
   },
   
