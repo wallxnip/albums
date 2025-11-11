964af1a4140227c9f71ede6e5bfc5fbedeb9cd97
Apps <wallxnip@gmail.com>
2025-11-11 05:41:09 -0300

    Add files via upload

--- album005.js
+++ album005.js
@@ -3,136 +3,137 @@ const playlist504 = [
   {
     title: "Universo Paralelo",
     artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Universo Paralelo.mp3",
+    file: "https://hs9.krakenfiles.com/uploads/14-10-2025/6tyWwfJkW5/music.m4a",
     cover: "https://i.postimg.cc/RhYxG80b/Funk.webp"
   },
   {
     title: "Alegra",
     artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Alegra.mp3,
+    file: "https://hs3.krakenfiles.com/uploads/05-10-2025/aDn5Pq6qtG/music.m4a",
     cover: "https://i.postimg.cc/RhYxG80b/Funk.webp"
   },
   {
     title: "Amanheceu",
     artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Amanheceu.mp3",
+    file: "https://hs2.krakenfiles.com/uploads/05-10-2025/sspheqELIt/music.m4a",
     cover: "https://i.postimg.cc/RhYxG80b/Funk.webp"
   },
   {
     title: "Aprendi Maldade Com Essa Maldita",
     artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Aprendi Maldade Com Essa Maldita.mp3",
+    file: "https://hs5.krakenfiles.com/uploads/05-10-2025/RsddNVWrQR/music.m4a",
     cover: "https://i.postimg.cc/RhYxG80b/Funk.webp"
   },
   {
     title: "Bela Dupla",
     artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Bela Dupla.mp3",
+    file: "https://hs7.krakenfiles.com/uploads/05-10-2025/41V1meZYYK/music.m4a",
     cover: "https://i.postimg.cc/RhYxG80b/Funk.webp"
   },
   {
     title: "É Que Eu Te Sarrei",
     artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/É Que Eu Te Sarrei.mp3",
+    file: "https://hs7.krakenfiles.com/uploads/05-10-2025/JsAH7R8fKJ/music.m4a",
     cover: "https://i.postimg.cc/RhYxG80b/Funk.webp"
   },
   {
     title: "Maldito Cupido",
     artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Maldito Cupido.mp3",
+    file: "https://hs6.krakenfiles.com/uploads/05-10-2025/etNfyFkA9C/music.m4a",
     cover: "https://i.postimg.cc/RhYxG80b/Funk.webp"
   },
   {
     title: "Não Dá pra Negar",
     artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Não Dá pra Negar.mp3",
+    file: "https://hs9.krakenfiles.com/uploads/05-10-2025/D8CrnjFbNJ/music.m4a",
     cover: "https://i.postimg.cc/RhYxG80b/Funk.webp"
   },
   {
     title: "O Que Que Tá Acontecendo",
     artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/O Que Que Tá Acontecendo.mp3",
+    file: "https://hs1.krakenfiles.com/uploads/05-10-2025/2qbFexcAGL/music.m4a",
     cover: "https://i.postimg.cc/RhYxG80b/Funk.webp"
   },
   {
     title: "Perdoa os Meus Pecados",
     artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Perdoa os Meus Pecados.mp3",
+    file: "https://hs1.krakenfiles.com/uploads/05-10-2025/F5V1l1Bg6w/music.m4a",
     cover: "https://i.postimg.cc/RhYxG80b/Funk.webp"
   },
   {
     title: "Plano Perfeito",
     artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Plano Perfeito.mp3",
+    file: "https://hs7.krakenfiles.com/uploads/05-10-2025/YOmZUNgL67/music.m4a",
     cover: "https://i.postimg.cc/RhYxG80b/Funk.webp"
   },
   {
     title: "Postura",
     artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Postura.mp3",
+    file: "https://hs7.krakenfiles.com/uploads/05-10-2025/72pZ95Sb98/music.m4a",
     cover: "https://i.postimg.cc/RhYxG80b/Funk.webp"
   },
   {
     title: "Quadradinho",
     artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Quadradinho.mp3",
+    file: "https://hs6.krakenfiles.com/uploads/05-10-2025/zRZhViZs6q/music.m4a",
     cover: "https://i.postimg.cc/RhYxG80b/Funk.webp"
   },
   {
     title: "Se Tu Quer Replay",
     artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Se Tu Quer Replay.mp3",
+    file: "https://hs5.krakenfiles.com/uploads/05-10-2025/HQUfWSpIEE/music.m4a",
     cover: "https://i.postimg.cc/RhYxG80b/Funk.webp"
   },
   {
     title: "Segue o Baile",
     artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Segue o Baile.mp3",
+    file: "https://hs10.krakenfiles.com/uploads/05-10-2025/pcUDgVtjcd/music.m4a",
     cover: "https://i.postimg.cc/RhYxG80b/Funk.webp"
   },
   {
     title: "Tatuagem da Borboleta",
     artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Tatuagem da Borboleta.mp3",
+    file: "https://hs7.krakenfiles.com/uploads/05-10-2025/bwOgi4wiH3/music.m4a",
     cover: "https://i.postimg.cc/RhYxG80b/Funk.webp"
   },
   {
     title: "Tava Escrito nas Estrelas",
     artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Tava Escrito nas Estrelas.mp3",
+    file: "https://hs10.krakenfiles.com/uploads/05-10-2025/XpdF3MsxUL/music.m4a",
     cover: "https://i.postimg.cc/RhYxG80b/Funk.webp"
   },
   {
     title: "Vou te Contar um Segredinho",
     artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Vou te Contar um Segredinho.mp3",
+    file: "https://hs10.krakenfiles.com/uploads/05-10-2025/HCbccjjg4D/music.m4a",
     cover: "https://i.postimg.cc/RhYxG80b/Funk.webp"
   },
 
   {
     title: "Olha Como Ela é Gata - Silicone",
     artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Olha Como Ela é Gata - Silicone.mp3",
+    file: "https://hs7.krakenfiles.com/uploads/14-10-2025/4K22TTyLw6/music.m4a",
     cover: "https://i.postimg.cc/RhYxG80b/Funk.webp"
   },
   {
     title: "Paraiso",
     artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Paraiso.mp3",
+    file: "https://hs2.krakenfiles.com/uploads/14-10-2025/bCzeqGC2R1/music.m4a",
     cover: "https://i.postimg.cc/RhYxG80b/Funk.webp"
   },
   {
     title: "Ritmada do Fifty",
     artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Ritmada do Fifty.mp3",
+    file: "https://hs5.krakenfiles.com/uploads/14-10-2025/VohnY35IUL/music.m4a",
     cover: "https://i.postimg.cc/RhYxG80b/Funk.webp"
   },
 
   {
     title: "Se Quiser Me Ver",
     artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Se Quiser Me Ver.mp3",
+    file: "https://hs9.krakenfiles.com/uploads/14-10-2025/d18cJxSfL5/music.m4a",
     cover: "https://i.postimg.cc/RhYxG80b/Funk.webp"
   },
+  
   
 ];
