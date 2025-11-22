1ee635606ae66621a981226e6b463a2e75e75cb5
Apps <wallxnip@gmail.com>
2025-11-22 12:05:45 -0300

    Atualizar o album016.js

--- album016.js
+++ album016.js
@@ -1,1 +1,15 @@
-g
+// Oh Oh Rei do Universo
+const playlist515 = [
+  {
+    title: "The Flames Of Fate",
+    artist: "Naraka: Bladepoint",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/GamesXD/The Flames Of Fate.mp3",
+    cover: "https://i.postimg.cc/rFpZHjSC/ohohrei.webp"
+  },
+  
+
+
+
+
+
+];
