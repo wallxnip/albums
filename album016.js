5db70b5fb99a57a22455eaa05ded2a2837c6e9b4
Apps <wallxnip@gmail.com>
2025-11-22 12:24:14 -0300

    Atualizar o album016.js

--- album016.js
+++ album016.js
@@ -1,13 +1,18 @@
 // Oh Oh Rei do Universo
 const playlist515 = [
   {
-    title: "The Flames Of Fate",
-    artist: "Naraka: Bladepoint",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/GamesXD/The Flames Of Fate.mp3",
+    title: "Um Novo Ser",
+    artist: "Hikari de Jesus",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Oh Rei do Universo Jesus ✞/Um Novo Ser.mp3",
     cover: "https://i.postimg.cc/rFpZHjSC/ohohrei.webp"
   },
   
-
+  {
+    title: "Minha Verdadeira Sorte",
+    artist: "Eterno Som",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Oh Rei do Universo Jesus ✞/Minha Verdadeira Sorte.mp3",
+    cover: "https://i.postimg.cc/rFpZHjSC/ohohrei.webp"
+  },
 
 
 
