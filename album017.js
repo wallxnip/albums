96cf612710e79e8c77e32de2f906740af9f0766d
Apps <wallxnip@gmail.com>
2025-11-25 13:17:19 -0300

    Atualizar o album017.js

--- album017.js
+++ album017.js
@@ -4,14 +4,14 @@ const playlist516 = [
     title: "Meditação Guiada (Som de Chuva)",
     artist: "Zentopia",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Zentopia/Som de Água (Relaxantes).mp3",
-    cover: "https://i.postimg.cc/3RdRghr8/Zentopia.webp"
+    cover: "https://i.postimg.cc/g2jTYgGg/Zentopia.webp"
   },
 
   {
     title: "Meditação Guiada (Som de Chuva)",
     artist: "Zentopia",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Zentopia/Meditação Guiada (Som de Chuva).mp3",
-    cover: "https://i.postimg.cc/3RdRghr8/Zentopia.webp"
+    cover: "https://i.postimg.cc/g2jTYgGg/Zentopia.webp"
   },
 
 
