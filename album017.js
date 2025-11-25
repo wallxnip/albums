3b176ad840b297196086ffd70781493a46114aa0
Apps <wallxnip@gmail.com>
2025-11-25 00:15:55 -0300

    Atualizar o album017.js

--- album017.js
+++ album017.js
@@ -1,1 +1,20 @@
-f
+// Zentopia
+const playlist516 = [
+  {
+    title: "Um Novo Ser",
+    artist: "Hikari de Jesus",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Oh Rei do Universo Jesus ✞/Um Novo Ser.mp3",
+    cover: "https://i.postimg.cc/rFpZHjSC/ohohrei.webp"
+  },
+
+  {
+    title: "Minha Verdadeira Sorte",
+    artist: "Eterno Som",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Oh Rei do Universo Jesus ✞/Minha Verdadeira Sorte.mp3",
+    cover: "https://i.postimg.cc/rFpZHjSC/ohohrei.webp"
+  },
+
+
+
+
+];
