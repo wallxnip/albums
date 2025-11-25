c4bfc43c5ba1f453b68afe7b775e600c8a37dab3
Apps <wallxnip@gmail.com>
2025-11-25 13:14:39 -0300

    Atualizar o album017.js

--- album017.js
+++ album017.js
@@ -1,16 +1,16 @@
 // Zentopia
 const playlist516 = [
   {
-    title: "Um Novo Ser",
-    artist: "Hikari de Jesus",
+    title: "Meditação Guiada (Som de Chuva)",
+    artist: "Zentopia",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Zentopia/Som de Água (Relaxantes).mp3",
     cover: "https://i.postimg.cc/3RdRghr8/Zentopia.webp"
   },
 
   {
-    title: "Minha Verdadeira Sorte",
-    artist: "Eterno Som",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Zentopia/Meditação Guiada (som de chuva no final).mp3",
+    title: "Meditação Guiada (Som de Chuva)",
+    artist: "Zentopia",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Zentopia/Meditação Guiada (Som de Chuva).mp3",
     cover: "https://i.postimg.cc/3RdRghr8/Zentopia.webp"
   },
 
