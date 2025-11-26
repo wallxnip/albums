f88ac24f4a719713ec18dcf26dbff10334fb8728
Apps <wallxnip@gmail.com>
2025-11-26 04:42:48 -0300

    Atualizar o album008.js

--- album008.js
+++ album008.js
@@ -4,66 +4,66 @@ const playlist507 = [
     title: "Estilo Capa Loka",
     artist: "Francildo Silva",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Pisadinha do Vaqueiro/Estilo Capa Loka.mp3",
-    cover: "https://lh3.googleusercontent.com/PBw8TlYiDpAX6bYbFsfrsSo8eKIpfowL0de7VpvneSmiyp7HMLnIDdjJM9INWHWZ8jAdkaBQKb9sjSXiMw=w544-h544-l90-rj"
+    cover: "https://i.postimg.cc/jjjgm19m/pisadinha.webp"
   },
   {
     title: "Ela Kika Diferente",
     artist: "Francildo Silva",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Pisadinha do Vaqueiro/Ela Kika Diferente.mp3",
-    cover: "https://i.postimg.cc/2SYbMkP4/Pisadinha-do-Vaqueiro.webp"
+    cover: "https://i.postimg.cc/jjjgm19m/pisadinha.webp"
   },
   {
     title: "Tara Louca",
     artist: "Francildo Silva",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Pisadinha do Vaqueiro/Tara Louca.mp3",
-    cover: "https://i.postimg.cc/2SYbMkP4/Pisadinha-do-Vaqueiro.webp"
+    cover: "https://i.postimg.cc/jjjgm19m/pisadinha.webp"
   },
   {
     title: "Farra sem Limite",
     artist: "Francildo Silva",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Pisadinha do Vaqueiro/Farra sem Limite.mp3",
-    cover: "https://i.postimg.cc/2SYbMkP4/Pisadinha-do-Vaqueiro.webp"
+    cover: "https://i.postimg.cc/jjjgm19m/pisadinha.webp"
   },
   {
     title: "Cavalo de um Milhão",
     artist: "Francildo Silva",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Pisadinha do Vaqueiro/Cavalo de um Milhão.mp3",
-    cover: "https://i.postimg.cc/2SYbMkP4/Pisadinha-do-Vaqueiro.webp"
+    cover: "https://i.postimg.cc/jjjgm19m/pisadinha.webp"
   },
   {
     title: "Burguesinha",
     artist: "Francildo Silva",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Pisadinha do Vaqueiro/Burguesinha.mp3",
-    cover: "https://i.postimg.cc/2SYbMkP4/Pisadinha-do-Vaqueiro.webp"
+    cover: "https://i.postimg.cc/jjjgm19m/pisadinha.webp"
   },
   {
     title: "Eu Nem Lembro",
     artist: "Francildo Silva",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Pisadinha do Vaqueiro/Eu Nem Lembro.mp3",
-    cover: "https://i.postimg.cc/2SYbMkP4/Pisadinha-do-Vaqueiro.webp"
+    cover: "https://i.postimg.cc/jjjgm19m/pisadinha.webp"
   },
   {
     title: "Eu Tenho É Gratidão",
     artist: "Francildo Silva",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Pisadinha do Vaqueiro/Eu Tenho É Gratidão.mp3",
-    cover: "https://i.postimg.cc/2SYbMkP4/Pisadinha-do-Vaqueiro.webp"
+    cover: "https://i.postimg.cc/jjjgm19m/pisadinha.webp"
   },
   {
     title: "Ronco Do Riacho",
     artist: "Francildo Silva",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Pisadinha do Vaqueiro/Ronco Do Riacho.mp3",
-    cover: "https://i.postimg.cc/2SYbMkP4/Pisadinha-do-Vaqueiro.webp"
+    cover: "https://i.postimg.cc/jjjgm19m/pisadinha.webp"
   },
   {
     title: "Aliança",
     artist: "Francildo Silva",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Pisadinha do Vaqueiro/Aliança.mp3",
-    cover: "https://i.postimg.cc/2SYbMkP4/Pisadinha-do-Vaqueiro.webp"
+    cover: "https://i.postimg.cc/jjjgm19m/pisadinha.webp"
   },
   {
     title: "Colecionador de Porquês",
     artist: "Francildo Silva",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Pisadinha do Vaqueiro/Colecionador de Porquês.mp3",
-    cover: "https://i.postimg.cc/2SYbMkP4/Pisadinha-do-Vaqueiro.webp"
+    cover: "https://i.postimg.cc/jjjgm19m/pisadinha.webp"
   }
 ];
