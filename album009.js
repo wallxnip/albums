58a42c19323e7f09d0e724d69792aac98efdad87
Apps <wallxnip@gmail.com>
2025-11-26 04:32:05 -0300

    Atualizar o album009.js

--- album009.js
+++ album009.js
@@ -4,24 +4,24 @@ const playlist508 = [
     title: "Eu Quero Só Você",
     artist: "Kiko Chicabana",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Axé das Antigas/Eu Quero Só Você.mp3",
-    cover: "https://i.postimg.cc/ZKc6xvMD/Chicabana.webp"
+    cover: "https://lh3.googleusercontent.com/ee18srcgYIr6rihbLGnxawIZgPk_A8-tKStw1uswcPqJNE_kC-eCLPHx7siSyqY4cSNxEaVGGSG12BXp=w544-h544-l90-rj"
   },
   {
     title: "Á Sua Maneira",
     artist: "Kiko Chicabana",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Axé das Antigas/Á Sua Maneira.mp3",
-    cover: "https://i.postimg.cc/ZKc6xvMD/Chicabana.webp"
+    cover: "https://lh3.googleusercontent.com/ee18srcgYIr6rihbLGnxawIZgPk_A8-tKStw1uswcPqJNE_kC-eCLPHx7siSyqY4cSNxEaVGGSG12BXp=w544-h544-l90-rj"
   },
   {
     title: "Malandramente",
     artist: "Kiko Chicabana",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Axé das Antigas/Malandramente.mp3",
-    cover: "https://i.postimg.cc/ZKc6xvMD/Chicabana.webp"
+    cover: "https://lh3.googleusercontent.com/ee18srcgYIr6rihbLGnxawIZgPk_A8-tKStw1uswcPqJNE_kC-eCLPHx7siSyqY4cSNxEaVGGSG12BXp=w544-h544-l90-rj"
   },
   {
     title: "Te Amo Sim",
     artist: "Kiko Chicabana",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Axé das Antigas/Te Amo Sim.mp3",
-    cover: "https://i.postimg.cc/ZKc6xvMD/Chicabana.webp"
+    cover: "https://lh3.googleusercontent.com/ee18srcgYIr6rihbLGnxawIZgPk_A8-tKStw1uswcPqJNE_kC-eCLPHx7siSyqY4cSNxEaVGGSG12BXp=w544-h544-l90-rj"
   }
 ];
