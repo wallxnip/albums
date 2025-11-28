14a3aab2e6f57e88f051f75571b712c8e7de9307
Apps <wallxnip@gmail.com>
2025-11-28 00:04:57 -0300

    Atualizar o album019.js

--- album019.js
+++ album019.js
@@ -4,48 +4,48 @@ const playlist518 = [
     title: "A Culpa É Nossa",
     artist: "Nadson O Ferinha",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Nadson O Ferinha/A Culpa É Nossa.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/tJf0tDKW/Nadson.webp"
   },
   {
     title: "Duas",
     artist: "Nadson O Ferinha",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Nadson O Ferinha/Duas.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/tJf0tDKW/Nadson.webp"
   },
   {
-    title: "",
+    title: "Nosso Quadro",
     artist: "Nadson O Ferinha",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Nadson O Ferinha/Nosso Quadro.mp3",
-    cover: "Nosso Quadro"
+    cover: "https://i.postimg.cc/tJf0tDKW/Nadson.webp"
   },
   {
     title: "Só Falta Eu",
     artist: "Nadson O Ferinha",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Nadson O Ferinha/Só Falta Eu.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/tJf0tDKW/Nadson.webp"
   },
   {
     title: "Investe em Mim",
     artist: "Nadson O Ferinha",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Nadson O Ferinha/Investe em Mim.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/tJf0tDKW/Nadson.webp"
   },
   {
     title: "Depois do Universo",
     artist: "Nadson O Ferinha",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Nadson O Ferinha/Depois do Universo.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/tJf0tDKW/Nadson.webp"
   },
   {
     title: "Ainda Bem",
     artist: "Nadson O Ferinha",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Nadson O Ferinha/Ainda Bem.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/tJf0tDKW/Nadson.webp"
   },
   {
     title: "Cuidado",
     artist: "Nadson O Ferinha",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Nadson O Ferinha/Cuidado.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/tJf0tDKW/Nadson.webp"
   }
 ];
