a4a394bcd53c46801ae7d36cfc4d3087de46b2bf
Apps <wallxnip@gmail.com>
2025-11-27 23:59:00 -0300

    Criar album019.js

---
+++ album019.js
@@ -0,0 +1,51 @@
+// Nadson O Ferinha
+const playlist518 = [
+  {
+    title: "A Culpa É Nossa",
+    artist: "Nadson O Ferinha",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Nadson O Ferinha/A Culpa É Nossa.mp3",
+    cover: ""
+  },
+  {
+    title: "Duas",
+    artist: "Nadson O Ferinha",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Nadson O Ferinha/Duas.mp3",
+    cover: ""
+  },
+  {
+    title: "",
+    artist: "Nadson O Ferinha",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Nadson O Ferinha/Nosso Quadro.mp3",
+    cover: "Nosso Quadro"
+  },
+  {
+    title: "Só Falta Eu",
+    artist: "Nadson O Ferinha",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Nadson O Ferinha/Só Falta Eu.mp3",
+    cover: ""
+  },
+  {
+    title: "Investe em Mim",
+    artist: "Nadson O Ferinha",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Nadson O Ferinha/Investe em Mim.mp3",
+    cover: ""
+  },
+  {
+    title: "Depois do Universo",
+    artist: "Nadson O Ferinha",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Nadson O Ferinha/Depois do Universo.mp3",
+    cover: ""
+  },
+  {
+    title: "Ainda Bem",
+    artist: "Nadson O Ferinha",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Nadson O Ferinha/Ainda Bem.mp3",
+    cover: ""
+  },
+  {
+    title: "Cuidado",
+    artist: "Nadson O Ferinha",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Nadson O Ferinha/Cuidado.mp3",
+    cover: ""
+  }
+];
