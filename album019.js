93f7d42d6ea4b6433efb0f0a69bf445cd1ef41d1
Apps <wallxnip@gmail.com>
2025-11-29 03:25:42 -0300

    Atualizar o album019.js

--- album019.js
+++ album019.js
@@ -1,12 +1,6 @@
 // Nadson O Ferinha
 const playlist518 = [
   {
-    title: "A Culpa É Nossa",
-    artist: "Nadson O Ferinha",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Nadson O Ferinha/A Culpa É Nossa.mp3",
-    cover: "https://i.postimg.cc/tJf0tDKW/Nadson.webp"
-  },
-  {
     title: "Duas",
     artist: "Nadson O Ferinha",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Nadson O Ferinha/Duas.mp3",
@@ -19,21 +13,39 @@ const playlist518 = [
     cover: "https://i.postimg.cc/tJf0tDKW/Nadson.webp"
   },
   {
+    title: "Casa de Milhões",
+    artist: "Nadson O Ferinha",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Nadson O Ferinha/Casa de Milhões.mp3",
+    cover: "https://i.postimg.cc/tJf0tDKW/Nadson.webp"
+  },
+  {
+    title: "Te Quero pra Mim",
+    artist: "Nadson O Ferinha",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Nadson O Ferinha/Te Quero pra Mim.mp3",
+    cover: "https://i.postimg.cc/tJf0tDKW/Nadson.webp"
+  },
+  {
     title: "Só Falta Eu",
     artist: "Nadson O Ferinha",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Nadson O Ferinha/Só Falta Eu.mp3",
     cover: "https://i.postimg.cc/tJf0tDKW/Nadson.webp"
   },
   {
-    title: "Investe em Mim",
+    title: "A Culpa É Nossa",
     artist: "Nadson O Ferinha",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Nadson O Ferinha/Investe em Mim.mp3",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Nadson O Ferinha/A Culpa É Nossa.mp3",
     cover: "https://i.postimg.cc/tJf0tDKW/Nadson.webp"
   },
   {
     title: "Depois do Universo",
     artist: "Nadson O Ferinha",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Nadson O Ferinha/Depois do Universo.mp3",
+    cover: "https://i.postimg.cc/tJf0tDKW/Nadson.webp"
+  },
+  {
+    title: "Investe em Mim",
+    artist: "Nadson O Ferinha",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Nadson O Ferinha/Investe em Mim.mp3",
     cover: "https://i.postimg.cc/tJf0tDKW/Nadson.webp"
   },
   {
