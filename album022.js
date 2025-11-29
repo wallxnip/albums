70e0dc939f8b32f4671335ea196a05095ea1294a
Apps <wallxnip@gmail.com>
2025-11-29 20:07:04 -0300

    Criar album022.js

---
+++ album022.js
@@ -0,0 +1,87 @@
+// Clássico do Sertanejo
+const playlist021 = [
+  {
+    title: "Chuva",
+    artist: "João Bosco & Vinicius",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Sertanejo (Clássico)/Chuva.mp3",
+    cover: "https://i.postimg.cc/t4fHMF2K/Chuva.webp"
+  },
+  {
+    title: "Na Linha do Tempo",
+    artist: "Victor & Leo",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Sertanejo (Clássico)/Na Linha do Tempo.mp3",
+    cover: "https://i.postimg.cc/Ssh3BdCh/Na-Linha-do-Tempo.webp"
+  },
+  {
+    title: "Destino",
+    artist: "Lucas Lucco",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Sertanejo (Clássico)/Destino.mp3",
+    cover: "https://i.postimg.cc/wTZfKFY2/Destino.webp"
+  },
+  {
+    title: "Pássaro De Fogo.mp3",
+    artist: "Paula Fernandes",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Sertanejo (Clássico)/Pássaro De Fogo.mp3",
+    cover: "https://i.postimg.cc/q70G05bj/Passaro-De-Fogo.webp"
+  },
+  {
+    title: "Loka",
+    artist: "Simone & Simaria, Anitta",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Sertanejo (Clássico)/Loka.mp3",
+    cover: "https://i.postimg.cc/ZY9swX1h/Loka.webp"
+  },
+  {
+    title: "Açúcar em Mim",
+    artist: "Pedro Paulo & Alex",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Sertanejo (Clássico)/Açúcar em Mim.mp3",
+    cover: "https://i.postimg.cc/6pzhWhDr/Acucar-em-Mim.webp"
+  },
+  {
+    title: "Água de Oceano",
+    artist: "Victor & Leo",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Sertanejo (Clássico)/Água de Oceano.mp3",
+    cover: "https://i.postimg.cc/QdNpyTHt/Agua-de-Oceano.webp"
+  },
+  {
+    title: "Varinha Mágica",
+    artist: "Pedro Henrique & Fernando",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Sertanejo (Clássico)/Varinha Mágica.mp3",
+    cover: "https://i.postimg.cc/Y2fLBScy/Varinha-Magica.webp"
+  },
+  {
+    title: "Tudo Que Você Quiser",
+    artist: "Luan Santana",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Sertanejo (Clássico)/Tudo Que Você Quiser.mp3",
+    cover: "https://i.postimg.cc/pdfR5pJg/Tudo-Que-Voce-Quiser.webp"
+  },
+  {
+    title: "Pegada Que Desgrama",
+    artist: "Naiara Azevedo",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Sertanejo (Clássico)/Pegada Que Desgrama.mp3",
+    cover: "https://i.postimg.cc/8CHxVWHC/Pegada-Que-Desgrama.webp"
+  },
+  {
+    title: "O Tempo Não Apaga",
+    artist: "Victor & Leo",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Sertanejo (Clássico)/O Tempo Não Apaga.mp3",
+    cover: "https://i.postimg.cc/ZYdtW9Ys/O-Tempo-Nao-Apaga.webp"
+  },
+  {
+    title: "Jeito Carinhoso",
+    artist: "Jads & Jadson",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Sertanejo (Clássico)/Jeito Carinhoso.mp3",
+    cover: "https://i.postimg.cc/dQm6Nv5J/Jeito-Carinhoso.webp"
+  },
+  {
+    title: "Ex do Seu Atual",
+    artist: "Naiara Azevedo",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Sertanejo (Clássico)/Ex do Seu Atual.mp3",
+    cover: "https://i.postimg.cc/hPXJz1zp/naz.webp"
+  },
+  {
+    title: "Boa Sorte Pra Você",
+    artist: "Victor & Leo",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Sertanejo (Clássico)/Boa Sorte Pra Você.mp3",
+    cover: "https://i.postimg.cc/QdNpyTHt/Agua-de-Oceano.webp"
+  }
+];
