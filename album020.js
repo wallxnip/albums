9bcd80725b2a503eded931519c1a29ea2d92f0ce
Apps <wallxnip@gmail.com>
2025-11-29 04:29:14 -0300

    Atualizar o album020.js

--- album020.js
+++ album020.js
@@ -4,247 +4,247 @@ const playlist519 = [
     title: "A Closeness",
     artist: "Dermot Kennedy",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Dermot Kennedy/A Closeness.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
   },
   {
     title: "After Rain",
     artist: "Dermot Kennedy",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Dermot Kennedy/After Rain.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
   },
   {
     title: "All My Friends",
     artist: "Dermot Kennedy",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Dermot Kennedy/All My Friends.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
   },
   {
     title: "Already Gone",
     artist: "Dermot Kennedy",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Dermot Kennedy/Already Gone.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
   },
   {
     title: "An Evening I Will Not Forget",
     artist: "Dermot Kennedy",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Dermot Kennedy/An Evening I Will Not Forget.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
   },
   {
     title: "Any Love",
     artist: "Dermot Kennedy",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Dermot Kennedy/Any Love.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
   },
   {
     title: "Better Days",
     artist: "Dermot Kennedy",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Dermot Kennedy/Better Days.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
   },
   {
     title: "Blossom",
     artist: "Dermot Kennedy",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Dermot Kennedy/Blossom.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
   },
   {
     title: "Couldn't Tell",
     artist: "Dermot Kennedy",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Dermot Kennedy/Couldn't Tell.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
   },
   {
     title: "Dancing Under Red Skies",
     artist: "Dermot Kennedy",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Dermot Kennedy/Dancing Under Red Skies.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
   },
   {
     title: "Days Like This",
     artist: "Dermot Kennedy",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Dermot Kennedy/Days Like This.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
   },
   {
     title: "Divide",
     artist: "Dermot Kennedy",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Dermot Kennedy/Divide.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
   },
   {
     title: "Don't Forget Me",
     artist: "Dermot Kennedy",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Dermot Kennedy/Don't Forget Me.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
   },
   {
     title: "Dreamer",
     artist: "Dermot Kennedy",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Dermot Kennedy/Dreamer.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
   },
   {
     title: "For Island Fires and Family",
     artist: "Dermot Kennedy",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Dermot Kennedy/For Island Fires and Family.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
   },
   {
     title: "Giants",
     artist: "Dermot Kennedy",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Dermot Kennedy/Giants.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
   },
   {
     title: "Glory",
     artist: "Dermot Kennedy",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Dermot Kennedy/Glory.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
   },
   {
     title: "Homeward",
     artist: "Dermot Kennedy",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Dermot Kennedy/Homeward.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
   },
   {
     title: "Innocence and Sadness",
     artist: "Dermot Kennedy",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Dermot Kennedy/Innocence and Sadness.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
   },
   {
     title: "Kiss Me",
     artist: "Dermot Kennedy",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Dermot Kennedy/Kiss Me.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
   },
     {
     title: "Lessons",
     artist: "Dermot Kennedy",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Dermot Kennedy/Lessons.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
   },
   {
     title: "Lost",
     artist: "Dermot Kennedy",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Dermot Kennedy/Lost.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
   },
   {
     title: "Lucky",
     artist: "Dermot Kennedy",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Dermot Kennedy/Lucky.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
   },
   {
     title: "Moments Passed",
     artist: "Dermot Kennedy",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Dermot Kennedy/Moments Passed.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
   },
   {
     title: "One Life",
     artist: "Dermot Kennedy",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Dermot Kennedy/One Life.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
   },
   {
     title: "Outgrown",
     artist: "Dermot Kennedy",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Dermot Kennedy/Outgrown.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
   },
   {
     title: "Outnumbered",
     artist: "Dermot Kennedy",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Dermot Kennedy/Outnumbered.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
   },
   {
     title: "Paradise",
     artist: "Dermot Kennedy",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Dermot Kennedy/Paradise.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
   },
   {
     title: "Power Over Me",
     artist: "Dermot Kennedy",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Dermot Kennedy/Power Over Me.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
   },
   {
     title: "Redemption",
     artist: "Dermot Kennedy",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Dermot Kennedy/Redemption.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
   },
   {
     title: "Rome",
     artist: "Dermot Kennedy",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Dermot Kennedy/Rome.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
   },
   {
     title: "Shelter",
     artist: "Dermot Kennedy",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Dermot Kennedy/Shelter.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
   },
   {
     title: "Something to Someone",
     artist: "Dermot Kennedy",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Dermot Kennedy/Something to Someone.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
   },
   {
     title: "Sunday",
     artist: "Dermot Kennedy",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Dermot Kennedy/Sunday.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
   },
   {
     title: "Temptation",
     artist: "Dermot Kennedy",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Dermot Kennedy/Temptation.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
   },
   {
     title: "The Corner",
     artist: "Dermot Kennedy",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Dermot Kennedy/The Corner.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
   },
   {
     title: "The Killer Was a Coward",
     artist: "Dermot Kennedy",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Dermot Kennedy/The Killer Was a Coward.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
   },
   {
     title: "Two Hearts",
     artist: "Dermot Kennedy",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Dermot Kennedy/Two Hearts.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
   },
   {
     title: "What Have I Done",
     artist: "Dermot Kennedy",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Dermot Kennedy/What Have I Done.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
   },
   {
     title: "Without Fear",
     artist: "Dermot Kennedy",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Dermot Kennedy/Without Fear.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
   },
   {
     title: "Young & Free",
     artist: "Dermot Kennedy",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Dermot Kennedy/Young & Free.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
   }
   
 ];
