bcc0509763249d4b6c0bc4b0820f2bc2bd4b5101
Apps <wallxnip@gmail.com>
2025-11-29 17:45:34 -0300

    Criar album021.js

---
+++ album021.js
@@ -0,0 +1,76 @@
+// Playlist (MIX)
+const playlist513 = [
+  {
+    title: "Backyard",
+    artist: "Naya",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Naya (Ruby)/Backyard.mp3",
+    cover: "https://i.postimg.cc/PqNHyC5w/naya.webp"
+  },
+  {
+    title: "Cat Song",
+    artist: "Naya",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Naya (Ruby)/Cat Song.mp3",
+    cover: "https://i.postimg.cc/PqNHyC5w/naya.webp"
+  },
+  {
+    title: "Coming Back to You",
+    artist: "Naya",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Naya (Ruby)/Coming Back to You.mp3",
+    cover: "https://i.postimg.cc/PqNHyC5w/naya.webp"
+  },
+  {
+    title: "Dream On",
+    artist: "Naya",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Naya (Ruby)/Dream On.mp3",
+    cover: "https://i.postimg.cc/PqNHyC5w/naya.webp"
+  },
+  {
+    title: "Ghost By Your Side",
+    artist: "Naya",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Naya (Ruby)/Ghost By Your Side.mp3",
+    cover: "https://i.postimg.cc/PqNHyC5w/naya.webp"
+  },
+  {
+    title: "Girl on the Moon",
+    artist: "Naya",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Naya (Ruby)/Girl on the Moon.mp3",
+    cover: "https://i.postimg.cc/PqNHyC5w/naya.webp"
+  },
+  {
+    title: "Great Ocean Road",
+    artist: "Naya",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Naya (Ruby)/Great Ocean Road.mp3",
+    cover: "https://i.postimg.cc/PqNHyC5w/naya.webp"
+  },
+  {
+    title: "Solar",
+    artist: "Naya",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Naya (Ruby)/Solar.mp3",
+    cover: "https://i.postimg.cc/PqNHyC5w/naya.webp"
+  },
+  {
+    title: "Sorrow",
+    artist: "Naya",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Naya (Ruby)/Sorrow.mp3",
+    cover: "https://i.postimg.cc/PqNHyC5w/naya.webp"
+  },
+  {
+    title: "Sparkle",
+    artist: "Naya",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Naya (Ruby)/Sparkle.mp3",
+    cover: "https://i.postimg.cc/PqNHyC5w/naya.webp"
+  },
+  {
+    title: "Velvet Walk",
+    artist: "Naya",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Naya (Ruby)/Velvet Walk.mp3",
+    cover: "https://i.postimg.cc/PqNHyC5w/naya.webp"
+  },
+  {
+    title: "Wishing Tree",
+    artist: "Naya",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Naya (Ruby)/Wishing Tree.mp3",
+    cover: "https://i.postimg.cc/PqNHyC5w/naya.webp"
+  }
+  
+];
