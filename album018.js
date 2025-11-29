b59472e8c9429f6b9ad06abd2214a03a8738b69b
Apps <wallxnip@gmail.com>
2025-11-29 17:13:21 -0300

    Atualizar o album018.js

--- album018.js
+++ album018.js
@@ -4,60 +4,60 @@ const playlist517 = [
     title: "Ensina-me",
     artist: "RBD",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Rebelde/Ensina-me.mp3",
-    cover: "https://lh3.googleusercontent.com/2FPRvFa23vXHuXi07Bfre8lIgEwool6gFc4_JZM_cGOcYDtbZQgwbnFsh4qvtrm8gXCcZ0PjvyB21CTz2g=w544-h544-l90-rj"
+    cover: "https://i.postimg.cc/qMr4pZvx/RBD.webp"
   },
   {
     title: "Fique Em Silêncio",
     artist: "RBD",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Rebelde/Fique Em Silêncio.mp3",
-    cover: "https://lh3.googleusercontent.com/2FPRvFa23vXHuXi07Bfre8lIgEwool6gFc4_JZM_cGOcYDtbZQgwbnFsh4qvtrm8gXCcZ0PjvyB21CTz2g=w544-h544-l90-rj"
+    cover: "https://i.postimg.cc/qMr4pZvx/RBD.webp"
   },
   {
     title: "Fuego",
     artist: "RBD",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Rebelde/Fuego.mp3",
-    cover: "https://lh3.googleusercontent.com/2FPRvFa23vXHuXi07Bfre8lIgEwool6gFc4_JZM_cGOcYDtbZQgwbnFsh4qvtrm8gXCcZ0PjvyB21CTz2g=w544-h544-l90-rj"
+    cover: "https://i.postimg.cc/qMr4pZvx/RBD.webp"
   },
   {
     title: "Futuro Ex - Novio",
     artist: "RBD",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Rebelde/Futuro Ex - Novio.mp3",
-    cover: "https://lh3.googleusercontent.com/2FPRvFa23vXHuXi07Bfre8lIgEwool6gFc4_JZM_cGOcYDtbZQgwbnFsh4qvtrm8gXCcZ0PjvyB21CTz2g=w544-h544-l90-rj"
+    cover: "https://i.postimg.cc/qMr4pZvx/RBD.webp"
   },
   {
     title: "Otro Día Que Va",
     artist: "RBD",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Rebelde/Otro Día Que Va.mp3",
-    cover: "https://lh3.googleusercontent.com/2FPRvFa23vXHuXi07Bfre8lIgEwool6gFc4_JZM_cGOcYDtbZQgwbnFsh4qvtrm8gXCcZ0PjvyB21CTz2g=w544-h544-l90-rj"
+    cover: "https://i.postimg.cc/qMr4pZvx/RBD.webp"
   },
   {
     title: "Quando O Amor Acaba",
     artist: "RBD",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Rebelde/Quando O Amor Acaba.mp3",
-    cover: "https://lh3.googleusercontent.com/2FPRvFa23vXHuXi07Bfre8lIgEwool6gFc4_JZM_cGOcYDtbZQgwbnFsh4qvtrm8gXCcZ0PjvyB21CTz2g=w544-h544-l90-rj"
+    cover: "https://i.postimg.cc/qMr4pZvx/RBD.webp"
   },
   {
     title: "Querer-te",
     artist: "RBD",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Rebelde/Querer-te.mp3",
-    cover: "https://lh3.googleusercontent.com/2FPRvFa23vXHuXi07Bfre8lIgEwool6gFc4_JZM_cGOcYDtbZQgwbnFsh4qvtrm8gXCcZ0PjvyB21CTz2g=w544-h544-l90-rj"
+    cover: "https://i.postimg.cc/qMr4pZvx/RBD.webp"
   },
   {
     title: "Rebelde",
     artist: "RBD",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Rebelde/Rebelde - Versão Português.mp3",
-    cover: "https://lh3.googleusercontent.com/2FPRvFa23vXHuXi07Bfre8lIgEwool6gFc4_JZM_cGOcYDtbZQgwbnFsh4qvtrm8gXCcZ0PjvyB21CTz2g=w544-h544-l90-rj"
+    cover: "https://i.postimg.cc/qMr4pZvx/RBD.webp"
   },
   {
     title: "Salva-me",
     artist: "RBD",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Rebelde/Salva-me.mp3",
-    cover: "https://lh3.googleusercontent.com/2FPRvFa23vXHuXi07Bfre8lIgEwool6gFc4_JZM_cGOcYDtbZQgwbnFsh4qvtrm8gXCcZ0PjvyB21CTz2g=w544-h544-l90-rj"
+    cover: "https://i.postimg.cc/qMr4pZvx/RBD.webp"
   },
   {
     title: "Um Pouco Desse Amor",
     artist: "RBD",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Rebelde/Um Pouco Desse Amor.mp3",
-    cover: "https://lh3.googleusercontent.com/2FPRvFa23vXHuXi07Bfre8lIgEwool6gFc4_JZM_cGOcYDtbZQgwbnFsh4qvtrm8gXCcZ0PjvyB21CTz2g=w544-h544-l90-rj"
+    cover: "https://i.postimg.cc/qMr4pZvx/RBD.webp"
   }
 ];
