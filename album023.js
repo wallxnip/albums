c3ec0bd1d3e2a17812d5b2f05a8853b1f01d1a01
Apps <wallxnip@gmail.com>
2025-11-29 22:30:28 -0300

    Atualizar o album023.js

--- album023.js
+++ album023.js
@@ -4,48 +4,54 @@ const playlist522 = [
     title: "Porta aberta",
     artist: "Luka",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Luka (MIX)/Porta aberta.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/nL9tbTzd/luka.webp"
   },
   {
     title: "Tô nem aí",
     artist: "Luka",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Luka (MIX)/Tô nem aí.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/nL9tbTzd/luka.webp"
   },
   {
     title: "Sem resposta",
     artist: "Luka",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Luka (MIX)/Sem resposta.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/HkSNLMK2/luka.webp"
   },
   {
     title: "Céu de Diamantes",
     artist: "Luka",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Luka (MIX)/Céu de Diamantes.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/QMCdBpM5/luka.webp"
   },
   {
     title: "Nada leve",
     artist: "Luka",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Luka (MIX)/Nada leve.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/nL9tbTzd/luka.webp"
   },
   {
     title: "A aposta",
     artist: "Luka",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Luka (MIX)/A aposta.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/HkSNLMK2/luka.webp"
   },
   {
     title: "Você Me Olha",
     artist: "Luka",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Luka (MIX)/Você Me Olha.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/QMCdBpM5/luka.webp"
   },
   {
     title: "Difícil pra você",
     artist: "Luka",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Luka (MIX)/Difícil pra você.mp3",
-    cover: ""
+    cover: "https://i.postimg.cc/nL9tbTzd/luka.webp"
+  }
+  {
+    title: "Livro Aberto",
+    artist: "Luka",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Luka (MIX)/Livro Aberto.mp3",
+    cover: "https://i.postimg.cc/QMCdBpM5/luka.webp"
   }
 ];
