4bdc83ecedd0a7b4de19ec64a7cf7459984c1b00
Apps <wallxnip@gmail.com>
2025-11-29 22:24:29 -0300

    Criar album023.js

---
+++ album023.js
@@ -0,0 +1,51 @@
+// Luka Sem Resposta
+const playlist522 = [
+  {
+    title: "Porta aberta",
+    artist: "Luka",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Luka (MIX)/Porta aberta.mp3",
+    cover: ""
+  },
+  {
+    title: "Tô nem aí",
+    artist: "Luka",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Luka (MIX)/Tô nem aí.mp3",
+    cover: ""
+  },
+  {
+    title: "Sem resposta",
+    artist: "Luka",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Luka (MIX)/Sem resposta.mp3",
+    cover: ""
+  },
+  {
+    title: "Céu de Diamantes",
+    artist: "Luka",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Luka (MIX)/Céu de Diamantes.mp3",
+    cover: ""
+  },
+  {
+    title: "Nada leve",
+    artist: "Luka",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Luka (MIX)/Nada leve.mp3",
+    cover: ""
+  },
+  {
+    title: "A aposta",
+    artist: "Luka",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Luka (MIX)/A aposta.mp3",
+    cover: ""
+  },
+  {
+    title: "Você Me Olha",
+    artist: "Luka",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Luka (MIX)/Você Me Olha.mp3",
+    cover: ""
+  },
+  {
+    title: "Difícil pra você",
+    artist: "Luka",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Luka (MIX)/Difícil pra você.mp3",
+    cover: ""
+  }
+];
