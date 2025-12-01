71db08b59714ce3b3e864478f0927d7ed2dcc9b8
Apps <wallxnip@gmail.com>
2025-12-01 00:07:14 -0300

    Atualizar o album016.js

--- album016.js
+++ album016.js
@@ -4,14 +4,14 @@ const playlist515 = [
     title: "Um Novo Ser",
     artist: "Hikari de Jesus",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Oh Rei do Universo Jesus ✞/Um Novo Ser.mp3",
-    cover: "https://i.postimg.cc/63xhk0Hg/js.webp"
+    cover: "https://i.postimg.cc/FFdLycDz/js.webp"
   },
   
   {
     title: "Minha Verdadeira Sorte",
     artist: "Eterno Som",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Oh Rei do Universo Jesus ✞/Minha Verdadeira Sorte.mp3",
-    cover: "https://i.postimg.cc/63xhk0Hg/js.webp"
+    cover: "https://i.postimg.cc/FFdLycDz/js.webp"
   },
 
 
