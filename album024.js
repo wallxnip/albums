ca10836413b5f583ab281505b095c041c57a2b14
Apps <wallxnip@gmail.com>
2025-12-04 16:57:10 -0300

    Atualizar o album024.js

--- album024.js
+++ album024.js
@@ -1,9 +1,45 @@
 // Flash Back 
 const playlist523 = [
   {
-    title: "Porta aberta",
-    artist: "Luka",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Luka (MIX)/Porta aberta.mp3",
-    cover: "https://i.postimg.cc/nL9tbTzd/luka.webp"
+    title: "Girls Just Want to Have Fun",
+    artist: "Cyndi Lauper",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Flash Back (Clássico)/Girls Just Want to Have Fun.mp3",
+    cover: "https://i.postimg.cc/1tBxVFRd/Cyndi-Lauper.webp"
+  },
+  {
+    title: "Tarzan Boy",
+    artist: "Baltimora",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Flash Back (Clássico)/Tarzan Boy.mp3",
+    cover: "https://i.postimg.cc/XqPz9kMS/Baltimora.webp"
+  },
+  {
+    title: "Dreams (Will Come Alive)",
+    artist: "2 Brothers On The 4th Floor",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Flash Back (Clássico)/Dreams (Will Come Alive).mp3",
+    cover: "https://i.postimg.cc/hjjsMQ1d/2-Brothers-On-The-4th-Floor.webp"
+  },
+  {
+    title: "Time After Time",
+    artist: "Cyndi Lauper",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Flash Back (Clássico)/Time After Time.mp3",
+    cover: "https://i.postimg.cc/1tBxVFRd/Cyndi-Lauper.webp"
+  },
+  {
+    title: "Your Love",
+    artist: "The Outfield",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Flash Back (Clássico)/Your Love.mp3",
+    cover: "https://i.postimg.cc/HW8RCFbW/The-Outfield.webp"
+  },
+  {
+    title: "Like a Virgin",
+    artist: "Madonna",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Flash Back (Clássico)/Like a Virgin.mp3",
+    cover: "https://i.postimg.cc/zX0n2G3X/Like-a-Virgin.webp"
+  },
+  {
+    title: "Don't Stop Believin",
+    artist: "Journey",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Flash Back (Clássico)/Don't Stop Believin.mp3",
+    cover: "https://i.postimg.cc/TwD9ZMFj/Journey.webp"
   }
 ];
