270ba016002daba00e29ead9e4fec0f5a70dfaaa
Apps <wallxnip@gmail.com>
2025-12-04 12:11:50 -0300

    Criar album024.js

---
+++ album024.js
@@ -0,0 +1,9 @@
+// Flash Back 
+const playlist523 = [
+  {
+    title: "Porta aberta",
+    artist: "Luka",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Luka (MIX)/Porta aberta.mp3",
+    cover: "https://i.postimg.cc/nL9tbTzd/luka.webp"
+  }
+];
