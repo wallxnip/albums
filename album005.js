568c20a0a9db618be89f3a84bb0f77a49bde43e7
Apps <wallxnip@gmail.com>
2025-12-14 15:47:59 -0300

    Atualizar o album005.js

--- album005.js
+++ album005.js
@@ -4,135 +4,135 @@ const playlist504 = [
     title: "Universo Paralelo",
     artist: "Funk (MIX)",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Universo Paralelo.mp3",
-    cover: "https://i.postimg.cc/mk5jBn9y/Funk.webp"
+    cover: "https://i.postimg.cc/FFLJR7DV/Funk.webp"
   },
   {
     title: "Alegra",
     artist: "Funk (MIX)",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Alegra.mp3",
-    cover: "https://i.postimg.cc/mk5jBn9y/Funk.webp"
+    cover: "https://i.postimg.cc/FFLJR7DV/Funk.webp"
   },
   {
     title: "Amanheceu",
     artist: "Funk (MIX)",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Amanheceu.mp3",
-    cover: "https://i.postimg.cc/mk5jBn9y/Funk.webp"
+    cover: "https://i.postimg.cc/FFLJR7DV/Funk.webp"
   },
   {
     title: "Aprendi Maldade Com Essa Maldita",
     artist: "Funk (MIX)",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Aprendi Maldade Com Essa Maldita.mp3",
-    cover: "https://i.postimg.cc/mk5jBn9y/Funk.webp"
+    cover: "https://i.postimg.cc/FFLJR7DV/Funk.webp"
   },
   {
     title: "Bela Dupla",
     artist: "Funk (MIX)",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Bela Dupla.mp3",
-    cover: "https://i.postimg.cc/mk5jBn9y/Funk.webp"
+    cover: "https://i.postimg.cc/FFLJR7DV/Funk.webp"
   },
   {
     title: "É Que Eu Te Sarrei",
     artist: "Funk (MIX)",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/É Que Eu Te Sarrei.mp3",
-    cover: "https://i.postimg.cc/mk5jBn9y/Funk.webp"
+    cover: "https://i.postimg.cc/FFLJR7DV/Funk.webp"
   },
   {
     title: "Maldito Cupido",
     artist: "Funk (MIX)",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Maldito Cupido.mp3",
-    cover: "https://i.postimg.cc/mk5jBn9y/Funk.webp"
+    cover: "https://i.postimg.cc/FFLJR7DV/Funk.webp"
   },
   {
     title: "Não Dá pra Negar",
     artist: "Funk (MIX)",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Não Dá pra Negar.mp3",
-    cover: "https://i.postimg.cc/mk5jBn9y/Funk.webp"
+    cover: "https://i.postimg.cc/FFLJR7DV/Funk.webp"
   },
   {
     title: "O Que Que Tá Acontecendo",
     artist: "Funk (MIX)",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/O Que Que Tá Acontecendo.mp3",
-    cover: "https://i.postimg.cc/mk5jBn9y/Funk.webp"
+    cover: "https://i.postimg.cc/FFLJR7DV/Funk.webp"
   },
   {
     title: "Perdoa os Meus Pecados",
     artist: "Funk (MIX)",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Perdoa os Meus Pecados.mp3",
-    cover: "https://i.postimg.cc/mk5jBn9y/Funk.webp"
+    cover: "https://i.postimg.cc/FFLJR7DV/Funk.webp"
   },
   {
     title: "Plano Perfeito",
     artist: "Funk (MIX)",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Plano Perfeito.mp3",
-    cover: "https://i.postimg.cc/mk5jBn9y/Funk.webp"
+    cover: "https://i.postimg.cc/FFLJR7DV/Funk.webp"
   },
   {
     title: "Postura",
     artist: "Funk (MIX)",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Postura.mp3",
-    cover: "https://i.postimg.cc/mk5jBn9y/Funk.webp"
+    cover: "https://i.postimg.cc/FFLJR7DV/Funk.webp"
   },
   {
     title: "Quadradinho",
     artist: "Funk (MIX)",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Quadradinho.mp3",
-    cover: "https://i.postimg.cc/mk5jBn9y/Funk.webp"
+    cover: "https://i.postimg.cc/FFLJR7DV/Funk.webp"
   },
   {
     title: "Se Tu Quer Replay",
     artist: "Funk (MIX)",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Se Tu Quer Replay.mp3",
-    cover: "https://i.postimg.cc/mk5jBn9y/Funk.webp"
+    cover: "https://i.postimg.cc/FFLJR7DV/Funk.webp"
   },
   {
     title: "Segue o Baile",
     artist: "Funk (MIX)",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Segue o Baile.mp3",
-    cover: "https://i.postimg.cc/mk5jBn9y/Funk.webp"
+    cover: "https://i.postimg.cc/FFLJR7DV/Funk.webp"
   },
   {
     title: "Tatuagem da Borboleta",
     artist: "Funk (MIX)",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Tatuagem da Borboleta.mp3",
-    cover: "https://i.postimg.cc/mk5jBn9y/Funk.webp"
+    cover: "https://i.postimg.cc/FFLJR7DV/Funk.webp"
   },
   {
     title: "Tava Escrito nas Estrelas",
     artist: "Funk (MIX)",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Tava Escrito nas Estrelas.mp3",
-    cover: "https://i.postimg.cc/mk5jBn9y/Funk.webp"
+    cover: "https://i.postimg.cc/FFLJR7DV/Funk.webp"
   },
   {
     title: "Vou te Contar um Segredinho",
     artist: "Funk (MIX)",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Vou te Contar um Segredinho.mp3",
-    cover: "https://i.postimg.cc/mk5jBn9y/Funk.webp"
+    cover: "https://i.postimg.cc/FFLJR7DV/Funk.webp"
   },
 
   {
     title: "Olha Como Ela é Gata - Silicone",
     artist: "Funk (MIX)",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Olha Como Ela é Gata - Silicone.mp3",
-    cover: "https://i.postimg.cc/mk5jBn9y/Funk.webp"
+    cover: "https://i.postimg.cc/FFLJR7DV/Funk.webp"
   },
   {
     title: "Paraiso",
     artist: "Funk (MIX)",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Paraiso.mp3",
-    cover: "https://i.postimg.cc/mk5jBn9y/Funk.webp"
+    cover: "https://i.postimg.cc/FFLJR7DV/Funk.webp"
   },
   {
     title: "Ritmada do Fifty",
     artist: "Funk (MIX)",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Ritmada do Fifty.mp3",
-    cover: "https://i.postimg.cc/mk5jBn9y/Funk.webp"
+    cover: "https://i.postimg.cc/FFLJR7DV/Funk.webp"
   },
 
   {
     title: "Se Quiser Me Ver",
     artist: "Funk (MIX)",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Se Quiser Me Ver.mp3",
-    cover: "https://i.postimg.cc/mk5jBn9y/Funk.webp"
+    cover: "https://i.postimg.cc/FFLJR7DV/Funk.webp"
   },
   
 ];
