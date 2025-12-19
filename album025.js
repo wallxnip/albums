6c546676bec6f0f56a9f6b0d1797308b110a3548
Apps <wallxnip@gmail.com>
2025-12-19 13:23:14 -0300

    Criar album025.js

---
+++ album025.js
@@ -0,0 +1,64 @@
+const playlist524 = [
+  {
+    title: "Cheiro de Mato",
+    artist: "Gabi Saiury, Kotim & MC Paiva ZS",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Gabi Sayuri/Cheiro de Mato.mp3",
+    cover: "https://lh3.googleusercontent.com/3Z3v-mLTZ99X48pJbEqZg0njWCqtM3jLyAZsmfGaZLo6NHNf0JCNHnpJ4srXFYN-080rriauclckziI=w544-h544-l90-rj"
+  },
+
+  {
+    title: "Coração Quebrado",
+    artist: "Gabi Saiury",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Gabi Sayuri/Coração Quebrado.mp3",
+    cover: "https://lh3.googleusercontent.com/iruD8PV2u7uKl5MEznqjNjiXoS40NOJxmiqQR9GlGIBDKpS5QePYs8sdnbNdjBtl2WSvasm4zwFtleZ9Yg=w544-h544-l90-rj"
+  },
+
+  {
+    title: "Cê Não Tem Moral",
+    artist: "Gabi Saiury",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Gabi Sayuri/Cê Não Tem Moral.mp3",
+    cover: "https://lh3.googleusercontent.com/_Z7aQwisPt6bJ1sviCd7K5bPSmaF0_DYEs_-qc4VY32n3byiUVNlX8SeZz9JJP6R_g2nOshdGjpyHubWwg=w544-h544-l90-rj"
+  },
+
+  {
+    title: "Erro Vitalício",
+    artist: "Gabi Saiury, MC Paiva ZS & Paula Guilherme",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Gabi Sayuri/Erro Vitalício.mp3",
+    cover: "https://lh3.googleusercontent.com/-wUcqHhwql9AieXAsHvCpti1p0CRGu8Z5SIbax7KFTk9hOPNPtIn7bC8ETm8UraSLD8BV-g8oXxZMdIH=w544-h544-l90-rj"
+  },
+
+  {
+    title: "Fabricada na Roça",
+    artist: "Gabi Saiury",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Gabi Sayuri/Fabricada na Roça.mp3",
+    cover: "https://lh3.googleusercontent.com/8dpVCNgAmSWgf8ZjSbV6Bl-po-8Lqgjiix20Nvp5ixIUudK6fPmc4LogoMu6Y-6WllW5ysHTG9pSzUTPDQ=w544-h544-l90-rj"
+  },
+
+  {
+    title: "Galopada Violenta",
+    artist: "Gabi Saiury",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Gabi Sayuri/Galopada Violenta.mp3",
+    cover: "https://lh3.googleusercontent.com/V08hHYoIydhCO2XW3n1hTIpPT7L85GPU_S2097F6VuaUTfX8PmO7sTg9nYrOryeuyS6cdlbQjRY-JN0=w544-h544-l90-rj"
+  },
+
+  {
+    title: "Nos É Agro",
+    artist: "Gabi Saiury",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Gabi Sayuri/Nos É Agro.mp3",
+    cover: "https://lh3.googleusercontent.com/Odb2R-DXjwx9lk8muy-RD38KZsoA0S8XJWyYJg1m6OeVDVX1D8cxsupr2bMLVR-5ACIaJ1OsIhkWdgsk=w544-h544-l90-rj"
+  },
+
+  {
+    title: "Pulseira da Pandora",
+    artist: "Vitória Freitas & Gabi Saiury",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Gabi Sayuri/Pulseira da Pandora.mp3",
+    cover: "https://lh3.googleusercontent.com/G7DE6ZAmeW-GAQCvoKUjSSbcqPu6WHoYh4ViiE-tjnihWLdRNLUPDyakRPq5WCMJPCNepUdngtkzyxQ=w544-h544-l90-rj"
+  },
+
+  {
+    title: "Querendo ou Não",
+    artist: "Gabi Saiury, Japinha Conde & Jonatas Nascimento",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Gabi Sayuri/Querendo ou Não.mp3",
+    cover: "https://lh3.googleusercontent.com/OyyaVucCCKttbJj8bsm-VDhd1MOX9eHDL7vTvmXhU7Sja3Qcaim3ZgjRmcFtcCyA5uqRO1ntgRHvbQQ=w544-h544-l90-rj"
+  }
+];
