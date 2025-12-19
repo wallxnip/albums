a7680486bd4a9ff885800e79f2467e22be2bfe9b
Apps <wallxnip@gmail.com>
2025-12-19 12:24:36 -0300

    Atualizar o album005.js

--- album005.js
+++ album005.js
@@ -1,144 +1,227 @@
 // Funk (MIX)
 const playlist504 = [
-  {
-    title: "Toma Botadinha",
-    artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Toma Botadinha.mp3",
-    cover: "https://i.postimg.cc/y8g4Xwxy/funk.webp"
-  },
-  {
-    title: "Universo Paralelo",
-    artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Universo Paralelo.mp3",
-    cover: "https://i.postimg.cc/y8g4Xwxy/funk.webp"
-  },
-  {
+   {
     title: "Alegra",
-    artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Alegra.mp3",
-    cover: "https://i.postimg.cc/y8g4Xwxy/funk.webp"
+    artist: "MC Arizinho",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/MC Arizinho (LoveFunk)/Alegra.mp3",
+    cover: "https://lh3.googleusercontent.com/R-7VnbuxgTwCIpJKZXFyLZZXhrIm5q1X555SQyEdy1G-InnWSiB-73XGLb-APdcSKnNqdr9R-9RMvPUnbQ=w544-h544-l90-rj"
   },
+
   {
     title: "Amanheceu",
-    artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Amanheceu.mp3",
-    cover: "https://i.postimg.cc/y8g4Xwxy/funk.webp"
+    artist: "MC Arizinho",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/MC Arizinho (LoveFunk)/Amanheceu.mp3",
+    cover: "https://lh3.googleusercontent.com/cecA_P-AyPbSSnSlDy7QfbgXumVXEpy7RRrB8WWUMdJ5UUiesPu6cMBvbEh4QUrkuU7IeHqbQotaI8iU=w544-h544-l90-rj"
   },
+
   {
     title: "Aprendi Maldade Com Essa Maldita",
-    artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Aprendi Maldade Com Essa Maldita.mp3",
-    cover: "https://i.postimg.cc/y8g4Xwxy/funk.webp"
+    artist: "MC Arizinho",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/MC Arizinho (LoveFunk)/Aprendi Maldade Com Essa Maldita.mp3",
+    cover: "https://lh3.googleusercontent.com/bj0JW0LlYNiUI_O2wHazEzsvT5cYHJsgJjUBtUUqqndTdFs8bSl7QMioTm-0r6Xdn2DdSIUJkyU1JP0E=w544-h544-l90-rj"
   },
+
+  {
+    title: "Bate Palma Com a Bunda",
+    artist: "MC Arizinho",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/MC Arizinho (LoveFunk)/Bate Palma Com a Bunda.mp3",
+    cover: "https://lh3.googleusercontent.com/jrpUWdTJkMIA1dTfWdatKX_XQiq3QxPs4ovdoXSCVtMl2l8_KJktOf_ZFXRC8wMOU2CJG78LrkaLNQs=w544-h544-l90-rj"
+  },
+
   {
     title: "Bela Dupla",
-    artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Bela Dupla.mp3",
-    cover: "https://i.postimg.cc/y8g4Xwxy/funk.webp"
+    artist: "MC Arizinho",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/MC Arizinho (LoveFunk)/Bela Dupla.mp3",
+    cover: "https://lh3.googleusercontent.com/YVFLhfWPqJNwxd5Kzy6i7ormcxpVXnH4e2MEEYBI112X1iXyBwtLjRicSpsGsISdAvgUs3v6pRBzip0=w544-h544-l90-rj"
   },
+
   {
-    title: "É Que Eu Te Sarrei",
-    artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/É Que Eu Te Sarrei.mp3",
-    cover: "https://i.postimg.cc/y8g4Xwxy/funk.webp"
+    title: "Climinha De Chuva",
+    artist: "MC Arizinho",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/MC Arizinho (LoveFunk)/Climinha De Chuva.mp3",
+    cover: "https://lh3.googleusercontent.com/x6BUHcEKh1swGhyuwTDqW61Zi8ca1Wb8nap5BC-FN5tW4iyBFUnu_q1eBmRJR-XXnRrKgF6DAhQRSBY=w544-h544-l90-rj"
   },
+
   {
-    title: "Maldito Cupido",
-    artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Maldito Cupido.mp3",
-    cover: "https://i.postimg.cc/y8g4Xwxy/funk.webp"
+    title: "Ela é Gata Demais",
+    artist: "MC Arizinho",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/MC Arizinho (LoveFunk)/Ela é Gata Demais.mp3",
+    cover: "https://lh3.googleusercontent.com/oc_4z2wR9cgQLCPO55L_zQtFsOdlgzO5hxIRb1ohJnGLCgRJfjnu4QBVDx8OS65FGLT88dEkEQmhvuOmew=w544-h544-l90-rj"
   },
+
+  {
+    title: "Então Joga o Bundão",
+    artist: "MC Arizinho",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/MC Arizinho (LoveFunk)/Então Joga o Bundão.mp3",
+    cover: "https://lh3.googleusercontent.com/oc_4z2wR9cgQLCPO55L_zQtFsOdlgzO5hxIRb1ohJnGLCgRJfjnu4QBVDx8OS65FGLT88dEkEQmhvuOmew=w544-h544-l90-rj"
+  },
+
+  {
+    title: "Espetacular",
+    artist: "MC Arizinho",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/MC Arizinho (LoveFunk)/Espetacular.mp3",
+    cover: "https://lh3.googleusercontent.com/0B7RX8YtWaTdKpSgwop9GBr4JlQ_xev6BEsN1CfjsozoaSBaNpX1WZKq1u2P9Y1i0vOJts_4-OcPRvMH=w544-h544-l90-rj"
+  },
+
+  {
+    title: "Mais Tarde Tem Baile",
+    artist: "MC Arizinho",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/MC Arizinho (LoveFunk)/Mais Tarde Tem Baile.mp3",
+    cover: "https://lh3.googleusercontent.com/Ma1BlvA1GpslMO3axUfGmJjt8MvKFdr0qeK99lJsDxI-lXr2wrBbBEr1HLnz3dBNdl1uK0PzMwTXwuoa=w544-h544-l90-rj"
+  },
+
+  {
+    title: "Nem Perguntei Seu Nome",
+    artist: "MC Arizinho",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/MC Arizinho (LoveFunk)/Nem Perguntei Seu Nome.mp3",
+    cover: "https://lh3.googleusercontent.com/yOV1JaSYaaongfDOMSx2NwTuAnO8qsd_Jh7256UOrUuFibf3xgBqZBKJRxB_QNshKRjGVUv2wT8t3wc0=w544-h544-l90-rj"
+  },
+
   {
     title: "Não Dá pra Negar",
-    artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Não Dá pra Negar.mp3",
-    cover: "https://i.postimg.cc/y8g4Xwxy/funk.webp"
+    artist: "MC Arizinho",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/MC Arizinho (LoveFunk)/Não Dá pra Negar.mp3",
+    cover: "https://lh3.googleusercontent.com/OyXDLUBrqq-ETKqAJcH6mMJ643hq9LTEb7LiZ9kqTAW9Px_STh1mgwIrTxGJLr346oPIg2Jw_fSZ-Ek=w544-h544-l90-rj"
   },
+
   {
-    title: "O Que Que Tá Acontecendo",
-    artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/O Que Que Tá Acontecendo.mp3",
-    cover: "https://i.postimg.cc/y8g4Xwxy/funk.webp"
+    title: "O Que Tá Acontecendo",
+    artist: "MC Arizinho",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/MC Arizinho (LoveFunk)/O Que Tá Acontecendo.mp3",
+    cover: "https://lh3.googleusercontent.com/m_36hT0_JjQm1KCvtgls8pild_MNqG1_iWBBK4haP7bvAO97zFJ_oLZ2xZS5mBH0iOpCzXLijtxC8tTbLg=w544-h544-l90-rj"
   },
+  
   {
-    title: "Perdoa os Meus Pecados",
-    artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Perdoa os Meus Pecados.mp3",
-    cover: "https://i.postimg.cc/y8g4Xwxy/funk.webp"
-  },
-  {
-    title: "Plano Perfeito",
-    artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Plano Perfeito.mp3",
-    cover: "https://i.postimg.cc/y8g4Xwxy/funk.webp"
-  },
-  {
-    title: "Postura",
-    artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Postura.mp3",
-    cover: "https://i.postimg.cc/y8g4Xwxy/funk.webp"
-  },
-  {
-    title: "Quadradinho",
-    artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Quadradinho.mp3",
-    cover: "https://i.postimg.cc/y8g4Xwxy/funk.webp"
-  },
-  {
-    title: "Se Tu Quer Replay",
-    artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Se Tu Quer Replay.mp3",
-    cover: "https://i.postimg.cc/y8g4Xwxy/funk.webp"
-  },
-  {
-    title: "Segue o Baile",
-    artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Segue o Baile.mp3",
-    cover: "https://i.postimg.cc/y8g4Xwxy/funk.webp"
-  },
-  {
-    title: "Tatuagem da Borboleta",
-    artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Tatuagem da Borboleta.mp3",
-    cover: "https://i.postimg.cc/y8g4Xwxy/funk.webp"
-  },
-  {
-    title: "Tava Escrito nas Estrelas",
-    artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Tava Escrito nas Estrelas.mp3",
-    cover: "https://i.postimg.cc/y8g4Xwxy/funk.webp"
-  },
-  {
-    title: "Vou te Contar um Segredinho",
-    artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Vou te Contar um Segredinho.mp3",
-    cover: "https://i.postimg.cc/y8g4Xwxy/funk.webp"
+    title: "O Que Tá Acontecendo 2",
+    artist: "MC Arizinho",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/MC Arizinho (LoveFunk)/O Que Tá Acontecendo 2.mp3",
+    cover: "https://lh3.googleusercontent.com/iVgApYcHlVC5IXhEZx3hWDeblIu3YaM0E4-_x91wF6d8O-B2vly6ZUnnDB2Wxi7qDm2MMsJ7Kpo2w_Vq1w=w544-h544-l90-rj"
   },
 
   {
     title: "Olha Como Ela é Gata - Silicone",
-    artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Olha Como Ela é Gata - Silicone.mp3",
-    cover: "https://i.postimg.cc/y8g4Xwxy/funk.webp"
+    artist: "MC Arizinho",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/MC Arizinho (LoveFunk)/Olha Como Ela é Gata - Silicone.mp3",
+    cover: "https://lh3.googleusercontent.com/XUC6zH9qgFdptRAFZYel-4WtNu2chT6jdILKVwP3GJUE9hqSlN0zHPZYnH1DGk7BB6YlWzH3bgpIbiU=w544-h544-l90-rj"
   },
+
   {
-    title: "Paraiso",
-    artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Paraiso.mp3",
-    cover: "https://i.postimg.cc/y8g4Xwxy/funk.webp"
+    title: "Paraíso",
+    artist: "MC Arizinho",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/MC Arizinho (LoveFunk)/Paraíso.mp3",
+    cover: "https://lh3.googleusercontent.com/LaARqyyJ8eC01QxY4R79NLxzSVEU4IYai3tdmEJhmPMSFLDgITwoqvfznmh4PNXR6LLO_R0HHq7yT_LC=w544-h544-l90-rj"
   },
+
   {
-    title: "Ritmada do Fifty",
-    artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Ritmada do Fifty.mp3",
-    cover: "https://i.postimg.cc/y8g4Xwxy/funk.webp"
+    title: "Plano Perfeito",
+    artist: "MC Arizinho",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/MC Arizinho (LoveFunk)/Plano Perfeito.mp3",
+    cover: "https://lh3.googleusercontent.com/Czb1fKMlMY7apn0HJ5800CPKhUjh8I2J8BEVSvYAn1kOGDEvuoJsHM7H4669Tn9w675mwn9ZCa_UHVE=w544-h544-l90-rj"
+  },
+
+  {
+    title: "Postura",
+    artist: "MC Arizinho",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/MC Arizinho (LoveFunk)/Postura.mp3",
+    cover: "https://lh3.googleusercontent.com/0g1c1ui7xWv1CWzvuGcNYDKV4mCuiNn6iMI-wl8e0MVt0tzG7DzOVsG3U_IUVT6D0LRbsoKvW3w3dig=w544-h544-l90-rj"
+  },
+
+  {
+    title: "Quadradinho",
+    artist: "MC Arizinho",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/MC Arizinho (LoveFunk)/Quadradinho.mp3",
+    cover: "https://lh3.googleusercontent.com/q9QYMUgDnN8wvxge33_HKIk-XJ45L4a_fCr28q4P5DXWeVaX5QqO5IpSXPdsvtU9y9TpYE8vQmCVFs4maA=w544-h544-l90-rj"
   },
 
   {
     title: "Se Quiser Me Ver",
-    artist: "Funk (MIX)",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Funk (MIX)/Se Quiser Me Ver.mp3",
-    cover: "https://i.postimg.cc/y8g4Xwxy/funk.webp"
+    artist: "MC Arizinho",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/MC Arizinho (LoveFunk)/Se Quiser Me Ver.mp3",
+    cover: "https://lh3.googleusercontent.com/tYW1DI00SeDzfymkuLe5pv1iY9S6i-eHx60obM4eVn9lWy-3QRUAB-9bw9I1FNA_5qEZpO2tNx4qBMVa=w544-h544-l90-rj"
   },
+
+  {
+    title: "Se Tu Quer Replay",
+    artist: "MC Arizinho",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/MC Arizinho (LoveFunk)/Se Tu Quer Replay.mp3",
+    cover: "https://lh3.googleusercontent.com/ssAasgaFoY48iBBK-GvYMrLWk5ISUdAONce6YM_lEIcOs_80OdMO92EIz85cxcVzHrxte3h7dtTBuEUE=w544-h544-l90-rj"
+  },
+
+  {
+    title: "Segue o Baile",
+    artist: "MC Arizinho",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/MC Arizinho (LoveFunk)/Segue o Baile.mp3",
+    cover: "https://lh3.googleusercontent.com/1wWGVwclvZt6ImwZ-PasmX5RI9lz85kjvIVihFwBIUP9Sl5uqoaiyRoxurnSgbkmRn1RGClnm4tOPJhK=w544-h544-l90-rj"
+  },
+
+  {
+    title: "Sequência de Botadão",
+    artist: "MC Arizinho",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/MC Arizinho (LoveFunk)/Sequência de Botadão.mp3",
+    cover: "https://lh3.googleusercontent.com/oc_4z2wR9cgQLCPO55L_zQtFsOdlgzO5hxIRb1ohJnGLCgRJfjnu4QBVDx8OS65FGLT88dEkEQmhvuOmew=w544-h544-l90-rj"
+  },
+
+  {
+    title: "Tatuagem da Borboleta",
+    artist: "MC Arizinho",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/MC Arizinho (LoveFunk)/Tatuagem da Borboleta.mp3",
+    cover: "https://lh3.googleusercontent.com/oYtCC-tMIKwiDwB64q9l06UbfVy0tU6ezLCWwZMuBULT_I5YxFX3jZSI0YmULg3V9igJ5zWSkGT5OB9I3w=w544-h544-l90-rj"
+  },
+
+  {
+    title: "Tava Escrito nas Estrelas",
+    artist: "MC Arizinho",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/MC Arizinho (LoveFunk)/Tava Escrito nas Estrelas.mp3",
+    cover: "https://lh3.googleusercontent.com/Ggbt9BbsuFghahO54NkCdt4PJioxgT-vA9pn6wjCcawSTq-fh7VK0KSZ1GL14JKtDAP5DJnd4Vl3JGLAsw=w544-h544-l90-rj"
+  },
+
+  {
+    title: "Te Respeitei",
+    artist: "MC Arizinho",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/MC Arizinho (LoveFunk)/Te Respeitei.mp3",
+    cover: "https://lh3.googleusercontent.com/FFeppIUt-NXinZFojwLiYB0tkaOuGeQJOXXuTI2_SLZPHi6mqbip8G2DU7et-WjtpiCnXac3u15biSg=w544-h544-l90-rj"
+  },
+
+  {
+    title: "Te Sorri No Baile",
+    artist: "Mc Pepeu, Mc Arizinho, Vitin Do MT, MC Gueguel",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/MC Arizinho (LoveFunk)/Te Sorri No Baile.mp3",
+    cover: "https://lh3.googleusercontent.com/1znyXFBbW2kpe5aBOiyfdIyjjm0-FghSwSgcLEB67jYihHRoh0Z2j3MTPy_yzoFx_7oJD_UoA_1jEtq3=w544-h544-l90-rj"
+  },
+
+  {
+    title: "Toma Botadinha",
+    artist: "MC Arizinho",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/MC Arizinho (LoveFunk)/Toma Botadinha.mp3",
+    cover: "https://lh3.googleusercontent.com/Nv0yUX8Fh3qsXQYuuF01qT1LjT-4-BsZR-t8N0YoWD5OF24xDTvJWQYbGmJ4lE9cXynSNnh9md13gsjR=w544-h544-l90-rj"
+  },
+
+  {
+    title: "Vem pro Seu Novin",
+    artist: "Mc Arizinho, Mc Negralha, Dj Kik Prod",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/MC Arizinho (LoveFunk)/Vem pro Seu Novin.mp3",
+    cover: "https://lh3.googleusercontent.com/eD_21kE8HaXf48cVFzUg1U72fjwyGd2tyLu83cul84Wnv9WIwL-uZcc9skQoGG2qAT--E_P1w34R5Tse=w544-h544-l90-rj"
+  },
+
+  {
+    title: "Você Representa",
+    artist: "Mc Arizinho, DJ TAK VADIÃO, Vitin do Mt",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/MC Arizinho (LoveFunk)/Você Representa.mp3",
+    cover: "https://lh3.googleusercontent.com/EmbKCSMtC_7NK4eMpEn2mOe9WDkUcUAqwccyN0CuFyhqfB9YR50CRWgs-09B7fGMHVRFxMdhHaEXcslI=w544-h544-l90-rj"
+  },
+
+  {
+    title: "É Que Eu Te Sarrei,
+    artist: "DJ THG, Mc Arizinho, Tavin",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/MC Arizinho (LoveFunk)/É Que Eu Te Sarrei.mp3",
+    cover: "https://lh3.googleusercontent.com/m-skSa-8rx1XeiLed9BQEpYm3TCDHRHxfl4d8UmEqKMos2CqqluMOblm3KhlW4LNLvHdMmlwBPvzNEFb0g=w544-h544-l90-rj"
+  },
+
+  {
+    title: "Único Erro",
+    artist: "Mc Arizinho, DJ TAK VADIÃO",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/MC Arizinho (LoveFunk)/Único Erro.mp3",
+    cover: "https://lh3.googleusercontent.com/6qF_A3xxSt0MYaol5MEqdjsIf_i2Olr5tUTvGIiNZqvn87iRqJYqWsjA8Is4Gr93rVkZgmkoVTK2xTfkVw=w544-h544-l90-rj"
+  }
   
 ];
