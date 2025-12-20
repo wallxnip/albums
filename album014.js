446340be16213477d801e4b0b99d202ac8a4342f
Apps <wallxnip@gmail.com>
2025-12-20 12:32:05 -0300

    Atualizar o album014.js

--- album014.js
+++ album014.js
@@ -6,8 +6,12 @@ const playlist513 = [
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Latina/Sorry.mp3",
     cover: "https://i.postimg.cc/g0DQcZxR/Latina.webp"
   },
-  
-  
+  {
+    title: "Tusa",
+    artist: "Karol",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/Tusa.mp3",
+    cover: "https://i.postimg.cc/W16f1s6Y/Tusa.webp"
+  },
   
   
 ];
