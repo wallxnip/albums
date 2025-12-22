ec2cbd5082cb87de55d430ae1eb6f8a4924c80c7
Apps <wallxnip@gmail.com>
2025-12-22 14:16:28 -0300

    Atualizar o album027.js

--- album027.js
+++ album027.js
@@ -1,10 +1,9 @@
+// KLB
 const playlist526 = [
   {
     title: "Pane no Sistema",
     artist: "Grego do Piseiro & Paulo Pires",
-    
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Grego do Piseiro/Pane no Sistema.mp3",
-    
     cover: "https://lh3.googleusercontent.com/CwFnVlWDyolBLFyrzRX9JEdE52gmzdjDKGG3wotLNb8hISPOWFrmHJ-Nm5fkMH94T36-apTnvykwTos6Dw=w544-h544-l90-rj"
   }
 ];
