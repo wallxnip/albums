98403d0c6123ffe4de93deaf7530858cf6fc956d
Apps <wallxnip@gmail.com>
2025-12-22 14:11:08 -0300

    Atualizar o album027.js

--- album027.js
+++ album027.js
@@ -1,57 +1,6 @@
-const playlist525 = [
-  {
-    title: "Aquele Love",
-    
-    artist: "Grego do Piseiro",
-    
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Grego do Piseiro/Aquele Love.mp3",
-    
-    cover: "https://lh3.googleusercontent.com/fGWWEBHU2QOFRFxkEzR482sU3jV1V9H1DdF-4uQukD1yT6_DgWqTfqqcieEySaNyABgFVY1CKkX6xEtV=w544-h544-l90-rj"
-  },
-  
-  {
-    title: "Casei com a Revoada",
-    
-    artist: "Grego do Piseiro",
-    
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Grego do Piseiro/Casei com a Revoada.mp3",
-    
-    cover: "https://lh3.googleusercontent.com/8SdCdTPZS48PSTY6BtkPwGiT5v3zhukbSB656iYoDGcc8grxjFf-qaBejLvZ88qReXnh6RJqlsiSQGwI=w544-h544-l90-rj"
-  },
-  
-  {
-    title: "Danada Danadinha",
-    
-    artist: "Grego do Piseiro",
-    
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Grego do Piseiro/Danada Danadinha.mp3",
-    
-    cover: "https://lh3.googleusercontent.com/0110Zu_fjz3tZSl51JNfzzZpxSIj2BpBRDq7P9Tg9LAJPlqAWugckpaZfPWCe_DyP37lrwMMjGTBlEq7=w544-h544-l90-rj"
-  },
-  
-  {
-    title: "Me Diz Quando Vem",
-    
-    artist: "Grego do Piseiro",
-    
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Grego do Piseiro/Me Diz Quando Vem.mp3",
-    
-    cover: "https://lh3.googleusercontent.com/F0PTsvp5WoqjYeUqvT5B_sY6YH4WJ5CJ2dA7Wjky2KA5fB7ut8FJ8KFPrVxXXLS5eXsUmr2OB5WfZBs=w544-h544-l90-rj"
-  },
-  
-  {
-    title: "Pane no Sistema 2",
-    
-    artist: "Grego do Piseiro",
-    
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Grego do Piseiro/Pane no Sistema 2.mp3",
-    
-    cover: "https://lh3.googleusercontent.com/lXy9ON9HsGVRdWiM1yApd1WASbiqrhnF58WC9PdBEtDTLQPNLyaWDETzcVgoD3-JkTClWoy9J9KaL5KN=w544-h544-l90-rj"
-  },
-  
+const playlist526 = [
   {
     title: "Pane no Sistema",
-    
     artist: "Grego do Piseiro & Paulo Pires",
     
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Grego do Piseiro/Pane no Sistema.mp3",
