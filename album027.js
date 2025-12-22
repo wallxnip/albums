112768343453701b4430568a02a34d88a06ab9ed
Apps <wallxnip@gmail.com>
2025-12-22 15:06:18 -0300

    Atualizar o album027.js

--- album027.js
+++ album027.js
@@ -1,9 +1,39 @@
 // KLB
 const playlist526 = [
   {
+    title: "Giuliana",
+    artist: "KLB",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/KLB/Giuliana.mp3",
+    cover: "https://i.postimg.cc/VkDm8zCb/klb.webp"
+  },
+  {
+    title: "Porque Tem Que Ser Assim",
+    artist: "KLB",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/KLB/Porque Tem Que Ser Assim.mp3",
+    cover: "https://i.postimg.cc/VkDm8zCb/klb.webp"
+  },
+  {
+    title: "Olhar 43",
+    artist: "KLB",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/KLB/Olhar 43.mp3",
+    cover: "https://i.postimg.cc/VkDm8zCb/klb.webp"
+  },
+  {
+    title: "Minha Timidez",
+    artist: "KLB",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/KLB/Minha Timidez.mp3",
+    cover: "https://i.postimg.cc/VkDm8zCb/klb.webp"
+  },
+  {
     title: "A Cada Dez Palavras",
     artist: "KLB",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/KLB/A Cada Dez Palavras.mp3",
+    cover: "https://i.postimg.cc/VkDm8zCb/klb.webp"
+  },
+  {
+    title: "Um Anjo",
+    artist: "KLB",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/KLB/Um Anjo.mp3",
     cover: "https://i.postimg.cc/VkDm8zCb/klb.webp"
   },
   {
@@ -25,39 +55,9 @@ const playlist526 = [
     cover: "https://i.postimg.cc/VkDm8zCb/klb.webp"
   },
   {
-    title: "Giuliana",
-    artist: "KLB",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/KLB/Giuliana.mp3",
-    cover: "https://i.postimg.cc/VkDm8zCb/klb.webp"
-  },
-  {
-    title: "Minha Timidez",
-    artist: "KLB",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/KLB/Minha Timidez.mp3",
-    cover: "https://i.postimg.cc/VkDm8zCb/klb.webp"
-  },
-  {
     title: "Obsessão",
     artist: "KLB",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/KLB/Obsessão.mp3",
-    cover: "https://i.postimg.cc/VkDm8zCb/klb.webp"
-  },
-  {
-    title: "Olhar 43",
-    artist: "KLB",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/KLB/Olhar 43.mp3",
-    cover: "https://i.postimg.cc/VkDm8zCb/klb.webp"
-  },
-  {
-    title: "Porque Tem Que Ser Assim",
-    artist: "KLB",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/KLB/Porque Tem Que Ser Assim.mp3",
-    cover: "https://i.postimg.cc/VkDm8zCb/klb.webp"
-  },
-  {
-    title: "Um Anjo",
-    artist: "KLB",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/KLB/Um Anjo.mp3",
     cover: "https://i.postimg.cc/VkDm8zCb/klb.webp"
   }
 ];
