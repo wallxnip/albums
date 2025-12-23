3c69b233763fa2b6e8e3035fb1d98d05df60e49c
Apps <wallxnip@gmail.com>
2025-12-23 12:20:44 -0300

    Atualizar o album028.js

--- album028.js
+++ album028.js
@@ -4,48 +4,48 @@ const playlist527 = [
     title: "190",
     artist: "Iohannes Imperador",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Iohannes Imperador/190.mp3",
-    cover: "https://i.imgur.com/9ZQZQ0n.jpg"
+    cover: "https://i.postimg.cc/cLrbL0p8/ihoranes.webp"
   },
   {
     title: "Batom Vermelho",
     artist: "Iohannes Imperador",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Iohannes Imperador/Batom Vermelho.mp3",
-    cover: "https://i.imgur.com/9ZQZQ0n.jpg"
+    cover: "https://i.postimg.cc/cLrbL0p8/ihoranes.webp"
   },
   {
     title: "Ele Não Merece Você",
     artist: "Iohannes Imperador",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Iohannes Imperador/Ele Não Merece Você.mp3",
-    cover: "https://i.imgur.com/9ZQZQ0n.jpg"
+    cover: "https://i.postimg.cc/cLrbL0p8/ihoranes.webp"
   },
   {
     title: "Férias em Las Vegas",
     artist: "Iohannes Imperador",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Iohannes Imperador/Férias em Las Vegas.mp3",
-    cover: "https://i.imgur.com/9ZQZQ0n.jpg"
+    cover: "https://i.postimg.cc/cLrbL0p8/ihoranes.webp"
   },
   {
     title: "O Lobo",
     artist: "Iohannes Imperador",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Iohannes Imperador/O Lobo.mp3",
-    cover: "https://i.imgur.com/9ZQZQ0n.jpg"
+    cover: "https://i.postimg.cc/cLrbL0p8/ihoranes.webp"
   },
   {
     title: "Se Afaste de Mim",
     artist: "Iohannes Imperador",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Iohannes Imperador/Se Afaste de Mim.mp3",
-    cover: "https://i.imgur.com/9ZQZQ0n.jpg"
+    cover: "https://i.postimg.cc/cLrbL0p8/ihoranes.webp"
   },
   {
     title: "Sou Louco Por Ela",
     artist: "Iohannes Imperador",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Iohannes Imperador/Sou Louco Por Ela.mp3",
-    cover: "https://i.imgur.com/9ZQZQ0n.jpg"
+    cover: "https://i.postimg.cc/cLrbL0p8/ihoranes.webp"
   },
   {
     title: "Volta Pra Mim",
     artist: "Iohannes Imperador",
     file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Iohannes Imperador/Volta Pra Mim.mp3",
-    cover: "https://i.imgur.com/9ZQZQ0n.jpg"
+    cover: "https://i.postimg.cc/cLrbL0p8/ihoranes.webp"
   }
 ];
