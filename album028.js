a91d680f41bd32b1d3d369a1ad8195785aebba08
Apps <wallxnip@gmail.com>
2025-12-23 12:15:58 -0300

    Criar album028.js

---
+++ album028.js
@@ -0,0 +1,51 @@
+// Iohannes Imperador
+const playlist527 = [
+  {
+    title: "190",
+    artist: "Iohannes Imperador",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Iohannes Imperador/190.mp3",
+    cover: "https://i.imgur.com/9ZQZQ0n.jpg"
+  },
+  {
+    title: "Batom Vermelho",
+    artist: "Iohannes Imperador",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Iohannes Imperador/Batom Vermelho.mp3",
+    cover: "https://i.imgur.com/9ZQZQ0n.jpg"
+  },
+  {
+    title: "Ele Não Merece Você",
+    artist: "Iohannes Imperador",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Iohannes Imperador/Ele Não Merece Você.mp3",
+    cover: "https://i.imgur.com/9ZQZQ0n.jpg"
+  },
+  {
+    title: "Férias em Las Vegas",
+    artist: "Iohannes Imperador",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Iohannes Imperador/Férias em Las Vegas.mp3",
+    cover: "https://i.imgur.com/9ZQZQ0n.jpg"
+  },
+  {
+    title: "O Lobo",
+    artist: "Iohannes Imperador",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Iohannes Imperador/O Lobo.mp3",
+    cover: "https://i.imgur.com/9ZQZQ0n.jpg"
+  },
+  {
+    title: "Se Afaste de Mim",
+    artist: "Iohannes Imperador",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Iohannes Imperador/Se Afaste de Mim.mp3",
+    cover: "https://i.imgur.com/9ZQZQ0n.jpg"
+  },
+  {
+    title: "Sou Louco Por Ela",
+    artist: "Iohannes Imperador",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Iohannes Imperador/Sou Louco Por Ela.mp3",
+    cover: "https://i.imgur.com/9ZQZQ0n.jpg"
+  },
+  {
+    title: "Volta Pra Mim",
+    artist: "Iohannes Imperador",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Iohannes Imperador/Volta Pra Mim.mp3",
+    cover: "https://i.imgur.com/9ZQZQ0n.jpg"
+  }
+];
