6dfc25055f94d0f094706dc411b311dc38ff82f3
Apps <wallxnip@gmail.com>
2025-12-25 17:12:12 -0300

    Atualizar o album029.js

--- album029.js
+++ album029.js
@@ -1,1 +1,9 @@
-https://lh3.googleusercontent.com/yFXbE2MBX5KJmDVhGWWMz8jMqup9T9nLzTc6iWG1ENTkZb-JMCbhKZUKhYkwRv_-yzANjWKkkfJD0EXQ=w544-h544-l90-rj
+// Iohannes Imperador
+const playlist527 = [
+  {
+    title: "190",
+    artist: "Iohannes Imperador",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Iohannes Imperador/190.mp3",
+    cover: "https://lh3.googleusercontent.com/wGlM3Iy2hpWgMuj7ZrL_py3XGOAERS7nm1w1g3eyLe2hNOnVf9olYIwrkqS5qzzymouQxnXMRrgHfFhD=w544-h544-l90-rj"
+  },
+];
