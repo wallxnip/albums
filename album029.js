af4abec73897b9720a54c09c2e4fc5cd4c9928a6
Apps <wallxnip@gmail.com>
2025-12-25 17:45:46 -0300

    Atualizar o album029.js

--- album029.js
+++ album029.js
@@ -1,9 +1,51 @@
-// Iohannes Imperador
+// Milsinho Toque Dez
 const playlist528 = [
   {
-    title: "190",
-    artist: "Iohannes Imperador",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Iohannes Imperador/190.mp3",
+    title: "Antes de Ir",
+    artist: "Milsinho Toque Dez",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Toque Dez/Antes de Ir.mp3",
+    cover: "https://lh3.googleusercontent.com/yFXbE2MBX5KJmDVhGWWMz8jMqup9T9nLzTc6iWG1ENTkZb-JMCbhKZUKhYkwRv_-yzANjWKkkfJD0EXQ=w544-h544-l90-rj"
+  },
+  {
+    title: "Bem Mais Que Eu",
+    artist: "Milsinho Toque Dez",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Toque Dez/Bem mais que eu.mp3",
+    cover: "https://lh3.googleusercontent.com/A5P8syU_D0iwghdja2Xt43DM-izKGXjHH3DiPpSiccYMT82J53pNHT_idV49bJw_indjrU3P-4dpMyII=w544-h544-l90-rj"
+  },
+  {
+    title: "Cena de Amor",
+    artist: "Milsinho Toque Dez",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Toque Dez/Cena de Amor.mp3",
+    cover: "https://lh3.googleusercontent.com/uB6kJXXz7dcbVcqh-JgLUuAaeIiFOEkTIwsOL-m7hOil2vF80sGVQtOydGG6TTeHvsb0TfVjj0Je2s8C=w544-h544-l90-rj"
+  },
+  {
+    title: "Pensa Direito",
+    artist: "Milsinho Toque Dez",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Toque Dez/Pensa direito.mp3",
     cover: "https://lh3.googleusercontent.com/wGlM3Iy2hpWgMuj7ZrL_py3XGOAERS7nm1w1g3eyLe2hNOnVf9olYIwrkqS5qzzymouQxnXMRrgHfFhD=w544-h544-l90-rj"
   },
+  {
+    title: "Sou Sua Recaída",
+    artist: "Milsinho Toque Dez",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Toque Dez/Sou Sua Recaída.mp3",
+    cover: "https://i.imgur.com/9ZQZQ0n.jpg"
+  },
+  {
+    title: "Traumatizei",
+    artist: "Milsinho Toque Dez",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Toque Dez/Tramautizei.mp3",
+    cover: "https://lh3.googleusercontent.com/yFXbE2MBX5KJmDVhGWWMz8jMqup9T9nLzTc6iWG1ENTkZb-JMCbhKZUKhYkwRv_-yzANjWKkkfJD0EXQ=w544-h544-l90-rj"
+  },
+  {
+    title: "Vaqueira",
+    artist: "Milsinho Toque Dez",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Toque Dez/Vaqueira.mp3",
+    cover: "https://lh3.googleusercontent.com/wGlM3Iy2hpWgMuj7ZrL_py3XGOAERS7nm1w1g3eyLe2hNOnVf9olYIwrkqS5qzzymouQxnXMRrgHfFhD=w544-h544-l90-rj"
+  },
+  {
+    title: "Você é Mó Bagunçada",
+    artist: "Milsinho Toque Dez",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Toque Dez/Você é mó bagunçada.mp3",
+    cover: "https://lh3.googleusercontent.com/A5P8syU_D0iwghdja2Xt43DM-izKGXjHH3DiPpSiccYMT82J53pNHT_idV49bJw_indjrU3P-4dpMyII=w544-h544-l90-rj"
+  }
 ];
