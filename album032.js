6edfc563820efa061c5ad95cfedef360ae9067e8
Apps <wallxnip@gmail.com>
2026-01-05 22:35:28 -0300

    Atualizar o album032.js

--- album032.js
+++ album032.js
@@ -1,1 +1,52 @@
-y
+// TOP Center
+const playlist531 = [
+  {
+    title: "Sorry",
+    artist: "Justin Bieber",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/Sorry.mp3",
+    cover: "https://i.postimg.cc/BvB2vLGt/Sorry.webp"
+  },
+  {
+    title: "Ghost",
+    artist: "Justin Bieber",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/Ghost.mp3",
+    cover: "https://i.postimg.cc/zG177R5k/ghost.webp"
+  },
+  {
+    title: "Kiss Me",
+    artist: "Dermot Kennedy",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/Kiss Me.mp3",
+    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
+  },
+  {
+    title: "Better Days",
+    artist: "Dermot Kennedy",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/Better Days.mp3",
+    cover: "https://i.postimg.cc/NfbFJ3vV/dm.webp"
+  },
+  {
+    title: "Ma Chérie",
+    artist: "DJ Antoine, The Beatshakers",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/Ma Chérie.mp3",
+    cover: "https://i.postimg.cc/hG5zLB41/DJA.webp"
+  },
+  {
+    title: "What The Hell",
+    artist: "Avril Lavigne",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/What The Hell.mp3",
+    cover: "https://i.postimg.cc/4yK16PT9/wth.webp"
+  },
+  {
+    title: "Don't Leave Me Now",
+    artist: "Lost Frequencies",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/Don't Leave Me Now.mp3",
+    cover: "https://i.postimg.cc/zv0VfZQH/Lost-Frequencies.webp"
+  },
+  {
+    title: "Knockout",
+    artist: "Tungevaag",
+    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Ty Músic/Knockout.mp3",
+    cover: "https://i.postimg.cc/KYfPMVCT/1000479222-2.webp"
+  }
+
+];
