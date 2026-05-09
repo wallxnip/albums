2d944b527fbcf99223ea0d0cd8cc577d0daba013
Apps <wallxnip@gmail.com>
2026-05-09 00:57:45 -0300

    Atualizar o album003.js

--- album003.js
+++ album003.js
@@ -3,164 +3,164 @@ const playlist502 = [
   {
     title: "Esquecer de Tudo",
     artist: "Hip-Hop",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Hip-Hop (2025)/Esquecer de Tudo.mp3",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Hip-Hop (2025)/Esquecer de Tudo.mp3",
     cover: "https://i.postimg.cc/s2XSNZy8/Hip-Hop.webp"
   },
   {
     title: "Agora",
     artist: "Hip-Hop",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Hip-Hop (2025)/Agora.mp3",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Hip-Hop (2025)/Agora.mp3",
     cover: "https://i.postimg.cc/s2XSNZy8/Hip-Hop.webp"
   },
   {
     title: "Alma de Pipa",
     artist: "Hip-Hop",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Hip-Hop (2025)/Alma de Pipa.mp3",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Hip-Hop (2025)/Alma de Pipa.mp3",
     cover: "https://i.postimg.cc/s2XSNZy8/Hip-Hop.webp"
   },
   {
     title: "Carro Velho",
     artist: "Hip-Hop",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Hip-Hop (2025)/Carro Velho.mp3",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Hip-Hop (2025)/Carro Velho.mp3",
     cover: "https://i.postimg.cc/s2XSNZy8/Hip-Hop.webp"
   },
   {
     title: "Copo Pro Alto",
     artist: "Hip-Hop",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Hip-Hop (2025)/Copo Pro Alto.mp3",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Hip-Hop (2025)/Copo Pro Alto.mp3",
     cover: "https://i.postimg.cc/s2XSNZy8/Hip-Hop.webp"
   },
   {
     title: "Coração de Aço",
     artist: "Hip-Hop",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Hip-Hop (2025)/Coração de Aço.mp3",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Hip-Hop (2025)/Coração de Aço.mp3",
     cover: "https://i.postimg.cc/s2XSNZy8/Hip-Hop.webp"
   },
   {
     title: "Coração de Carro Forte",
     artist: "Hip-Hop",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Hip-Hop (2025)/Coração de Carro Forte.mp3",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Hip-Hop (2025)/Coração de Carro Forte.mp3",
     cover: "https://i.postimg.cc/s2XSNZy8/Hip-Hop.webp"
   },
   {
     title: "Obsessão",
     artist: "Hip-Hop",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Hip-Hop (2025)/Obsessão.mp3",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Hip-Hop (2025)/Obsessão.mp3",
     cover: "https://i.postimg.cc/s2XSNZy8/Hip-Hop.webp"
   },
   {
     title: "A Fenda",
     artist: "Hip-Hop",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Hip-Hop (2025)/A Fenda.mp3",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Hip-Hop (2025)/A Fenda.mp3",
     cover: "https://i.postimg.cc/s2XSNZy8/Hip-Hop.webp"
   },
   {
     title: "Preta",
     artist: "Hip-Hop",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Hip-Hop (2025)/Preta.mp3",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Hip-Hop (2025)/Preta.mp3",
     cover: "https://i.postimg.cc/s2XSNZy8/Hip-Hop.webp"
   },
   {
     title: "Vida Bagunçada",
     artist: "Hip-Hop",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Hip-Hop (2025)/Vida Bagunçada.mp3",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Hip-Hop (2025)/Vida Bagunçada.mp3",
     cover: "https://i.postimg.cc/s2XSNZy8/Hip-Hop.webp"
   },
   {
     title: "Quebra Cabeça",
     artist: "Hip-Hop",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Hip-Hop (2025)/Quebra Cabeça.mp3",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Hip-Hop (2025)/Quebra Cabeça.mp3",
     cover: "https://i.postimg.cc/s2XSNZy8/Hip-Hop.webp"
   },
   {
     title: "Shortinho Curto",
     artist: "Hip-Hop",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Hip-Hop (2025)/Shortinho Curto.mp3",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Hip-Hop (2025)/Shortinho Curto.mp3",
     cover: "https://i.postimg.cc/s2XSNZy8/Hip-Hop.webp"
   },
   {
     title: "Temporal",
     artist: "Hip-Hop",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Hip-Hop (2025)/Temporal.mp3",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Hip-Hop (2025)/Temporal.mp3",
     cover: "https://i.postimg.cc/s2XSNZy8/Hip-Hop.webp"
   },
   {
     title: "Um Pedido",
     artist: "Hip-Hop",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Hip-Hop (2025)/Um Pedido.mp3",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Hip-Hop (2025)/Um Pedido.mp3",
     cover: "https://i.postimg.cc/s2XSNZy8/Hip-Hop.webp"
   },
   {
     title: "Valores",
     artist: "Hip-Hop",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Hip-Hop (2025)/Valores.mp3",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Hip-Hop (2025)/Valores.mp3",
     cover: "https://i.postimg.cc/s2XSNZy8/Hip-Hop.webp"
   },
   {
     title: "Terrorista Memo",
     artist: "Hip-Hop",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Hip-Hop (2025)/Terrorista Memo.mp3",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Hip-Hop (2025)/Terrorista Memo.mp3",
     cover: "https://i.postimg.cc/s2XSNZy8/Hip-Hop.webp"
   },
   {
     title: "Sexta-Feira",
     artist: "Hip-Hop",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Hip-Hop (2025)/Sexta-Feira.mp3",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Hip-Hop (2025)/Sexta-Feira.mp3",
     cover: "https://i.postimg.cc/s2XSNZy8/Hip-Hop.webp"
   },
   {
     title: "Seu Coração Rodou",
     artist: "Hip-Hop",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Hip-Hop (2025)/Seu Coração Rodou.mp3",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Hip-Hop (2025)/Seu Coração Rodou.mp3",
     cover: "https://i.postimg.cc/s2XSNZy8/Hip-Hop.webp"
   },
   {
     title: "Renascer",
     artist: "Hip-Hop",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Hip-Hop (2025)/Renascer.mp3",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Hip-Hop (2025)/Renascer.mp3",
     cover: "https://i.postimg.cc/s2XSNZy8/Hip-Hop.webp"
   },
   {
     title: "Pirata de Esquina",
     artist: "Hip-Hop",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Hip-Hop (2025)/Pirata de Esquina.mp3",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Hip-Hop (2025)/Pirata de Esquina.mp3",
     cover: "https://i.postimg.cc/s2XSNZy8/Hip-Hop.webp"
   },
   {
     title: "O Amanhã Vai Ser Melhor",
     artist: "Hip-Hop",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Hip-Hop (2025)/O Amanhã Vai Ser Melhor.mp3",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Hip-Hop (2025)/O Amanhã Vai Ser Melhor.mp3",
     cover: "https://i.postimg.cc/s2XSNZy8/Hip-Hop.webp"
   },
   {
     title: "Furacão de Ninar",
     artist: "Hip-Hop",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Hip-Hop (2025)/Furacão de Ninar.mp3",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Hip-Hop (2025)/Furacão de Ninar.mp3",
     cover: "https://i.postimg.cc/s2XSNZy8/Hip-Hop.webp"
   },
 
   {
     title: "Imprevisível",
     artist: "Hip-Hop",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Hip-Hop (2025)/Imprevisível.mp3",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Hip-Hop (2025)/Imprevisível.mp3",
     cover: "https://i.postimg.cc/s2XSNZy8/Hip-Hop.webp"
   },
   {
     title: "Meu Melhor",
     artist: "Hip-Hop",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Hip-Hop (2025)/Meu Melhor.mp3",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Hip-Hop (2025)/Meu Melhor.mp3",
     cover: "https://i.postimg.cc/s2XSNZy8/Hip-Hop.webp"
   },
   {
     title: "Nosso Plano",
     artist: "Hip-Hop",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Hip-Hop (2025)/Nosso Plano.mp3",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Hip-Hop (2025)/Nosso Plano.mp3",
     cover: "https://i.postimg.cc/s2XSNZy8/Hip-Hop.webp"
   },
   {
     title: "Escondido",
     artist: "Hip-Hop",
-    file: "https://nd.nl.tab.digital/public.php/dav/files/QjzyC5ZixygLoHL/Hip-Hop (2025)/Escondido.mp3",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Hip-Hop (2025)/Escondido.mp3",
     cover: "https://i.postimg.cc/s2XSNZy8/Hip-Hop.webp"
   }
 
