ff6fc1ac28375f1296c0959c4a33b8da74652e0f
Apps <wallxnip@gmail.com>
2026-05-18 17:59:45 -0300

    Atualizar o album001.js

--- album001.js
+++ album001.js
@@ -69,3 +69,174 @@ const playlist500 = [
   }
 
 ];
+
+
+
+
+
+
+
+const playlist532 = [
+  {
+    title: "Agora É Tudo Meu",
+    artist: "MC Kevin o Chris",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Funk de destaque/Agora é Tudo Meu.mp3",
+    cover: "https://i.postimg.cc/BQkPKHFR/kh.webp"
+  },
+  {
+    title: "Ai Preto",
+    artist: "L7NNON, Bianca & Biel do Furduncinho",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Funk de destaque/Ai Preto.mp3",
+    cover: "https://i.postimg.cc/W4n4XkDG/Ai-Preto.webp"
+  },
+  {
+    title: "Amor de Verdade",
+    artist: "MC Kekel & MC Rita",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Funk de destaque/Amor de Verdade.mp3",
+    cover: "https://i.postimg.cc/d04q8cXn/Amor-de-Verdade.webp"
+  },
+  {
+    title: "Amor Falso",
+    artist: "Aldair Playboy",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Funk de destaque/Amor Falso.mp3",
+    cover: "https://i.postimg.cc/qvGtZYSK/Amor-Falso.webp"
+  },
+  {
+    title: "Azul Piscina",
+    artist: "MC Livinho",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Funk de destaque/Azul Piscina.mp3",
+    cover: "https://i.postimg.cc/7Y3bfdxV/Azul-Piscina.webp"
+  },
+  {
+    title: "Boca de Mel",
+    artist: "MC Bruninho",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Funk de destaque/Boca de Mel.mp3",
+    cover: "https://i.postimg.cc/SRWQYYpX/Boca-de-Mel.webp"
+  },
+  {
+    title: "Bola Rebola",
+    artist: "Tropkillaz, Anitta & J Balvin",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Funk de destaque/Bola Rebola.mp3",
+    cover: "https://i.postimg.cc/P56XDk01/Bola-Rebola.webp"
+  },
+  {
+    title: "Bota pra Tremer",
+    artist: "MC Danny",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Funk de destaque/Bota pra Tremer.mp3",
+    cover: "https://i.postimg.cc/kGF9grx8/Bota-pra-Tremer.webp"
+  },
+  {
+    title: "Cara Bacana",
+    artist: "MC Kevinho",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Funk de destaque/Cara Bacana.mp3",
+    cover: "https://i.postimg.cc/05dqmswD/Cara-Bacana.webp"
+  },
+  {
+    title: "Cheia de Marra",
+    artist: "MC Livinho",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Funk de destaque/Cheia de Marra.mp3",
+    cover: "https://i.postimg.cc/qRsW8sgL/Cheia-de-Marra.webp"
+  },
+  {
+    title: "DANÇARINA",
+    artist: "Pedro Sampaio & MC Pedrinho",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Funk de destaque/DANÇARINA.mp3",
+    cover: "https://i.postimg.cc/pXX3Fnxt/DANCARINA.webp"
+  },
+  {
+    title: "Eae Mozão 2",
+    artist: "DJ Ivis",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Funk de destaque/Eae Mozão 2.mp3",
+    cover: "https://i.postimg.cc/zDPFxLKN/Mozao2.webp"
+  },
+  {
+    title: "Eu Vou Com Carinho",
+    artist: "MC Leozinho ZS",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Funk de destaque/Eu Vou Com Carinho.mp3",
+    cover: "https://i.postimg.cc/KYJM4V5G/Eu-vou-com-carinho.webp"
+  },
+  {
+    title: "Eu Vou Te Pegar",
+    artist: "MC Kevin",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Funk de destaque/Eu Vou Te Pegar.mp3",
+    cover: "https://i.postimg.cc/kG8NVs4W/Eu-Vou-Te-Pegar.webp"
+  },
+  {
+    title: "Evoque Prata",
+    artist: "MC Menor HR",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Funk de destaque/Evoque Prata.mp3",
+    cover: "https://i.postimg.cc/Gm3Yrdkm/Evoque-Prata.webp"
+  },
+  {
+    title: "Fazer Falta",
+    artist: "Mc Livinho",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Funk de destaque/Fazer Falta.mp3",
+    cover: "https://i.postimg.cc/dtkp6c70/Fazer-Falta.webp"
+  },
+  {
+    title: "Forte Forte de Lacoste",
+    artist: "MC GW",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Funk de destaque/Forte Forte de Lacoste.mp3",
+    cover: "https://i.postimg.cc/hv9yFcVr/Forte-Forte-de-Lacoste.webp"
+  },
+  {
+    title: "Melodia Alucinógena",
+    artist: "MC Ryan SP",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Funk de destaque/Melodia Alucinógena.mp3",
+    cover: "https://i.postimg.cc/26nFyT38/Melodia-Alucinogena.webp"
+  },
+  {
+    title: "Não Para",
+    artist: "Anitta",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Funk de destaque/Não Para.mp3",
+    cover: "https://i.postimg.cc/FRX3McGH/Nao-Para.webp"
+  },
+  {
+    title: "O Bebê",
+    artist: "MC Kekel",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Funk de destaque/O Bebê.mp3",
+    cover: "https://i.postimg.cc/6QnGfqww/O-bebe.webp"
+  },
+  {
+    title: "Rebeca",
+    artist: "MC Livinho",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Funk de destaque/Rebeca.mp3",
+    cover: "https://i.postimg.cc/25GCYN75/Rebeca.webp"
+  },
+  {
+    title: "Te Amo Sem Compromisso",
+    artist: "MC Don Juan",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Funk de destaque/Te Amo Sem Compromisso.mp3",
+    cover: "https://i.postimg.cc/0Qr1qMNc/Te-Amo-Sem-Compromisso.webp"
+  },
+  {
+    title: "Tenebrosa",
+    artist: "MC Kevin o Chris",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Funk de destaque/Tenebrosa.mp3",
+    cover: "https://i.postimg.cc/C5YtxRMx/Tenebrosa.webp"
+  },
+  {
+    title: "Toma Tapa",
+    artist: "MC Danny",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Funk de destaque/Toma Tapa.mp3",
+    cover: "https://i.postimg.cc/TYQ1Vssr/Toma-tapa.webp"
+  },
+  {
+    title: "Veio pra Mudar",
+    artist: "MC Davi",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Funk de destaque/Veio pra Mudar.mp3",
+    cover: "https://i.postimg.cc/05nPFwCq/Veio-pra-Mudar.webp"
+  },
+  {
+    title: "Vem Quebrando",
+    artist: "MC Pedrinho",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Funk de destaque/Vem Quebrando.mp3",
+    cover: "https://i.postimg.cc/SQgbDpy0/Vem-Quebrando.webp"
+  },
+  {
+    title: "Você é Gostosa",
+    artist: "MC Kevin o Chris",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Funk de destaque/Você é Gostosa.mp3",
+    cover: "https://i.postimg.cc/2SvJRpfc/Voce-e-Gostosa.webp"
+  }
+];
