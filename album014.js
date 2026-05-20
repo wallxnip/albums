cc7d0b38ce4c37ef6dfef3578c64907e29c586f3
Apps <wallxnip@gmail.com>
2026-05-20 07:09:11 -0300

    Atualizar o album014.js

--- album014.js
+++ album014.js
@@ -16,132 +16,126 @@ const playlist513 = [
     title: "Alguien Conmigo",
     artist: "CNCO",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Latina (MIX)/Alguien Conmigo.mp3",
-    cover: "https://i.postimg.cc/g0DQcZxR/Latina.webp"
+    cover: "https://i.postimg.cc/nL6rDLJd/Alguien-Conmigo-cover-art.webp"
   },
   {
     title: "Baila Morena",
     artist: "Héctor & Tito",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Latina (MIX)/Baila Morena.mp3",
-    cover: "https://i.postimg.cc/g0DQcZxR/Latina.webp"
+    cover: "https://i.postimg.cc/CxnVfsxw/Baila-Morena-cover-art.webp"
   },
   {
     title: "Borracho Y Loco",
     artist: "Yandel",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Latina (MIX)/Borracho Y Loco.mp3",
-    cover: "https://i.postimg.cc/g0DQcZxR/Latina.webp"
+    cover: "https://i.postimg.cc/283sP7vn/Borracho-Y-Loco-cover-art.webp"
   },
   {
     title: "Calma",
     artist: "Pedro Capó & Farruko",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Latina (MIX)/Calma.mp3",
-    cover: "https://i.postimg.cc/g0DQcZxR/Latina.webp"
+    cover: "https://i.postimg.cc/m205KfTh/Calma-cover-art.webp"
   },
   {
     title: "Danza Kuduro",
     artist: "Don Omar & Lucenzo",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Latina (MIX)/Danza Kuduro.mp3",
-    cover: "https://i.postimg.cc/g0DQcZxR/Latina.webp"
+    cover: "https://i.postimg.cc/tggvxX5V/Danza-Kuduro-cover-art.webp"
   },
   {
     title: "Despacito",
     artist: "Luis Fonsi & Daddy Yankee",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Latina (MIX)/Despacito.mp3",
-    cover: "https://i.postimg.cc/g0DQcZxR/Latina.webp"
+    cover: "https://i.postimg.cc/DZZ68B2V/Despacito-cover-art.webp"
   },
   {
     title: "El Perdón",
     artist: "Nicky Jam & Enrique Iglesias",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Latina (MIX)/El Perdón.mp3",
-    cover: "https://i.postimg.cc/g0DQcZxR/Latina.webp"
+    cover: "https://i.postimg.cc/kgTf90mM/El-Perdon-cover-art.webp"
   },
   {
     title: "La La La",
     artist: "Shakira",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Latina (MIX)/La La La.mp3",
-    cover: "https://i.postimg.cc/g0DQcZxR/Latina.webp"
+    cover: "https://i.postimg.cc/mgpNK7YT/La-La-La-cover-art.webp"
   },
   {
     title: "La Tortura",
     artist: "Shakira & Alejandro Sanz",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Latina (MIX)/La Tortura.mp3",
-    cover: "https://i.postimg.cc/g0DQcZxR/Latina.webp"
+    cover: "https://i.postimg.cc/x1ZM6nQN/La-Tortura-cover-art.webp"
   },
   {
     title: "Llamada",
     artist: "J Álvarez",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Latina (MIX)/Llamada.mp3",
-    cover: "https://i.postimg.cc/g0DQcZxR/Latina.webp"
+    cover: "https://i.postimg.cc/qMz7jnvs/Llamada-cover-art.webp"
   },
   {
     title: "Llamada Perdida",
     artist: "Prince Royce",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Latina (MIX)/Llamada Perdida.mp3",
-    cover: "https://i.postimg.cc/g0DQcZxR/Latina.webp"
+    cover: "https://i.postimg.cc/vZCxWh7t/Llamada-Perdida-cover-art.webp"
   },
   {
     title: "Me Equivoqué",
     artist: "CD9",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Latina (MIX)/Me Equivoqué.mp3",
-    cover: "https://i.postimg.cc/g0DQcZxR/Latina.webp"
+    cover: "https://i.postimg.cc/RFSSfJ8B/Me-Equivoque-cover-art.webp"
   },
   {
     title: "Muriendo",
     artist: "KHEA",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Latina (MIX)/Muriendo.mp3",
-    cover: "https://i.postimg.cc/g0DQcZxR/Latina.webp"
+    cover: "https://i.postimg.cc/rpcMXtM7/Muriendo-cover-art.webp"
   },
   {
     title: "No Hay Nada Imposible",
     artist: "Chayanne",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Latina (MIX)/No Hay Nada Imposible.mp3",
-    cover: "https://i.postimg.cc/g0DQcZxR/Latina.webp"
+    cover: "https://i.postimg.cc/Kj3dgZmD/No-Hay-Nada-Imposible-cover-art.webp"
   },
   {
     title: "Perro Fiel",
     artist: "Shakira & Nicky Jam",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Latina (MIX)/Perro Fiel.mp3",
-    cover: "https://i.postimg.cc/g0DQcZxR/Latina.webp"
+    cover: "https://i.postimg.cc/7PTHz4sF/Perro-Fiel-cover-art.webp"
   },
   {
     title: "Rabiosa",
     artist: "Shakira & Pitbull",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Latina (MIX)/Rabiosa.mp3",
-    cover: "https://i.postimg.cc/g0DQcZxR/Latina.webp"
+    cover: "https://i.postimg.cc/mkQW5bKT/Rabiosa-cover-art.webp"
   },
   {
     title: "Responde",
     artist: "Brytiago",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Latina (MIX)/Responde.mp3",
-    cover: "https://i.postimg.cc/g0DQcZxR/Latina.webp"
+    cover: "https://i.postimg.cc/nLCbtKN3/Responde-cover-art.webp"
   },
   {
     title: "Sofia",
     artist: "Álvaro Soler",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Latina (MIX)/Sofia.mp3",
-    cover: "https://i.postimg.cc/g0DQcZxR/Latina.webp"
+    cover: "https://i.postimg.cc/DwpH5Jm9/Sofia-cover-art.webp"
   },
   {
     title: "Solo Tu",
     artist: "CNCO",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Latina (MIX)/Solo Tu.mp3",
-    cover: "https://i.postimg.cc/g0DQcZxR/Latina.webp"
-  },
-  {
-    title: "Tusa",
-    artist: "Karol G & Nicki Minaj",
-    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Latina (MIX)/Tusa.mp3",
-    cover: "https://i.postimg.cc/g0DQcZxR/Latina.webp"
+    cover: "https://i.postimg.cc/4NJ0gb9L/Solo-Tu-cover-art.webp"
   },
   {
     title: "Vem Dancar Kuduro",
     artist: "Latino",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Latina (MIX)/Vem Dancar Kuduro.mp3",
-    cover: "https://i.postimg.cc/g0DQcZxR/Latina.webp"
+    cover: "https://i.postimg.cc/tT3r0YCb/Vem-Dancar-Kuduro-cover-art.webp"
   },
   {
     title: "Waiting for Tonight",
     artist: "Jennifer Lopez",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Latina (MIX)/Waiting for Tonight.mp3",
-    cover: "https://i.postimg.cc/g0DQcZxR/Latina.webp"
+    cover: "https://i.postimg.cc/qMRGwmnT/Waiting-for-Tonight-cover-art.webp"
   }
 ];
