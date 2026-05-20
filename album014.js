da67e66c72df9c7bfcb80fe4cb900558e3c806b7
Apps <wallxnip@gmail.com>
2026-05-20 00:44:28 -0300

    Atualizar o album014.js

--- album014.js
+++ album014.js
@@ -12,6 +12,59 @@ const playlist513 = [
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Ty Músic/Tusa.mp3",
     cover: "https://i.postimg.cc/W16f1s6Y/Tusa.webp"
   },
-  
+    {
+    title: "Despacito",
+    artist: "Luis Fonsi & Daddy Yankee",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Latina (MIX)/Despacito.mp3",
+    cover: "https://open.voidware.de/track/6habFhsOp2NvshLv26DqMb"
+  },
+  {
+    title: "Calma",
+    artist: "Pedro Capó & Farruko",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Latina (MIX)/Calma.mp3",
+    cover: "https://open.voidware.de/track/1xQ6trAsedVPCdbtDAmk0c"
+  },
+  {
+    title: "Danza Kuduro",
+    artist: "Don Omar & Lucenzo",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Latina (MIX)/Danza Kuduro.mp3",
+    cover: "https://open.voidware.de/track/0DQ1ITjI0YoLFzuADN1ZBW"
+  },
+  {
+    title: "El Perdón",
+    artist: "Nicky Jam & Enrique Iglesias",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Latina (MIX)/El Perdón.mp3",
+    cover: "https://open.voidware.de/track/50iBJRk4SMp7b5Ne9nS1rk"
+  },
+  {
+    title: "Tusa",
+    artist: "Karol G & Nicki Minaj",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Latina (MIX)/Tusa.mp3",
+    cover: "https://open.voidware.de/track/7k4t7uLgtOxPwTpFmtJNTY"
+  },
+  {
+    title: "Sofia",
+    artist: "Álvaro Soler",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Latina (MIX)/Sofia.mp3",
+    cover: "https://open.voidware.de/track/2bgTY4UwhfBYhGT4HUYStN"
+  },
+  {
+    title: "Perro Fiel",
+    artist: "Shakira & Nicky Jam",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Latina (MIX)/Perro Fiel.mp3",
+    cover: "https://open.voidware.de/track/6R7gkzq6bNWUC0leQjT0Q9"
+  },
+  {
+    title: "Rabiosa",
+    artist: "Shakira & Pitbull",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Latina (MIX)/Rabiosa.mp3",
+    cover: "https://open.voidware.de/track/2dlln9jfJ4i0lY8Yk6N3f3"
+  },
+  {
+    title: "Waiting for Tonight",
+    artist: "Jennifer Lopez",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Latina (MIX)/Waiting for Tonight.mp3",
+    cover: "https://open.voidware.de/track/0uGgYZIi8VYx0YuzH0d8rU"
+  }
   
 ];
