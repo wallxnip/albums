0b02e4fd30b562b1043a8a6d57097b674a8d7671
Apps <wallxnip@gmail.com>
2026-05-21 20:26:39 -0300

    Atualizar o album002.js

--- album002.js
+++ album002.js
@@ -128,143 +128,150 @@ const playlist501 = [
 ];
 
 
+// Velhos Tempos
 const playlist533 = [
+    {
+    title: "Ela cansou",
+    artist: "Erreap",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/Ela cansou.mp3",
+    cover: "https://i.postimg.cc/dVyQ0HRR/Ela-cansou-cover-art.webp"
+  },
   {
     title: "Esquecer de Tudo",
     artist: "MC Marcinho",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/Esquecer de Tudo.mp3",
-    cover: "https://i.scdn.co/image/ab67616d0000b273585b1c77c64f7935bc9c0fd6"
+    cover: "https://i.postimg.cc/v8Kd67Zk/Esquecer-de-Tudo-cover-art.webp"
   },
   {
     title: "Eterna Amante",
     artist: "Sampa Crew",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/Eterna Amante.mp3",
-    cover: "https://i.scdn.co/image/ab67616d0000b273822d741e3bb7d638055fd9c6"
+    cover: "https://i.postimg.cc/gJB18dmk/Eterna-Amante-cover-art.webp"
   },
   {
     title: "Eterno",
-    artist: "Exaltasamba",
+    artist: "Jhef",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/Eterno.mp3",
-    cover: "https://i.scdn.co/image/ab67616d0000b2730f4f39004fcb63dcd2f05f6a"
+    cover: "https://i.postimg.cc/tC9mJYh6/Beauty-Plus-20260521194026186-save.webp"
   },
   {
     title: "Fogo",
-    artist: "Bonde da Stronda",
+    artist: "Silas Magalhães",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/Fogo.mp3",
-    cover: "https://i.scdn.co/image/ab67616d0000b273b5c8b6dc8f8b9a6f4c2e5b1a"
+    cover: "https://i.postimg.cc/XvBsmFbj/Fogo-cover-art.webp"
   },
   {
     title: "Há um Lugar",
-    artist: "Onze:20",
+    artist: "Michaell D, Dimael Kharrara",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/Há um Lugar.mp3",
-    cover: "https://i.scdn.co/image/ab67616d0000b2739d4f5c7a1b2d3e4f6a7b8c9d"
+    cover: "https://i.postimg.cc/7YPmDjkM/Ha-um-Lugar-cover-art.webp"
   },
   {
     title: "Inconsciente",
-    artist: "Strike",
+    artist: "Fiitu",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/Inconsciente.mp3",
-    cover: "https://i.scdn.co/image/ab67616d0000b2736c5d4b3a2f1e9d8c7b6a5f4e"
+    cover: "https://i.postimg.cc/wBNk0pXH/Inconsciente-cover-art.webp"
   },
   {
     title: "Irrepreensível",
-    artist: "NX Zero",
+    artist: "Mano Sassá, Karen Ribeiro",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/Irrepreensível.mp3",
-    cover: "https://i.scdn.co/image/ab67616d0000b2735f4e3d2c1b0a9e8d7c6b5a4f"
+    cover: "https://i.postimg.cc/02x2NdWB/Irrepreensivel.webp"
   },
   {
     title: "Momentos",
-    artist: "Calcinha Preta",
+    artist: "Ken Dantas, Sadstation, Felipe Phyre, Dreicon",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/Momentos.mp3",
-    cover: "https://i.scdn.co/image/ab67616d0000b2731d2c3b4a5f6e7d8c9b0a1f2e"
+    cover: "https://i.postimg.cc/zfR17V2v/Momentos-cover-art.webp"
   },
   {
     title: "Não Consigo Revoar",
-    artist: "Charlie Brown Jr.",
+    artist: "Jhef",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/Não Consigo Revoar.mp3",
-    cover: "https://i.scdn.co/image/ab67616d0000b2737a8b9c0d1e2f3a4b5c6d7e8f"
+    cover: "https://i.postimg.cc/XqnSnxjH/Nao-Consigo-Revoar-cover-art.webp"
   },
   {
     title: "Não Vivo Sem Você",
-    artist: "LS Jack",
+    artist: "Novo Cenario, Wlad Borges",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/Não Vivo Sem Você.mp3",
-    cover: "https://i.scdn.co/image/ab67616d0000b2732a3b4c5d6e7f8a9b0c1d2e3f"
+    cover: "https://i.postimg.cc/44Pjz6N4/Nao-Vivo-Sem-Voce-cover-art.webp"
   },
   {
     title: "Não Vou Me Perder",
-    artist: "CPM 22",
+    artist: "Marcela",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/Não Vou Me Perder.mp3",
-    cover: "https://i.scdn.co/image/ab67616d0000b2738e7d6c5b4a3f2e1d0c9b8a7f"
+    cover: "https://i.postimg.cc/8CL0sW0T/Nao-Vou-Me-Perder-cover-art.webp"
   },
   {
     title: "O Amanhã Vai Ser Melhor, Pt. 2",
-    artist: "MC Hariel",
+    artist: "Thiagão",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/O Amanhã Vai Ser Melhor, Pt. 2.mp3",
-    cover: "https://i.scdn.co/image/ab67616d0000b2730f1e2d3c4b5a697887766554"
+    cover: "https://i.postimg.cc/gk5SD4Cq/O-Amanha-Vai-Ser-Melhor-Pt-2-cover-art.webp"
   },
   {
     title: "O Que eu Quero",
-    artist: "Strike",
+    artist: "Tauz",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/O Que eu Quero.mp3",
-    cover: "https://i.scdn.co/image/ab67616d0000b2734b5c6d7e8f9a0b1c2d3e4f5a"
+    cover: "https://i.postimg.cc/Pxt2x7DY/O-Que-eu-Quero-cover-art.webp"
   },
   {
     title: "O Tempo",
-    artist: "Charlie Brown Jr.",
+    artist: "Rapdemia",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/O Tempo.mp3",
-    cover: "https://i.scdn.co/image/ab67616d0000b2739a8b7c6d5e4f3a2b1c0d9e8f"
+    cover: "https://i.postimg.cc/BbzgmQVZ/O-Tempo-cover-art.webp"
   },
   {
     title: "Obrigado Senhor",
-    artist: "Belo",
+    artist: "Mateca",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/Obrigado Senhor.mp3",
-    cover: "https://i.scdn.co/image/ab67616d0000b2731c2d3e4f5a6b7c8d9e0f1a2b"
+    cover: "https://i.postimg.cc/3RSCwxYK/Obrigado-Senhor-cover-art.webp"
   },
   {
     title: "Perdão Pai",
-    artist: "MC Daleste",
+    artist: "Rap Menorah",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/Perdão Pai.mp3",
-    cover: "https://i.scdn.co/image/ab67616d0000b2733b4c5d6e7f8a9b0c1d2e3f4a"
+    cover: "https://i.postimg.cc/y8mmZPsD/Perdao-Pai-cover-art.webp"
   },
   {
     title: "Quantas Vezes",
-    artist: "Pixote",
+    artist: "Tauz",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/Quantas Vezes.mp3",
-    cover: "https://i.scdn.co/image/ab67616d0000b2735a6b7c8d9e0f1a2b3c4d5e6f"
+    cover: "https://i.postimg.cc/ydbcHpyd/Quantas-Vezes-cover-art.webp"
   },
   {
     title: "Quarto Equipado",
-    artist: "Hungria Hip Hop",
+    artist: "Hariel Costta",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/Quarto Equipado.mp3",
-    cover: "https://i.scdn.co/image/ab67616d0000b2737c8d9e0f1a2b3c4d5e6f7a8b"
+    cover: "https://i.postimg.cc/kMLYNTPm/Quarto-Equipado-cover-art.webpb"
   },
   {
     title: "Renascer",
-    artist: "Rashid",
+    artist: "Insanou",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/Renascer.mp3",
-    cover: "https://i.scdn.co/image/ab67616d0000b2732d3e4f5a6b7c8d9e0f1a2b3c"
+    cover: "https://i.postimg.cc/DZ85LXC4/Renascer-cover-art.webp"
   },
   {
     title: "Se Acaba e Adora",
-    artist: "Bonde da Stronda",
+    artist: "Jhef",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/Se Acaba e Adora.mp3",
-    cover: "https://i.scdn.co/image/ab67616d0000b2736e7f8a9b0c1d2e3f4a5b6c7d"
+    cover: "https://i.postimg.cc/YqwfqZBZ/Se-Acaba-e-Adora-cover-art.webp"
   },
   {
     title: "Sou Vencedor",
-    artist: "Projota",
+    artist: "Mc Pedrinho",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/Sou Vencedor.mp3",
-    cover: "https://i.scdn.co/image/ab67616d0000b27390f791db8457ef576c0d18c1"
+    cover: "https://i.postimg.cc/g2gvs0LG/Sou-Vencedor-cover-art.png"
   },
   {
     title: "Tô Fixado",
-    artist: "MC Smith",
+    artist: "ConexaoCN Feat. Kallebi",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/Tô Fixado.mp3",
-    cover: "https://i.scdn.co/image/ab67616d0000b273f89fe61b0c726427f8536d0d"
+    cover: "https://i.postimg.cc/PJbDwfkj/To-Fixado-cover-art.webp"
   },
   {
     title: "Vazio",
-    artist: "Filipe Ret",
+    artist: "Lookas",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/Vazio.mp3",
-    cover: "https://i.scdn.co/image/ab67616d0000b2734d7b9f5c8a1e2d3c7b4f5a6d"
+    cover: "https://i.postimg.cc/X7QGSzjx/Vazio-cover-art.webp"
   }
 ];
