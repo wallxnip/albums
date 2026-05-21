bbb21e9d877e01bc2d602de23c56b4d8ed7afc95
Apps <wallxnip@gmail.com>
2026-05-21 13:12:12 -0300

    Atualizar o album002.js

--- album002.js
+++ album002.js
@@ -125,9 +125,146 @@ const playlist501 = [
   },
   
 
+];
 
 
-
-
-
+const playlist533 = [
+  {
+    title: "Esquecer de Tudo",
+    artist: "MC Marcinho",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/Esquecer de Tudo.mp3",
+    cover: "https://i.scdn.co/image/ab67616d0000b273585b1c77c64f7935bc9c0fd6"
+  },
+  {
+    title: "Eterna Amante",
+    artist: "Sampa Crew",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/Eterna Amante.mp3",
+    cover: "https://i.scdn.co/image/ab67616d0000b273822d741e3bb7d638055fd9c6"
+  },
+  {
+    title: "Eterno",
+    artist: "Exaltasamba",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/Eterno.mp3",
+    cover: "https://i.scdn.co/image/ab67616d0000b2730f4f39004fcb63dcd2f05f6a"
+  },
+  {
+    title: "Fogo",
+    artist: "Bonde da Stronda",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/Fogo.mp3",
+    cover: "https://i.scdn.co/image/ab67616d0000b273b5c8b6dc8f8b9a6f4c2e5b1a"
+  },
+  {
+    title: "Há um Lugar",
+    artist: "Onze:20",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/Há um Lugar.mp3",
+    cover: "https://i.scdn.co/image/ab67616d0000b2739d4f5c7a1b2d3e4f6a7b8c9d"
+  },
+  {
+    title: "Inconsciente",
+    artist: "Strike",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/Inconsciente.mp3",
+    cover: "https://i.scdn.co/image/ab67616d0000b2736c5d4b3a2f1e9d8c7b6a5f4e"
+  },
+  {
+    title: "Irrepreensível",
+    artist: "NX Zero",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/Irrepreensível.mp3",
+    cover: "https://i.scdn.co/image/ab67616d0000b2735f4e3d2c1b0a9e8d7c6b5a4f"
+  },
+  {
+    title: "Momentos",
+    artist: "Calcinha Preta",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/Momentos.mp3",
+    cover: "https://i.scdn.co/image/ab67616d0000b2731d2c3b4a5f6e7d8c9b0a1f2e"
+  },
+  {
+    title: "Não Consigo Revoar",
+    artist: "Charlie Brown Jr.",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/Não Consigo Revoar.mp3",
+    cover: "https://i.scdn.co/image/ab67616d0000b2737a8b9c0d1e2f3a4b5c6d7e8f"
+  },
+  {
+    title: "Não Vivo Sem Você",
+    artist: "LS Jack",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/Não Vivo Sem Você.mp3",
+    cover: "https://i.scdn.co/image/ab67616d0000b2732a3b4c5d6e7f8a9b0c1d2e3f"
+  },
+  {
+    title: "Não Vou Me Perder",
+    artist: "CPM 22",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/Não Vou Me Perder.mp3",
+    cover: "https://i.scdn.co/image/ab67616d0000b2738e7d6c5b4a3f2e1d0c9b8a7f"
+  },
+  {
+    title: "O Amanhã Vai Ser Melhor, Pt. 2",
+    artist: "MC Hariel",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/O Amanhã Vai Ser Melhor, Pt. 2.mp3",
+    cover: "https://i.scdn.co/image/ab67616d0000b2730f1e2d3c4b5a697887766554"
+  },
+  {
+    title: "O Que eu Quero",
+    artist: "Strike",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/O Que eu Quero.mp3",
+    cover: "https://i.scdn.co/image/ab67616d0000b2734b5c6d7e8f9a0b1c2d3e4f5a"
+  },
+  {
+    title: "O Tempo",
+    artist: "Charlie Brown Jr.",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/O Tempo.mp3",
+    cover: "https://i.scdn.co/image/ab67616d0000b2739a8b7c6d5e4f3a2b1c0d9e8f"
+  },
+  {
+    title: "Obrigado Senhor",
+    artist: "Belo",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/Obrigado Senhor.mp3",
+    cover: "https://i.scdn.co/image/ab67616d0000b2731c2d3e4f5a6b7c8d9e0f1a2b"
+  },
+  {
+    title: "Perdão Pai",
+    artist: "MC Daleste",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/Perdão Pai.mp3",
+    cover: "https://i.scdn.co/image/ab67616d0000b2733b4c5d6e7f8a9b0c1d2e3f4a"
+  },
+  {
+    title: "Quantas Vezes",
+    artist: "Pixote",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/Quantas Vezes.mp3",
+    cover: "https://i.scdn.co/image/ab67616d0000b2735a6b7c8d9e0f1a2b3c4d5e6f"
+  },
+  {
+    title: "Quarto Equipado",
+    artist: "Hungria Hip Hop",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/Quarto Equipado.mp3",
+    cover: "https://i.scdn.co/image/ab67616d0000b2737c8d9e0f1a2b3c4d5e6f7a8b"
+  },
+  {
+    title: "Renascer",
+    artist: "Rashid",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/Renascer.mp3",
+    cover: "https://i.scdn.co/image/ab67616d0000b2732d3e4f5a6b7c8d9e0f1a2b3c"
+  },
+  {
+    title: "Se Acaba e Adora",
+    artist: "Bonde da Stronda",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/Se Acaba e Adora.mp3",
+    cover: "https://i.scdn.co/image/ab67616d0000b2736e7f8a9b0c1d2e3f4a5b6c7d"
+  },
+  {
+    title: "Sou Vencedor",
+    artist: "Projota",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/Sou Vencedor.mp3",
+    cover: "https://i.scdn.co/image/ab67616d0000b27390f791db8457ef576c0d18c1"
+  },
+  {
+    title: "Tô Fixado",
+    artist: "MC Smith",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/Tô Fixado.mp3",
+    cover: "https://i.scdn.co/image/ab67616d0000b273f89fe61b0c726427f8536d0d"
+  },
+  {
+    title: "Vazio",
+    artist: "Filipe Ret",
+    file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Velhos Tempos/Vazio.mp3",
+    cover: "https://i.scdn.co/image/ab67616d0000b2734d7b9f5c8a1e2d3c7b4f5a6d"
+  }
 ];
