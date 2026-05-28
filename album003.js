f4935d93751593da8b66f30f5b22899699ff7b21
Apps <wallxnip@gmail.com>
2026-05-27 22:05:47 -0300

    Atualizar o album003.js

--- album003.js
+++ album003.js
@@ -171,166 +171,166 @@ const playlist502 = [
 
 // Wallker (REMIX)
 const playlist534 = [
-  {
+{
     title: "Aetheris",
     artist: "Xenø Fader",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Wallker (REMIX)/Aetheris.mp3",
-    cover: "#"
+    cover: "https://i.postimg.cc/4xfXGY5c/Aetheris.webp"
   },
   {
     title: "All I Know",
     artist: "Axel Johansson, JANARAE",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Wallker (REMIX)/All I Know.mp3",
-    cover: "#"
+    cover: "https://i.postimg.cc/j26r0TJb/All-I-Know-cover-art.webp"
   },
   {
     title: "Another Life",
     artist: "Lucas & Steve, Alida",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Wallker (REMIX)/Another Life.mp3",
-    cover: "#"
+    cover: "https://i.postimg.cc/7ZtrymhK/Another-Life-cover-art.webp"
   },
   {
     title: "Broken",
     artist: "Amél",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Wallker (REMIX)/Broken.mp3",
-    cover: "#"
+    cover: "https://i.postimg.cc/HLJDWX7j/Broken-cover-art.webp"
   },
   {
     title: "Can't Reach You",
     artist: "XZIO Music",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Wallker (REMIX)/Can't Reach You.mp3",
-    cover: "#"
+    cover: "https://i.postimg.cc/sxrtJLHB/Can-t-Reach-You-cover-art.webp"
   },
   {
     title: "Close Your Eyes",
     artist: "KSHMR",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Wallker (REMIX)/Close Your Eyes.mp3",
-    cover: "#"
+    cover: "https://i.postimg.cc/90YsgBs6/Close-Your-Eyes-cover-art.webp"
   },
   {
     title: "Cold Goodbye",
     artist: "LadyMelody",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Wallker (REMIX)/Cold Goodbye.mp3",
-    cover: "#"
+    cover: "https://i.postimg.cc/jdvGzZH4/Cold-Goodbye-cover-art.webp"
   },
   {
     title: "Crazy",
     artist: "Tungevaag",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Wallker (REMIX)/Crazy.mp3",
-    cover: "#"
+    cover: "https://i.postimg.cc/cC0b3n67/Crazy-cover-art.webp"
   },
   {
     title: "Dancers In The Dark",
     artist: "K-391, Modi",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Wallker (REMIX)/Dancers In The Dark.mp3",
-    cover: "#"
+    cover: "https://i.postimg.cc/6py1Cw5B/Dancers-In-The-Dark-cover-art.webp"
   },
   {
     title: "Don't Go",
     artist: "Ashlee Walker",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Wallker (REMIX)/Don't Go.mp3",
-    cover: "#"
+    cover: "https://i.postimg.cc/Kj0Jgp6x/Don-t-Go-cover-art.webp"
   },
   {
     title: "Find My Way",
     artist: "Albert Vishi",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Wallker (REMIX)/Find My Way.mp3",
-    cover: "#"
+    cover: "https://i.postimg.cc/mgz8tw8T/Find-My-Way-cover-art.webp"
   },
   {
     title: "I Need Your Love",
     artist: "Audio Vibes Alliance",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Wallker (REMIX)/I Need Your Love.mp3",
-    cover: "#"
+    cover: "https://i.postimg.cc/Kc0NfVWz/I-Need-Your-Love-cover-art.webp"
   },
   {
     title: "I Still Need You",
     artist: "XZIO Music",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Wallker (REMIX)/I Still Need You.mp3",
-    cover: "#"
+    cover: "https://i.postimg.cc/prrK3WSH/I-Still-Need-You-cover-art.webp"
   },
   {
     title: "I'll Be There",
     artist: "Audio Vibes Alliance (feat. Yassine DJS)",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Wallker (REMIX)/I'll Be There.mp3",
-    cover: "#"
+    cover: "https://i.postimg.cc/SxCCBtgh/I-ll-Be-There-cover-art.webp"
   },
   {
     title: "Invisible",
     artist: "Julius Dreisig, Zeus X Crona",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Wallker (REMIX)/Invisible.mp3",
-    cover: "#"
+    cover: "https://i.postimg.cc/jjRwGqW1/Invisible-cover-art.webp"
   },
   {
     title: "Kiss Forever",
     artist: "Ashlee Walker",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Wallker (REMIX)/Kiss Forever.mp3",
-    cover: "#"
+    cover: "https://i.postimg.cc/Kj0Jgp6x/Don-t-Go-cover-art.webp"
   },
   {
     title: "Nothing At All",
     artist: "Remedeus",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Wallker (REMIX)/Nothing At All.mp3",
-    cover: "#"
+    cover: "https://i.postimg.cc/5yQ6w6G0/Nothing-At-All-cover-art.webp"
   },
   {
     title: "One of Us - Fan Edition",
     artist: "Axel Johansson, ANARAE",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Wallker (REMIX)/One of Us - Fan Edition.mp3",
-    cover: "#"
+    cover: "https://i.postimg.cc/YCHzCHGR/One-of-Us-Fan-Edition-cover-art.webp"
   },
   {
     title: "Remember Our Summer",
     artist: "AbdielGE",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Wallker (REMIX)/Remember Our Summer.mp3",
-    cover: "#"
+    cover: "https://i.postimg.cc/Ls4kj3V8/Remember-Our-Summer-cover-art.webp"
   },
   {
     title: "Right Side of my Brain",
     artist: "DJ Johnny JSA",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Wallker (REMIX)/Right Side of my Brain.mp3",
-    cover: "#"
+    cover: "https://i.postimg.cc/B6XxVZyh/Right-Side-of-my-Brain-cover-art.webp"
   },
   {
     title: "Save Me",
     artist: "Jerber lazaro",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Wallker (REMIX)/Save Me.mp3",
-    cover: "#"
+    cover: "https://i.postimg.cc/T25VjwnK/Save-Me-cover-art.webp"
   },
   {
     title: "Stay Forever",
     artist: "Elian Skye",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Wallker (REMIX)/Stay Forever.mp3",
-    cover: "#"
+    cover: "https://i.postimg.cc/m2Q7q5X1/Stay-Forever-cover-art.webp"
   },
   {
     title: "Takeaway",
     artist: "Arvy Nacht",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Wallker (REMIX)/Takeaway.mp3",
-    cover: "#"
+    cover: "https://i.postimg.cc/26bZxcbt/Takeaway-cover-art.webp"
   },
   {
     title: "Tired - Kygo Remix",
     artist: "Alan Walker, Gavin James, Kygo",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Wallker (REMIX)/Tired - Kygo Remix.mp3",
-    cover: "#"
+    cover: "https://i.postimg.cc/1XDVk8KH/Tired-Kygo-Remix-cover-art.webp"
   },
   {
     title: "Turn The Lights Off",
     artist: "AlanthorDJ (feat. JJON)",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Wallker (REMIX)/Turn The Lights Off.mp3",
-    cover: "#"
+    cover: "https://i.postimg.cc/15ZqpK9k/Turn-the-Lights-Off-Fan-Edition-cover-art.webp"
   },
   {
     title: "Turn the Lights Off - Fan Edition",
     artist: "Dj Quba, Sandra K, ISHNLV",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Wallker (REMIX)/Turn the Lights Off - Fan Edition.mp3",
-    cover: "#"
+    cover: "https://i.postimg.cc/pLW5SXnH/Turn-The-Lights-Off-cover-art.webp"
   },
   {
     title: "Wherever You Are",
     artist: "Martin Garrix, DubVision, Shaun Farrugia",
     file: "https://pub-e00f997a056944a1ada4449909225910.r2.dev/Wallker (REMIX)/Wherever You Are.mp3",
-    cover: "#"
+    cover: "https://i.postimg.cc/R0q6MsRq/Wherever-You-Are-cover-art.webp"
   }
 ];
